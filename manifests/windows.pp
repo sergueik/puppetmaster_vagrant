7da7f3e6740cab157fd6139cc89ed0b3be17075d
sergueik <kouzmine_serguei@yahoo.com>
2015-09-01 17:34:34 -0400

    added basic iis

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -1,18 +1,6 @@
+# -*- mode: puppet -*-
+# vi: set ft=puppet :
+
 node 'windows7' {
-
-  notify { 'log_message1' :
-    message => 'Started node manifest',
-  }
-
-  exec { 'connect to WMI':
-     # file will be written to c:/Windows/System32/
-     command   => 'write-output (Get-WMIObject "cim_operatingsystem") | out-file -filepath "puppet_powershell_run.txt"',
-     provider  => powershell,
-  }
-
-include 'mywebsite'
-
-  notify { 'log_message2' :
-    message => 'completed execution',
-  }
+  include 'mywebsite'
 }
---
+++ modules/mywebsite/manifests/init.pp
@@ -0,0 +1,29 @@
+# -*- mode: puppet -*-
+# vi: set ft=puppet :
+class mywebsite {
+    iis::manage_app_pool {'my_application_pool':
+      enable_32_bit           => true,
+      managed_runtime_version => 'v4.0',
+    }
+
+    iis::manage_site {'www.mysite.com':
+      site_path     => 'C:\inetpub\wwwroot\mysite',
+      port          => '80',
+      ip_address    => '*',
+      host_header   => 'www.mysite.com',
+      app_pool      => 'my_application_pool'
+    }
+
+    iis::manage_virtual_application {'application1':
+      site_name   => 'www.mysite.com',
+      site_path   => 'C:\inetpub\wwwroot\application1',
+      app_pool    => 'my_application_pool'
+    }
+
+    iis::manage_virtual_application {'application2':
+      site_name   => 'www.mysite.com',
+      site_path   => 'C:\inetpub\wwwroot\application2',
+      app_pool    => 'my_application_pool'
+    }
+}
+
