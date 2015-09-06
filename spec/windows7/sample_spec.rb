d28c631505782236deb277b84f550ed95939a03d
sergueik <kouzmine_serguei@yahoo.com>
2015-09-06 14:57:03 -0400

    intermediate commit: loading assemlies

--- spec/windows7/sample_spec.rb
+++ spec/windows7/sample_spec.rb
@@ -4,82 +4,22 @@
 
 require_relative '../windows_spec_helper'
 context 'Commands' do
-  describe command ('ipconfig ') do
+  describe command ('& "ipconfig" ') do
     its(:stdout) { should match /^Windows IP Configuration/ }
     its(:stderr) { should be_empty }
     its(:exit_status) {should eq 0 }
   end
+  describe command ('(get-CIMInstance "Win32_ComputerSystem" -Property "DNSHostName").DNSHostName') do
+    its(:stdout) { should match /\bwindows7\b/ }
+    its(:stderr) { should be_empty }
+    its(:exit_status) {should eq 0 }
+  end
+  describe command ('[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms");  $f = New-Object System.Windows.Forms.Form; write-output ($f.getType().Namespace)') do
+    its(:stdout) { should match /System.Windows.Forms/ }
+    its(:stderr) { should be_empty }
+    its(:exit_status) {should eq 0 }
+  end
+  describe command ('([System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")).GetExportedTypes() |foreach-object { if ($_.Name -eq "Form") {write-output $_.NameSpace}}') do
+    its(:stdout) { should match /System.Windows.Forms/ }
+  end
 end 
-context 'Default Site' do
-  describe windows_feature('IIS-Webserver') do
-    it{ should be_installed.by("dism") }
-  end
-  describe iis_app_pool('DefaultAppPool') do
-    it{ should exist }
-  end
-  describe file('c:/inetpub/wwwroot') do
-    it { should be_directory }
-  end
-end
-context 'mysite' do
-  describe iis_app_pool('my_application_pool') do
-    it{ should exist }
-    it{ should have_dotnet_version('4.0') }
-    it{ should have_managed_pipeline_mode('integrated') }
-  end
-  describe iis_website('www.mysite.com') do
-    xit{ should be_installed }
-    it{ should exist }
-    it{ should be_enabled }
-    it{ should be_running }
-    it{ should have_physical_path('C:\\inetpub\\wwwroot\\mysite') } 
-    it{ should be_in_app_pool('my_application_pool') }
-    it{ should have_site_application('application1') }
-    it{ should have_site_bindings('8080','http','*') }
-  end
-  describe file('c:/inetpub/wwwroot/mysite') do
-    it { should be_directory }
-  end
-  describe file( 'c:/windows/system32/inetsrv/config/applicationHost.config') do
-    # let(:pre_command) { 'start-sleep -seconds 120 '  }
-    it { should be_file  }
-    it { should contain('www.mysite.com')  }
-  end
-  describe port(8080) do
-    it { should be_listening }
-  end
-end
-context 'World Wide Web Publishing Service' do
-  describe service('W3SVC') do
-    # comment slow command
-    it { should be_running }
-  end
-end
-context 'Windows Process Activation Service' do
-  describe service('WAS') do
-    # comment slow command
-    it { should be_running }
-  end
-end
-context 'environment' do
-  # note non-standard syntax
-  describe windows_registry_key("HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment") do
-    let(:value_check) do
-      { :name  => 'Path',
-        :type  => :type_string,
-        :value => '' 
-     }
-    end
-    it { should exist }
-    it { should have_property('Path', :type_string) }
-    it { should have_property_value( 'PROCESSOR_ARCHITECTURE', :type_string_converted, 'x86' ) }
-
-    xit { should have_property_value( 'TEST', :type_string_converted, 'c:\windows' ) }
-    # 
-    xit { should have_property_valuecontaining( 'Path', :type_string_converted, 'c:\\\\windows' ) }
-    # expected Windows registry key "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" to respond to `has_property_valuecontaining?`
-    it { should have_property_value( 'Path', :type_string_converted, 'c:\\\\windows' ) }
-  end
-end
-
-
