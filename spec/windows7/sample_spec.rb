551f5781f55e0a17b47cd0ff26c24c84efeed840
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-09-10 20:50:19 -0400

    intermediate revision
    added netstat example

--- spec/windows7/sample_spec.rb
+++ spec/windows7/sample_spec.rb
@@ -591,6 +591,29 @@ context 'Windows Process Activation Service' do
     # comment slow command
     it { should be_running }
   end
+  
+
+context 'Inspecting Netstat' do
+
+  describe command (<<-EOF 
+$netstat_output = invoke-expression -command "cmd.exe /c netstat -ano -p TCP" ;
+$connected_port = 1521
+$oracle_port_listening_pid = (
+$netstat_output |
+  where-object  { $_ -match ":${connected_port}" } | 
+    select-object -first 1 | 
+      foreach-object { $fields = $_.split(" ") ; write-output ('{0}' -f $fields[-1]) })
+
+$oracle_port_listening_pid
+$oracle_port_listening_process = get-CIMInstance win32_process | where-object { $_.Processid -eq  $oracle_port_listening_pid}
+
+write-output  $oracle_port_listening_process.commandLine
+
+EOF
+) do
+    its(:stdout) { should match /TNSLSNR/io }
+  end
+
 end
 
 
