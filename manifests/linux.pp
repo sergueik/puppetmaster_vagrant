f757fe2a497211d3ca95e2cce27df926c8896a7c
sergueik <kouzmine_serguei@yahoo.com>
2015-09-15 23:36:34 -0400

    intermediate revision added misc. Perl Net modules

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -12,20 +12,39 @@ node 'default' {
       provider => 'rpm',
       source   => '/vagrant/modules/rpm/files/perl-XML-Simple-2.18-6.el6.noarch.rpm',
   }
+ 
+  # Net::Ping is with Perl 5.10
+
+  package { 'perl-Net-Netmask':
+      ensure   => '1.9015-8.el6',
+      provider => 'rpm',
+      source   => '/vagrant/modules/rpm/files/perl-Net-Netmask-1.9015-8.el6.noarch.rpm',
+  }
+  
+  # Net::hostent is with Perl 5.10
+
+  package { 'perl-Data-Validate-IP':
+      ensure => '0.10-1.el6.rf',
+      provider => 'rpm',
+      source   => '/vagrant/modules/rpm/files/perl-Data-Validate-IP-0.10-1.el6.rf.noarch.rpm',
+  }
   
   package { 'perl-Time-HiRes':
     ensure   => '1.9726-1',
     provider => 'rpm',
     source   => '/vagrant/modules/rpm/files/perl-Time-HiRes-1.9726-1.x86_64.rpm',
   }
+
   package { 'perl-IPC-ShareLite':
     ensure   => '0.17-1',
     provider => 'rpm',
     source   => '/vagrant/modules/rpm/files/perl-IPC-ShareLite-0.17-1.x86_64.rpm'
   }
+
   package { 'mailcap':
     ensure => 'present',
   } ->
+
   package { 'perl-Compress-Raw-Zlib':
     ensure          => '2.021-141.el6',
     provider        => 'rpm',
@@ -33,6 +52,7 @@ node 'default' {
     install_options => '--nodeps',
     # perl = 4:5.10.1-141.el6 is needed by perl-Compress-Raw-Zlib-1:2.021-141.el6.x86_64
   } ->
+
   package { 'perl-IO-Compress-Zlib':
     ensure          => '2.021-141.el6',
     provider        => 'rpm',
@@ -40,6 +60,7 @@ node 'default' {
     install_options => '--nodeps',
     # perl = 4:5.10.1-141.el6 is needed by perl-IO-Compress-Zlib-0:2.021-141.el6.x86_64
   } ->
+
   package { 'perl-IO-Compress-Base':
     ensure          => '2.021-141.el6',
     provider        => 'rpm',
@@ -47,6 +68,7 @@ node 'default' {
     install_options => '--nodeps',
   # perl = 4:5.10.1-141.el6 is needed by perl-IO-Compress-Base-0:2.021-141.el6.x86_64
   } ->
+
   package { 'perl-Compress-Zlib':
     ensure          => '2.021-141.el6',
     provider        => 'rpm',
@@ -54,21 +76,25 @@ node 'default' {
     install_options => '--nodeps',
     # perl = 4:5.10.1-141.el6 is needed by perl-Compress-Zlib-0:2.021-141.el6.x86_64
   } ->
+
   package { 'perl-URI':
     ensure   => '1.40-2.el6',
     provider => 'rpm',
     source   => '/vagrant/modules/rpm/files/perl-URI-1.40-2.el6.noarch.rpm'
   } ->
+
   package { 'perl-HTML-Tagset':
     ensure   => '3.20-4.el6',
     provider => 'rpm',
     source   => '/vagrant/modules/rpm/files/perl-HTML-Tagset-3.20-4.el6.noarch.rpm'
   } ->
+
   package { 'perl-HTML-Parser':
     ensure   => '3.64-2.el6',
     provider => 'rpm',
     source   => '/vagrant/modules/rpm/files/perl-HTML-Parser-3.64-2.el6.x86_64.rpm'
   } ->
+
   package { 'perl-libwww-perl':
     ensure   => '5.833-2.el6',
     provider => 'rpm',
@@ -79,6 +105,7 @@ node 'default' {
     provider => 'rpm',
     source   => '/vagrant/modules/rpm/files/perl-XML-Parser-2.44-1.x86_64.rpm'
   } ->
+
   package { 'perl-XML-XPath':
     ensure   => '1.13-1',
     provider => 'rpm',
