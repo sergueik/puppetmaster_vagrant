a8f4ad7a635c4f0ca9275cef35d843fc00ac91fe
sergueik <kouzmine_serguei@yahoo.com>
2015-09-16 22:47:13 -0400

    intermediate revision - parsing hiera shallow nested platrofm_specifi_paramsi - empty value bugfix

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -2,13 +2,13 @@
 # vi: set ft=puppet :
 
 node 'default' { 
-  $platform_specific_params_string = "a:'1' b:'2' c:'3'"
+  $platform_specific_params_string = "a.b.c:'1' b:'2' c:'3' d:''"
 
-  $dummy = regsubst(regsubst($platform_specific_params_string, " +", ',', 'G'), "([^,:]+):'([^']+)'", '"\1":"\2"', 'G')
+  $dummy = regsubst(regsubst($platform_specific_params_string, " +", ',', 'G'), "([^,:]+):'([^']*)'", '"\1":"\2"', 'G')
   
   $platform_specific_params = parsejson("{ ${dummy} }")
   notify {'platform_specific_params test':
-    message => inline_template("<% @platform_specific_params.each do |key,val| -%> <%= key -%> = <%= val -%><% end -%>")
+    message => inline_template('<% @platform_specific_params.each do |key,val| -%> <%= key -%> = <%= val -%><% end -%>')
   }
   # NOTE:
   # rpm -q --requires '<rpm syntax>'
