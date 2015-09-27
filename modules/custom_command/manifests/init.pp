969a998f9ac5cb3c37a9638bd4266527836a2454
sergueik <kouzmine_serguei@yahoo.com>
2015-09-26 20:56:10 -0400

    final revision - run scheduled task through powershell

--- modules/custom_command/manifests/init.pp
+++ modules/custom_command/manifests/init.pp
@@ -5,39 +5,36 @@ class custom_command(
   $enable  = $custom_command::params::enable,
   $config  = $custom_command::params::config,
   $title,
+  $run_command = 'notepad.exe',
+  $script_name = 'manage_scheduled_task',
   $version = $custom_command::params::version
 )  inherits custom_command::params { 
   # Validate install parameters.
   validate_bool($enable)
   validate_string($config)
+  validate_string($script_name)
+  validate_string($title)
   validate_re($version, '^\d+\.\d+\.\d+(-\d+)*$') 
-  $script_name = 'manage_scheduled_task' 
+  # $script_name = 'manage_scheduled_task' 
   $script_path = "c:\\temp\\${script_name}.ps1"
-  $spoon_command = 'run base,spoonbrew/selenium-grid'
-  $run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' ${spoon_command}"
+  # $spoon_command = 'run base,spoonbrew/selenium-grid'
+  # $run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' ${spoon_command}"
   $taskname = 'Launch_selenium_grid_node'
-  # Write powershell script
-  # notify { 'Write powershell script':} ->
+  notify { "Write powershell script ${script_name}.ps1":} ->
  
-  file { 'launcher.ps1':
+  file { "${title} launcher script":
     ensure             => file,
     name               => $script_name,
     path               => $script_path,
     content            => template("custom_command/${script_name}.erb"),
     source_permissions => ignore,
- #   notify             => Exec["Execute script that will create and run scheduled task ${title}"],
   } -> 
 
-  # notify { 'Execute script that will create and run scheduled task':} ->
-  # Execute script that will create and run scheduled task
   exec { "Execute script that will create and run scheduled task ${title}": 
     path    => 'C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32',
     command => "powershell -executionpolicy remotesigned -file ${script_path}",
-    require  => File['launcher.ps1'],
-#    provider => powershell,
-#    refreshonly => true
+    require  => File[ "${title} launcher script"],
   } ->
 
   notify { "Done ${title}.":}
-  # Execute script that will create and run scheduled task
 }
