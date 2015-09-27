5c82b1f2f6f1541d8949907c399e7fa3083a201d
sergueik <kouzmine_serguei@yahoo.com>
2015-09-27 00:45:12 -0400

    intermediate revision - converted to defined type

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -2,15 +2,26 @@
 # vi: set ft=puppet :
 
 node 'windows7' {
-  include wait_for
-  $_name = 'test application'
-  # $my_file_arg = 'c:/temp/cleanup.cmd'
-  # include 'mywebsite'
- # include 'custom_command'
-  class { 'custom_command': 
-    title   => 'Launch_selenium_grid_node',
-    enable  => true,
-    config  => 'unused',
-    version => '0.1.0'
+#  include wait_for
+  # include 'custom_command'
+  # class { 'custom_command': 
+  #  title   => 'Launch_selenium_grid_node',
+  #  enable  => true,
+  #  config  => 'unused',
+  #  version => '0.1.0'
+  #}
+  $spoon_command = 'run base,spoonbrew/selenium-grid'
+  $run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' ${spoon_command}"
+
+  custom_command { 'Launch nodepad':
+    command => 'notepad.exe',
+    script  => 'launch_notepad',
+    wait    => true,
+  } ->
+
+  custom_command { 'Run spoon selenium grid':
+    command => $run_command,
+    script  => 'launch_spoon',
+    wait    => true,
   }
 }
--- modules/custom_command/manifests/init.pp
+++ modules/custom_command/manifests/init.pp
@@ -1,40 +1,41 @@
 # -*- mode: puppet -*-
 # vi: set ft=puppet :
 
-class custom_command(
-  $enable  = $custom_command::params::enable,
-  $config  = $custom_command::params::config,
-  $title,
-  $run_command = 'notepad.exe',
-  $script_name = 'manage_scheduled_task',
-  $version = $custom_command::params::version
-)  inherits custom_command::params { 
+define custom_command(
+  $wait        = true,
+  $command     = 'notepad.exe',
+  $script      = 'manage_scheduled_task',
+  $version     = '0.2.0'
+)   { 
   # Validate install parameters.
-  validate_bool($enable)
-  validate_string($config)
-  validate_string($script_name)
-  validate_string($title)
+  validate_bool($wait)
+  validate_string($script)
+  validate_string($command)
   validate_re($version, '^\d+\.\d+\.\d+(-\d+)*$') 
-  # $script_name = 'manage_scheduled_task' 
-  $script_path = "c:\\temp\\${script_name}.ps1"
-  # $spoon_command = 'run base,spoonbrew/selenium-grid'
-  # $run_command = "'C:\\Program Files\\Spoon\\Cmd\\spoon.exe' ${spoon_command}"
-  $taskname = 'Launch_selenium_grid_node'
-  notify { "Write powershell script ${script_name}.ps1":} ->
+  $script_path = "c:\\temp\\${script}.ps1"
+  $random = fqdn_rand(1000,$::uptime_seconds)
+  $log = "c:\\temp\\${script}${random}.log"
+  $taskname = regsubst($name, " +", '_', 'G') # 'Launch_selenium_grid_node'
+  notify { "Write powershell launcher script for ${name}":} ->
+  file { "${name} launcher log":
+    name               => "${script}${random}.log",
+    path               => $log,
+    ensure             => absent,
+    source_permissions => ignore,
+  } -> 
  
-  file { "${title} launcher script":
+  file { "${name} launcher script":
     ensure             => file,
-    name               => $script_name,
     path               => $script_path,
-    content            => template("custom_command/${script_name}.erb"),
+    content            => template('custom_command/manage_scheduled_task.erb'),
     source_permissions => ignore,
   } -> 
 
-  exec { "Execute script that will create and run scheduled task ${title}": 
+  exec { "Execute script that will create and run scheduled task ${name}": 
     path    => 'C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32',
     command => "powershell -executionpolicy remotesigned -file ${script_path}",
-    require  => File[ "${title} launcher script"],
+    require  => File[ "${name} launcher script"],
   } ->
 
-  notify { "Done ${title}.":}
+  notify { "Done ${name}.":}
 }
