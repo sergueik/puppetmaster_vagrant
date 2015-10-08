0c1155900eb9373dd42163f0530634d410c496ff
sergueik <kouzmine_serguei@yahoo.com>
2015-10-08 17:18:40 -0400

    interediat revision - fix formatting corruption

--- remove_from_environment.ps1
+++ remove_from_environment.ps1
@@ -1,31 +1,29 @@
-# Remove  Application path from the MACHINE PATH environent
+$application_path  = '<%= @application_path  -%>'
+if ($application_path - eq '') { 
+  $application_path = 'C:\Program Files\Spoon\Cmd'
+}
 
-current_path_environment  = [environment]::GetEnvironmentVariable(PATH',,[System.EnvironmentVariableTarget]::Machine)
-application_path  = C:\Program Files\Spoon\Cmd'
+$current_path_environment = [Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::Machine )
 
-
-if (urrent_path_environment.T.ToLower().Contains(pplication_path.T.ToLower())) {
-  Write-Host ACHINE PATH environment variable has '${application_path}' - removing..."
-
-  ew_path_array = = @()
-  ath_separator = = '
-
-  urrent_path_environment - -split ath_separator | | ForEach-Object {
-    ath = = 
-
-    if (ath - -ne  - -and (-not (ath.T.ToLower().Contains(pplication_path.T.ToLower())))) {
-      ew_path_array + += ath
-
-    }
+if ($current_path_environment.ToLower().Contains($application_path.ToLower())) {
+  write-output ('Removing  Application path "{0}" from the MACHINE PATH environent' -f  $application_path )
+  $removed = $false
+  $new_path_array = @()
+  $path_separator = ';'
+  $current_path_environment -split $path_separator | foreach-object { 
+    $path = $_
+     if ( $path -ne '' -and ( -not ($path.ToLower().Contains($application_path.ToLower())))){ 
+     $new_path_array += $path 
+  }  else { 
+     $removed = $false
+  }
+}
+  if ($removed) { 
+    Write-Host 'Removed.'
   }
 
-  [environment]::SetEnvironmentVariable(TH', (,(w_path_array -j -join th_separator),[),[System.EnvironmentVariableTarget]::Machine)
+[environment]::SetEnvironmentVariable('PATH', ($new_path_array -join $path_separator),[System.EnvironmentVariableTarget]::Machine)
 
-
-
-} else {
-  Write-Host INE PATH environment variable is clean: `n${current_path_environment}"
+} else { 
+  Write-Host ( 'MACHINE PATH environment doew not include "{0}". ' -f $application_path )
 }
-
-}
-
