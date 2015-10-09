71e88b6503374b0b8f7e4fb8798c35b4add0b2be
sergueik <kouzmine_serguei@yahoo.com>
2015-10-09 19:41:43 -0400

    intermediate commit - extracted service manip scripts

---
+++ modules/exec_service_control/README.md
@@ -0,0 +1,3 @@
+Info
+----
+This module contains Powershell script templates to stop and uninstall service. It may be useful in a situation service resource is already declared for creation
---
+++ modules/exec_service_control/manifests/init.pp
@@ -0,0 +1,33 @@
+# -*- mode: puppet -*-
+# vi: set ft=puppet :
+
+define exec_service_control(
+  $service_name = $title, # e.g. 'aspnet_state'
+  $version      = '0.1.0'
+)   { 
+  # Validate install parameters.
+  validate_string($service_name)
+  validate_re($version, '^\d+\.\d+\.\d+(-\d+)*$') 
+  $random = fqdn_rand(1000,$::uptime_seconds)
+  $taskname = regsubst($title, "[$/\\|:, ]", '_', 'G')
+  $log_dir = "c:\\temp\\${taskname}"
+  $log = "${log_dir}\\${task_name}.${random}.log"
+
+  exec {"${title} stopping service: '${service_name}'":
+    command   =>  template("exec_service_control/stop_service_ps1.erb"),
+    cwd       => 'c:\windows\temp',
+    logoutput => true,
+    onlyif    =>  template("exec_service_control/query_service_ps1.erb"), 
+    path      => 'C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32',
+    provider  => 'powershell',
+  } ->
+
+  exec {"${title} deleting service: '${service_name}'":
+    command   =>  template("exec_service_control/delete_service_ps1.erb"),
+    cwd       => 'c:\windows\temp',
+    logoutput => true,
+    onlyif    =>  template("exec_service_control/query_service_ps1.erb"),
+    path      => 'C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32',
+    provider  => 'powershell',
+  }  
+}
