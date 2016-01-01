9d950d4ad78bdcb326892941574a650ecbf843a6
sergueik <kouzmine_serguei@yahoo.com>
2016-01-01 15:10:09 -0500

    intermediate revision - second part of tweaks

--- config.cmd
+++ config.cmd
@@ -16,13 +16,29 @@ call winrm.cmd set winrm/config @{MaxTimeoutms="1800000"}
 call winrm.cmd set winrm/config/client/auth @{Basic="true"}
 call winrm.cmd set winrm/config/service @{AllowUnencrypted="true"}
 call winrm.cmd set winrm/config/service/auth @{Basic="true"}
+
+
 REM Alternative commands:
 
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value True; Set-Item WSMan:\localhost\Service\Auth\Basic -Value True"
 sc.exe triggerinfo winrm start/networkon stop/networkoff
-goto :EOF
 
+
+goto :EOF
+REM further tweaks:
 REM for Windows 10 
 REM uninitialized constant WinRM::FS::Core::CommandExecutor::WinRMUploadError
 REM see https://github.com/mitchellh/vagrant/issues/6060
 REM need to update the winrmm-fs vagrant plugin to version 0.2.3 or later
+
+REM Finally to unblock creation of local resources on the guest 
+REM Directory of c:\vagrant-chef\465fb3b29b23535e31953e1920ccd88e
+REM
+REM 01/01/2016  11:30 AM    ^<DIR^>          .
+REM 01/01/2016  11:30 AM    ^<DIR^>          ..
+REM 01/01/2016  11:30 AM    ^<SYMLINKD^>     cookbooks [\\vboxsrv\v-csc-e48252ec5]
+REM apply configurarion described in https://github.com/mwrock/boxstarter/blob/master/Boxstarter.WinConfig
+REM and https://gist.github.com/novascreen/10d5d891fc04ecfd758b
+REM It can be done remotely through Vagrant shell provisioner, which will work.
+REM Without this change, chef-solo and Puppet will fail with
+REM Shared folders that Chef requires are missing on the virtual machine.
