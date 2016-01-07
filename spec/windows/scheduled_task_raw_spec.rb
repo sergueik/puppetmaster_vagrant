94e2afb63cc9ee133aff6b03439be03bf698a4d0
sergueik <kouzmine_serguei@yahoo.com>
2016-01-07 12:59:46 -0500

    intermediate commit - draft of raw scheduled task expectations before they go away

---
+++ spec/windows/scheduled_task_raw_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../windows_spec_helper' 
+context 'Scheduled Tasks' do
+  version = '0.0.0.14'
+  context 'Application Task Scheduler configuration' do
+    describe file('C:/Programdata/LogRotate_scheduled_task.xml') do
+      it { should exist }
+      it { should be_file }
+      ['<Command>"C:\\\\Program Files \\"</Command>','<Arguments>"C:\\\\Program Files \\(x86\\)\\\\LogRotate\\\\Content\\\\sample.conf"</Arguments>','<WorkingDirectory>c:\\\\windows\\\\temp</WorkingDirectory>'].each do |line| 
+        it { should contain /#{Regexp.new(line)}/i }
+      end 
+      it { should contain '<Task xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task" version="1.3">' }
+      it { should contain '<UserId>S-1-5-18</UserId>' }
+
+    end
+  end
+
+  context 'Application Task Scheduler' do
+    name = 'LogRotate' 
+    describe command(<<-EOF
+schtasks.exe /Query /TN #{name} /xml
+EOF
+    ) do
+      its(:exit_status) {should eq 0 }
+      ['<Command>"C:\\\\Program Files \\(x86\\)\\\\LogRotate\\\\logrotate.exe"</Command>','<Arguments>"C:\\\\Program Files \\(x86\\)\\\\LogRotate\\\\Content\\\\sample.conf"</Arguments>','<WorkingDirectory>c:\\\\windows\\\\temp</WorkingDirectory>'].each do |line| 
+        its(:stdout) {should match /#{Regexp.new(line)}/i }
+      end 
+    end
+  end
+end
