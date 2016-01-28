946bc5a37def148329a5ca206e6c261b282cf832
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-01-28 13:05:00 -0500

    intermediate revision - refactoring

--- spec/windows/eventlog_spec.rb
+++ spec/windows/eventlog_spec.rb
@@ -18,4 +18,22 @@ EOF
   ) do
     its(:stdout) { should match /Shutdown Type:/io } 
   end
+  
+    event_log_id = 10001
+    log_name = 'Application'
+    source = 'RestartManager'
+    message = 'Ending session'
+    describe command(<<-EOF
+$event_log_id = '#{event_log_id}'
+$log_name = '#{log_name}'
+get-winevent -FilterHashTable @{LogName=$log_name; ID=$event_log_id; } -MaxEvents 10  |
+sort-object TimeCreated -descending |
+select-object -first 1 |
+select-object -property Message |
+format-list
+EOF
+  ) do
+      its(:stdout) { should match /#{message}/io }
+    end
+  
 end
