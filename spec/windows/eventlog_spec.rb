d23d6e79e5305d13d55b564f37247d2a11e244c9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-01-28 10:09:19 -0500

    intermediate revision - added the real
    RestartManager eventlog sample info

--- spec/windows/eventlog_spec.rb
+++ spec/windows/eventlog_spec.rb
@@ -1,4 +1,11 @@
 context 'Event Log' do # http://ss64.com/ps/get-winevent.html
+  # Log Name : Application
+  # Source: RestartManager
+  # EventID: 10001
+  # Text: Ending session 0 started 2016-01-28T14:44:21.217074400Z.
+  # 
+  # EventID: 10000
+  # Text: Starting session 0 - 2016-01-28T14:41:49.061645600Z.
   event_log_id = 1074
   describe command(<<-EOF
 $Id = '#{event_log_id}'
