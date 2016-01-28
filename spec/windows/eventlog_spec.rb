200b855ce831bfd1dcd6589a72293fbb571039b7
sergueik <kouzmine_serguei@yahoo.com>
2016-01-28 14:13:52 -0500

    interediate revision - allow multiple event log tests to be collapsed together

--- spec/windows/eventlog_spec.rb
+++ spec/windows/eventlog_spec.rb
@@ -1,28 +1,24 @@
 context 'Event Log' do # http://ss64.com/ps/get-winevent.html
-  # Log Name : Application
-  # Source: RestartManager
-  # EventID: 10001
-  # Text: Ending session 0 started 2016-01-28T14:44:21.217074400Z.
-  # 
-  # EventID: 10000
-  # Text: Starting session 0 - 2016-01-28T14:41:49.061645600Z.
-  event_log_id = 1074
-  describe command(<<-EOF
-$Id = '#{event_log_id}'
-get-winevent -FilterHashTable @{LogName='System'; ID=$Id; } -MaxEvents 10  |
-sort-object TimeCreated -descending | 
-select-object -first 1 | 
-select-object -property Message | 
-format-list
-EOF
-  ) do
-    its(:stdout) { should match /Shutdown Type:/io } 
-  end
-  
-    event_log_id = 10001
-    log_name = 'Application'
-    source = 'RestartManager'
-    message = 'Ending session'
+[
+{ 
+    :event_log_id => 10001,
+    :log_name => 'Application',
+    :source => 'RestartManager',
+    :message => 'Ending session'
+
+},
+{ 
+    :event_log_id => 1074,
+    :log_name => 'System',
+    :source => 'RestartManager',
+    :message => 'Shutdown Type:'
+
+}
+].each do |row|
+    event_log_id = row[:event_log_id]
+    log_name = row[:log_name]
+    source = row[:source]
+    message = row[:message]
     describe command(<<-EOF
 $event_log_id = '#{event_log_id}'
 $log_name = '#{log_name}'
@@ -35,5 +31,4 @@ EOF
   ) do
       its(:stdout) { should match /#{message}/io }
     end
-  
 end
