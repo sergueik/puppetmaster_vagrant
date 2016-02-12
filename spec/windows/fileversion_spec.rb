db72e7fb07112b6ee99a74a8f62159abf555520e
sergueik <kouzmine_serguei@yahoo.com>
2016-02-12 11:40:52 -0500

    intermediate revision - fixed typo

--- spec/windows/fileversion_spec.rb
+++ spec/windows/fileversion_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../windows_spec_helper'
 
-context 'File version' do
+context 'File version' do # example of handling the convertto_json format
   {
    'c:\windows\system32\notepad.exe' => '6.1.7600.16385',
    'c:/programdata/chocolatey/choco.exe' =>  '0.9.9.11',
@@ -24,8 +24,7 @@ EOF
         # x.gsub(/[abc]/,"\\#{$&}")  # => "(\\a\\a\\a)"
         # x.gsub(/(a|b|c)/,"\\#{$&}") # => "(\\c\\c\\c)"
 
-
-        should match Regexp.new('"FileName":  "' + file_path.gsub('(','\\(').gsub(')','\\)').gsub('/','\\').gsub(/\\/,'\\\\\\\\\\\\\\\\') + '"', Regexp::IGNORECASE)
+        should match Regexp.new('"FileName":  "' + file_path.gsub('/','\\').gsub(/\\/,'\\\\\\\\\\\\\\\\').gsub('(','\\(').gsub(')','\\)') + '"', Regexp::IGNORECASE)
         should match /"ProductVersion":  "#{file_version}"/
       end
     end 
@@ -36,15 +35,14 @@ EOF
 end
 
 
-context 'File version Powershell 2.0' do
+context 'File version Powershell 2.0' do # Powershell 2.0 lacks convertto-json cmdlet
   {
    'C:\Program Files (x86)\Columbo\Columbo.exe' =>  '1.1.1.0',
   }.each do |file_path, file_version|
     describe command(<<-EOF
     $file_path = '#{file_path}'
       try {
-        $info = get-item -path $file_path
-        # Powershell 2.0 lacks convertto-json cmdlet
+        $info = get-item -path $file_path        
         write-output ($info.VersionInfo | format-list)
       } catch [Exception]  { 
         write-output 'Error reading file'
@@ -52,7 +50,7 @@ context 'File version Powershell 2.0' do
     EOF
     ) do
         its(:stdout) do
-          should match /ProductVersion +: +#{file_version}"/
+          should match /ProductVersion +: +#{file_version}/
         end
       end 
     end 
