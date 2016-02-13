db25dc91a261ec7f2b0ca8b23e509933e3ec5b18
sergueik <kouzmine_serguei@yahoo.com>
2016-02-12 19:58:16 -0500

    intermediate revision -  fix of specinfra's 'find_installed_application.ps1'

--- spec/windows/installed_application_spec.rb
+++ spec/windows/installed_application_spec.rb
@@ -1,93 +1,62 @@
 require_relative '../windows_spec_helper'
 
 context 'Columbo' do
+  # fix of
+  # https://github.com/sergueik/specinfra/blob/master/lib/specinfra/backend/powershell/support/find_installed_application.ps1
+  # to support non-standard application names like 'Foo [Bar]'
   context 'Installed Application' do
   {
    'Columbo [SRV]' => '1.1.2',
   }.each do |appName, appVersion|
+
     describe command(<<-EOF
-# fix to 
-# to support non-standard application names like 'Foo [Bar]'
-# https://github.com/sergueik/specinfra/blob/master/lib/specinfra/backend/powershell/support/find_installed_application.ps1
 function FindInstalledApplication {
-  param($appName, $appVersion)
-    write-host ('appName  = "{0}"' -f $appName )
-    start-sleep 1
+  param($appName,$appVersion)
+  # Write-Host ('appName  = "{0}", appVersion={1}' -f $appName,$appVersion)
+  $appNameRegex = New-Object Regex (($appName -replace '\\[','\\[' -replace '\\]','\\]'))
 
   if ((Get-WmiObject win32_operatingsystem).OSArchitecture -notmatch '64')
   {
-    $keys= (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*')
-    $possible_path= 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*'
+    $keys = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*')
+    $possible_path = 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*'
     if (Test-Path $possible_path)
     {
-      $keys+= (Get-ItemProperty $possible_path)
+      $keys += (Get-ItemProperty $possible_path)
     }
   }
-    else
+  else
   {
     $keys = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*','HKLM:\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*')
-    $possible_path= 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*'
+    $possible_path = 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*'
     if (Test-Path $possible_path)
     {
-      $keys+= (Get-ItemProperty $possible_path)
+      $keys += (Get-ItemProperty $possible_path)
     }
-    $possible_path= 'HKCU:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*'
+    $possible_path = 'HKCU:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\*'
     if (Test-Path $possible_path)
     {
-      $keys+= (Get-ItemProperty $possible_path)
+      $keys += (Get-ItemProperty $possible_path)
     }
   }
-
-    $keys | foreach-object { write-host $_ }
 
   if ($appVersion -eq $null) {
-    write-host ('appName  = "{0}"' -f $appName )
-start-sleep 1
-$appNameRegex = new-object Regex(($appName -replace '\\[', '\\[' -replace '\\]', '\\]'))
-
-$keys | foreach-Object {
-write-host ('DisplayName = "{0}"' -f $_.DisplayName)
-write-host ('PSChildName = "{0}"' -f $_.PSChildName)
-if ($appNameRegex.ismatch($_.DisplayName) -or $appNameRegex.ismatch($_.PSChildName) ){
-write-host 'x'
-}
- } 
-    @($keys | Where-Object {$appNameRegex.ismatch($_.DisplayName) -or $appNameRegex.ismatch($_.PSChildName)}).Length -gt 0
-
+    @( $keys | Where-Object { $appNameRegex.ismatch($_.DisplayName) -or $appNameRegex.ismatch($_.PSChildName) }).Length -gt 0
   }
-  else{
-    write-host ('appName  = "{0}", appVersion={1}' -f $appName,$appVersion )
-start-sleep 1
-$appNameRegex = new-object Regex(($appName -replace '\\[', '\\[' -replace '\\]', '\\]'))
-
-$keys | foreach-Object {
-write-host ('DisplayName = "{0}"' -f $_.DisplayName)
-write-host ('PSChildName = "{0}"' -f $_.PSChildName)
-if ($appNameRegex.ismatch($_.DisplayName) -or $appNameRegex.ismatch($_.PSChildName) ){
-write-host 'x'
-}
- } 
-
-
-    @($keys | Where-Object {$appNameRegex.ismatch($_.DisplayName) -or $appNameRegex.ismatch($_.PSChildName) } | Where-Object {$_.DisplayVersion -eq $appVersion} ).Length -gt 0
-
+  else {
+    @( $keys | Where-Object { $appNameRegex.ismatch($_.DisplayName) -or $appNameRegex.ismatch($_.PSChildName) } | Where-Object { $_.DisplayVersion -eq $appVersion }).Length -gt 0
   }
-
 }
-
-
 
 $exitCode = 1
 $ProgressPreference = 'SilentlyContinue'
 try {
   $success = ((FindInstalledApplication -appName '#{appName}' -appVersion '#{appVersion}') -eq $true)
-  if ($success -is [Boolean] -and $success) { 
-  $exitCode = 0 }   
-  } catch {
+  if ($success -is [boolean] -and $success) {
+    $exitCode = 0 }
+} catch {
   Write-Output $_.Exception.Message
 }
-Write-Output "Exiting with code: $exitCode"
-
+Write-Output 'Exiting with code: $exitCode'
     EOF
     ) do
         its(:stdout) do
