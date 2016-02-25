716db8ed0fac3348adfb877f71e5107ff1e3dc74
sergueik <kouzmine_serguei@yahoo.com>
2016-02-24 20:16:50 -0500

    intermediate revision - fixing bad ascii detector to jump to first zero byte if any

--- spec/windows/broken_encoding_spec.rb
+++ spec/windows/broken_encoding_spec.rb
@@ -17,26 +17,46 @@ function test_bomless_file {
     return
   }
 
-  $byte_content = (Get-Content -Path $file_path -Encoding byte -ReadCount $byte_content_size -TotalCount $byte_content_size)
-  $byte_count = $byte_content.Count
+  # http://stackoverflow.com/questions/2980182/binary-file-to-string
+  # $byte_content = (Get-Content -Path $file_path -Encoding byte -ReadCount $byte_content_size -TotalCount $byte_content_size)
+  # $byte_count = $byte_content.Count
+
+
+  [System.IO.FileStream]$content = New-Object System.IO.FileStream ($file_path,[System.IO.FileMode]::Open)
+  #  Cannot find an overload for "FileStream" and the argument count:3
+  # , [System.IO.FileAccess]::CanRead )
+  [byte[]]$byte_content = (New-Object System.IO.BinaryReader ($content)).ReadBytes([convert]::ToInt32($content.Length))
+  $content.Close()
+  $offset = 0
+  for ([int]$cnt = 0; $cnt -ne $byte_content.Length; $cnt++) { if ($byte_content[$cnt] -eq 0) {
+      if ($offset -eq 0) {
+        $offset = $cnt + 1
+        Write-Output $cnt
+      } } }
+
+
+  $byte_count = $byte_content.Length - $offset
+  if ($byte_count -gt $byte_content_size) {
+    $byte_count = $byte_content_size
+  }
   [bool]$high_ascii_byte_count = $false
 
-  # big endian Unicode - even bytes will be 0
+  # check if big endian Unicode first - even-numbered index bytes will be 0)
   $zero_byte_count = 0
   for ($i = 0; $i -lt $byte_count; $i += 2) {
-    if ($byte_content[$i] -eq 0) { $zero_byte_count++ }
-    if ($byte_content[$i] -gt 127) { $high_ascii_byte_count = $true }
+    if ($byte_content[$i + $offset] -eq 0) { $zero_byte_count++ }
+    if ($byte_content[$i + $offset] -gt 127) { $high_ascii_byte_count = $true }
   }
   if (($zero_byte_count / ($byte_count / 2)) -ge $unicode_threshold) {
     # big-endian Unicode with no BOM
     New-Object (System.Text.UnicodeEncoding $true,$false)
     return
   }
-  # little endian Unicode - odd bytes will be 0
+  # check if little endian Unicode next - odd-numbered index bytes will be 0)
   $zero_byte_count = 0
   for ($i = 1; $i -lt $byte_count; $i += 2) {
-    if ($byte_content[$i] -eq 0) { $zero_byte_count++ }
-    if ($byte_content[$i] -gt 127) { $high_ascii_byte_count = $true }
+    if ($byte_content[$i + $offset] -eq 0) { $zero_byte_count++ }
+    if ($byte_content[$i + $offset] -gt 127) { $high_ascii_byte_count = $true }
   }
   if (($zero_byte_count / ($byte_count / 2)) -ge $unicode_threshold) {
     # little-endian Unicode with no BOM
@@ -44,11 +64,11 @@ function test_bomless_file {
 
   }
 
-  #  UTF8 - too many 
+  #  UTF8 with no BOM
   if ($high_ascii_byte_count -eq $true) {
     return (New-Object System.Text.UTF8Encoding $false)
   } else {
-    # ASCII 
+    # if made it this far, I'm calling it ASCII; done deal pal  
     return [System.Text.Encoding]::'ASCII'
   }
 
