df5f726faefde95abee31858f89beac0302fce22
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-02-26 14:05:07 -0500

    intermediate revision - refactoring the spec tests

--- spec/windows/facter_sample2_spec.rb
+++ spec/windows/facter_sample2_spec.rb
@@ -4,53 +4,37 @@ context 'Execute Facter Fact in Puppet Agent Ruby Environment' do
   filename = 'C:/Program Files/SplunkUniversalForwarder/bin/splunk.exe'
   context 'With LOAD_PATH' do
     lines = [ 
-      'answer: 42',
-    #  'FileVersion  6.3.1',
-      '"C:\Program Files\SplunkUniversalForwarder\bin\splunkd.exe" service'
+      'C:/Program Files/SplunkUniversalForwarder/bin/splunkd.exe'
     ]
     puppet_home = 'C:/Program Files/Puppet Labs/Puppet'
     rubylib = "#{puppet_home}/facter/lib;#{puppet_home}/hiera/lib;#{puppet_home}/puppet/lib;"
     rubyopt = 'rubygems' 
     script_file = 'c:/windows/temp/test.rb'
     ruby_script = <<-EOF
-`$LOAD_PATH.insert(0, '#{puppet_home}/facter/lib')
-`$LOAD_PATH.insert(0, '#{puppet_home}/hiera/lib')
-`$LOAD_PATH.insert(0, '#{puppet_home}/puppet/lib')
-require 'yaml'
-require 'puppet'
-require 'pp'
-require 'win32/registry'
-
-# Do basic smoke test
-puts 'Parse YAML string' 
-pp YAML.load(<<-'END_DATA'
----
-answer: 42
-END_DATA
-)
-
-puts 'Parse YAML string' 
-puts "Generate YAML\\n" +  YAML.dump({'answer'=>42})
-
-  filename = '#{filename}'
+      `$LOAD_PATH.insert(0, '#{puppet_home}/facter/lib')
+      `$LOAD_PATH.insert(0, '#{puppet_home}/hiera/lib')
+      `$LOAD_PATH.insert(0, '#{puppet_home}/puppet/lib')
+      require 'yaml'
+      require 'puppet'
+      require 'pp'
+      require 'win32/registry'
   
-  def hklm_registry_read(key, value)
-    # Always read the registry in 64-bit mode.
-    begin
-      reg = Win32::Registry::HKEY_LOCAL_MACHINE.open(key, Win32::Registry::KEY_READ | 0x0100)
-      rval = reg[value]
-      reg.close
-      rval
-    rescue
-      nil
-    end
-  end
+      def hklm_registry_read(key, value)
+        begin
+          reg = Win32::Registry::HKEY_LOCAL_MACHINE.open(key, Win32::Registry::KEY_READ | 0x0100) # read 64 subkey
+          rval = reg[value]
+          reg.close
+          rval
+        rescue
+          nil
+        end
+      end
   
   def get_install_dir
     hklm_registry_read('SYSTEM\\ControlSet001\\services\\SplunkForwarder', 'ImagePath')
   end
   
-pp get_install_dir
+  pp get_install_dir
 
   EOF
 
@@ -66,9 +50,9 @@ pp get_install_dir
   
     describe command("iex \"ruby.exe '#{script_file}'\"") do
       let(:path) { "#{puppet_home}/sys/ruby/bin" }
-      lines.each do |line| 
+      lines.each do |file_path|
         its(:stdout) do
-          should match  Regexp.new(line.gsub('\\','\\\\').gsub(/[()]/,"\\#{$&}").gsub('[','\[').gsub(']','\]'))
+          should match  Regexp.new(file_path.gsub('/','\\').gsub(/\\/,'\\\\\\\\\\\\\\\\').gsub('(','\\(').gsub(')','\\)'), Regexp::IGNORECASE)
         end
       end
     end
--- spec/windows/facter_sample_spec.rb
+++ spec/windows/facter_sample_spec.rb
@@ -4,8 +4,6 @@ context 'Execute Facter Fact in Puppet Agent Ruby Environment' do
   filename = 'C:/Program Files/SplunkUniversalForwarder/bin/splunk.exe'
   context 'With LOAD_PATH' do
     lines = [ 
-      'answer: 42',
-    #  'FileVersion  6.3.1',
       '6.3.1'
     ]
     puppet_home = 'C:/Program Files/Puppet Labs/Puppet'
@@ -13,45 +11,25 @@ context 'Execute Facter Fact in Puppet Agent Ruby Environment' do
     rubyopt = 'rubygems' 
     script_file = 'c:/windows/temp/test.rb'
     ruby_script = <<-EOF
-`$LOAD_PATH.insert(0, '#{puppet_home}/facter/lib')
-`$LOAD_PATH.insert(0, '#{puppet_home}/hiera/lib')
-`$LOAD_PATH.insert(0, '#{puppet_home}/puppet/lib')
-require 'yaml'
-require 'puppet'
-require 'pp'
-require 'Win32API'
+      `$LOAD_PATH.insert(0, '#{puppet_home}/facter/lib')
+      `$LOAD_PATH.insert(0, '#{puppet_home}/hiera/lib')
+      `$LOAD_PATH.insert(0, '#{puppet_home}/puppet/lib')
+      require 'yaml'
+      require 'puppet'
+      require 'pp'
+      require 'Win32API'
 
-# Do basic smoke test
-puts 'Parse YAML string' 
-pp YAML.load(<<-'END_DATA'
----
-answer: 42
-END_DATA
-)
-
-puts 'Parse YAML string' 
-puts "Generate YAML\\n" +  YAML.dump({'answer'=>42})
-
-  filename = '#{filename}'
-  if File.exists?(filename)
-    s=""
-    vsize=Win32API.new('version.dll', 'GetFileVersionInfoSize', 
-                       ['P', 'P'], 'L').call(filename, s)
-  
-    # If file size is greater than zero, let's try to grab the file version info.
-    if (vsize > 0)
-      result = ' '*vsize
-      Win32API.new('version.dll', 'GetFileVersionInfo', 
-                   ['P', 'L', 'L', 'P'], 'L').call(filename, 0, vsize, result)
-      rstring = result.unpack('v*').map{|s| s.chr if s<256}*''
-      version = /FileVersion..(.*?)\\000/.match(rstring)
-      
-      # Print file product version.
-      puts version
-  end
- end
-
-
+      filename = '#{filename}'
+      if File.exists?(filename)
+        vsize=Win32API.new('version.dll', 'GetFileVersionInfoSize', ['P', 'P'], 'L').call(filename, '')
+        if (vsize > 0)
+          result = ' '*vsize
+          Win32API.new('version.dll', 'GetFileVersionInfo', ['P', 'L', 'L', 'P'], 'L').call(filename, 0, vsize, result)
+          rstring = result.unpack('v*').map{|s| s.chr if s<256}*''
+          version = /FileVersion..(.*?)\\000/.match(rstring)
+          puts version
+        end
+      end
   EOF
 
   before(:each) do
