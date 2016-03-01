092075eeefb6aafda86177a9027a28f0a4336e43
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-02-29 21:02:55 -0500

    interemediate revision -  added uninstall command for custom idempotency solutions

---
+++ modules/custom_command/manifests/exec_uninstall_msiexec.pp
@@ -0,0 +1,35 @@
+# -*- mode: puppet -*-
+# vi: set ft=puppet :
+
+define custom_command::exec_uninstall_msiexec_command(
+  $application_name = $title, # e.g. 'Puppet agent (64-bit)'
+  $version      = '0.1.0'
+)   { 
+  # Validate install parameters.
+  validate_string($service_name)
+  validate_re($version, '^\d+\.\d+\.\d+(-\d+)*$') 
+  $random = fqdn_rand(1000,$::uptime_seconds)
+  $task_name = regsubst($title, "[$/\\|:, ]", '_', 'G')
+  $log_dir = "c:\\temp\\${task_name}"
+  $log = "${log_dir}\\${task_name}.${random}.log"
+
+  exec {"${title} Uninstalling application: '${application_name}'":
+    command   => template('custom_command/uninstall_msiexec_ps1.erb'),
+    cwd       => 'c:\windows\temp',
+    logoutput => true,
+    # declare always
+    # onlyif    => template('custom_command/uninstall_msiexec_ps1.erb'), 
+    path      => 'C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32',
+    provider  => 'powershell',
+  } ->
+  # run again 
+  exec {"${title} Uninstalling application (2nd time): '${application_name}'":
+    command   => template('custom_command/uninstall_msiexec_ps1.erb'),
+    cwd       => 'c:\windows\temp',
+    logoutput => true,
+    # declare always
+    # onlyif    => template('custom_command/uninstall_msiexec_ps1.erb'), 
+    path      => 'C:\Windows\System32\WindowsPowerShell\v1.0;C:\Windows\System32',
+    provider  => 'powershell',
+  }
+}
