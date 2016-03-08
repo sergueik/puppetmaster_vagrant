f6986c659b6c077ae25c42292ad8aab07065a26e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-07 20:39:24 -0500

    initial revision - added trimmed down server spec repo

---
+++ serverspec/lib/serverspec/type/windows_registry_key.rb
@@ -0,0 +1,23 @@
+module Serverspec::Type
+  class WindowsRegistryKey < Base
+    def exists?
+      @runner.check_registry_key_exists(@name)
+    end
+
+    def has_property?(property_name, property_type = :type_string)
+      @runner.check_registry_key_has_property(@name, {:name => property_name, :type => property_type})
+    end
+
+    def has_value?(value)
+      @runner.check_registry_key_has_value(@name, {:name => '', :type => :type_string, :value => value})
+    end
+
+    def has_property_value?(property_name, property_type, value)
+      @runner.check_registry_key_has_value(@name, {:name => property_name, :type => property_type, :value => value})
+    end
+
+    def has_propertyvaluecontaining?(property_name,  value)
+      @runner.check_registry_key_has_propertyvaluecontaining(@name, {:name => property_name, :value => value})
+    end
+  end
+end
