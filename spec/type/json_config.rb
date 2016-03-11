fe2f446e71f52f5f9a2d3a2035b0bf6ada06b429
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-10 23:08:41 -0500

    intermediate revision - refactoring to reduce the backend calls

--- spec/type/json_config.rb
+++ spec/type/json_config.rb
@@ -11,46 +11,6 @@ module Serverspec
         # check if the files are valid
       end
 
-      def real_initialize(file) # unfinished
-        if os[:family] == 'redhat'
-          # RedHat bases OS related environment spec
-        elsif ['windows'].include?(os[:family])
-          # Windows related environment spec
-
-puppet_home = 'C:/Program Files/Puppet Labs/Puppet'
-puppet_statedir = 'C:/ProgramData/PuppetLabs/puppet/var/state'
-last_run_report = "#{puppet_statedir}/#{file}"
-rubylib = "#{puppet_home}/facter/lib;#{puppet_home}/hiera/lib;#{puppet_home}/puppet/lib;"
-rubyopt = 'rubygems'
-script_file = 'c:/windows/temp/test.rb'
-script_result = 'c:/windows/temp/test.yaml'
-
-ruby_script = <<-EOF
-`$LOAD_PATH.insert(0, '#{puppet_home}/facter/lib')
-`$LOAD_PATH.insert(0, '#{puppet_home}/hiera/lib')
-`$LOAD_PATH.insert(0, '#{puppet_home}/puppet/lib')
-require 'yaml'
-require 'puppet'
-require 'pp'
-# Do basic smoke test
-`$stderr.puts YAML.dump({'answer'=>42})
-File.open('#{script_result}', 'w') { |file| file.write(YAML.dump({'answer'=>42})) }
-EOF
-Specinfra::Runner::run_command(<<-END_COMMAND
-@"
-#{ruby_script}
-"@ | out-file '#{script_file}' -encoding ascii
-END_COMMAND
-)
-# Class: Specinfra::CommandResult 
-# :stdout, :stderr =  empty string
-# http://www.rubydoc.info/gems/specinfra/2.51.1/Specinfra/CommandResult#stdout-instance_method
-      end
-      
-      @content = Specinfra::Runner::get_file_content(script_result).stdout
-      @data = "(" + @content  +")" 
-      # @data = YAML.load(@content)
-    end
     
     def initialize(file) 
     
@@ -85,8 +45,7 @@ Specinfra::Runner::run_command("iex \"ruby.exe '#{script_file}'\"")
 
         script_result = 'c:/windows/temp/test.yaml'
         @content = Specinfra::Runner::get_file_content(script_result).stdout
-        @data = "(" + @content  +")" 
-      # @data = YAML.load(@content)
+      @data = YAML.load(@content)
     end
     
       def has_key?(key)
