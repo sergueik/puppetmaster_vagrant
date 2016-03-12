1839d6a287ea55a2878eca4042f762f61eb70c93
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-11 19:07:48 -0500

    intermediate revision - fixed path, segregated plain run and repeated run

--- spec/linux/ruby_spec.rb
+++ spec/linux/ruby_spec.rb
@@ -76,52 +76,5 @@ context 'Puppet Last Run Report' do
       # TODO: provide example of differently quoted shell script
       Specinfra::Runner::run_command("echo \"#{ruby_script}\" > #{script_file}")
     end
-    context 'Repeated Run' do 
-      lines = [ 
-        'answer: 42',
-        'Status: unchanged',
-        '"failed"=>0', # resources
-        '"failure"=>0', # events
-      ]
-      describe command(<<-EOF
-        export RUBYOPT='rubygems'
-        ruby #{script_file} --run=2
-      EOF
-      ) do
-        # NOTE: Ruby may not be installed system-wide, need to add agent to the PATH 
-        # Puppet 4.3 (2015.2) : `/opt/puppetlabs/bin`
-        # Puppet 3.4 (Puppet Enterprise 3.2) `/opt/puppet/bin`: 
-        let(:path) { '/opt/puppet/bin:/opt/puppetlabs/bin' } 
-        its(:stderr) { should be_empty }
-        its(:exit_status) {should eq 0 }
-        lines.each do |line| 
-          its(:stdout) do
-            should match  Regexp.new(line.gsub(/[()]/,"\\#{$&}").gsub('[','\[').gsub(']','\]'))
-          end
-        end
-      end
-    end
-    context 'First Run' do 
-      lines = [ 
-        'answer: 42',
-        'Status: changed',
-        '"failed"=>0', # resources
-        '"failure"=>0', # events
-      ]
-
-      describe command(<<-EOF
-        export RUBYOPT='rubygems'
-        ruby #{script_file} --run=1
-      EOF
-      ) do
-        its(:stderr) { should be_empty }
-        its(:exit_status) {should eq 0 }
-        lines.each do |line| 
-          its(:stdout) do
-            should match  Regexp.new(line.gsub(/[()]/,"\\#{$&}").gsub('[','\[').gsub(']','\]'))
-          end
-        end
-      end
-    end
   end
 end
