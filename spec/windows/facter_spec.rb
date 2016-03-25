984ab78fb441095a4155c767768360e2bf0ca5e8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-25 13:45:57 -0400

    initial revision

---
+++ spec/windows/facter_spec.rb
@@ -0,0 +1,65 @@
+require_relative '../windows_spec_helper'
+context 'Execute Facter Ruby' do
+  context 'With Environment' do
+    answer = 'Fact: 42'
+    
+    # TODO: distinguish Puppet Community Edition and Puppet Enterprise
+    puppet_home_folder = 'Puppet Enterprise'
+    puppet_home_folder = 'Puppet'
+    # Note: os[:arch] is not being set in Windows platform     
+    puppet_home = 'C:/Program Files (x86)/Puppet Labs/' + puppet_home_folder
+    puppet_home = 'C:/Program Files/Puppet Labs/' + puppet_home_folder
+    rubylib = "#{puppet_home}/facter/lib;#{puppet_home}/hiera/lib;#{puppet_home}/puppet/lib;"
+    rubyopt = 'rubygems' 
+    script_file = 'c:/windows/temp/test.rb'
+    ruby_script = <<-EOF
+require 'yaml'
+require 'puppet'
+require 'pp'
+  
+# Facter code
+require 'facter'
+fact_name = 'puppet_version'
+if Facter.value(:kernel) == 'windows'
+  Facter.add(fact_name) do
+    require 'win32/registry'
+    setcode { '42' }
+  end
+else
+  Facter.add(fact_name) do
+    setcode { '42' }
+  end
+end
+# Facter validation
+
+puts 'Fact: ' +  Facter.value(fact_name.to_sym)
+  
+  EOF
+  
+  Specinfra::Runner::run_command(<<-END_COMMAND
+  @'
+  #{ruby_script}
+'@ | out-file '#{script_file}' -encoding ascii
+  
+  END_COMMAND
+  )
+  
+  
+    describe command(<<-EOF
+  $env:RUBYLIB="#{rubylib}"
+  $env:RUBYOPT="#{rubyopt}"
+  iex "ruby.exe '#{script_file}'"
+  EOF
+  ) do
+      # TODO: distinguish Puppet Community Edition and Puppet Enterprise
+      # Note: os[:arch] is not being set in Windows platform     
+      # 32-bit environment,       
+      let(:path) { 'C:/Program Files/Puppet Labs/Puppet/sys/ruby/bin' }
+      # 64-bit 
+      let(:path) { 'C:/Program Files (x86)/Puppet Labs/Puppet/sys/ruby/bin' }
+      its(:stdout) do
+        should match  Regexp.new(answer.gsub(/[()]/,"\\#{$&}").gsub('[','\[').gsub(']','\]'))
+      end
+    end
+  end
+end
