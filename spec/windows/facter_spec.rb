b0ba03cc7c6e8c1110fb8f3fd1ee6dd0407fe54f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-25 16:06:14 -0400

    intermediate revision - finished consolidation of existing examples

--- spec/windows/facter_spec.rb
+++ spec/windows/facter_spec.rb
@@ -1,8 +1,7 @@
 require_relative '../windows_spec_helper'
 context 'Execute Facter Ruby' do
   context 'With Environment' do
-    answer = 'answer: 42'
-    # +FileVersion 5.0.0.101573
+
     # TODO: distinguish Puppet Community Edition and Puppet Enterprise
     puppet_home_folder = 'Puppet Enterprise'
     puppet_home_folder = 'Puppet'
@@ -11,15 +10,21 @@ context 'Execute Facter Ruby' do
     puppet_home = 'C:/Program Files/Puppet Labs/' + puppet_home_folder
     rubylib = "#{puppet_home}/facter/lib;#{puppet_home}/hiera/lib;#{puppet_home}/puppet/lib;"
     rubyopt = 'rubygems'
+
+    answer = 'file_version: 5.0.0.101573'
+    # answer: 42
+    # registry_value: system32\\\\DRIVERS\\\\cdrom.sys
+    # file_Version: 5.0.0.101573
+    filename = 'c:\Program Files\Windows NT\Accessories\wordpad.exe'
     filename =  'c:\Program Files\Oracle\VirtualBox Guest Additions\VboxMouse.sys'  
     script_file = 'c:/windows/temp/test.rb'
     ruby_script = <<-EOF
 require 'yaml'
 require 'puppet'
 require 'pp'
+require 'facter'
   
 # Facter code
-require 'facter'
 fact_name = 'answer'
 if Facter.value(:kernel) == 'windows'
   Facter.add(fact_name) do
@@ -38,23 +43,52 @@ if Facter.value(:kernel) == 'windows'
   Facter.add(fact_name) do
     require 'Win32API'
       filename = '#{filename}'
+      # http://stackoverflow.com/questions/76472/checking-version-of-file-in-ruby-on-windows
       if File.exists?(filename)
-        vsize=Win32API.new('version.dll', 'GetFileVersionInfoSize', ['P', 'P'], 'L').call(filename, '')
+        vsize = Win32API.new('version.dll', 'GetFileVersionInfoSizeA', ['P', 'P'], 'L').call(filename, '')
+        
         if (vsize > 0)
-          result = ' '*vsize
-          Win32API.new('version.dll', 'GetFileVersionInfo', ['P', 'L', 'L', 'P'], 'L').call(filename, 0, vsize, result)
-          rstring = result.unpack('v*').map{|s| s.chr if s<256}*''
-          version = /FileVersion..(.*?)\\000/.match(rstring)
-          puts version
+          # extracting a data from UTF16
+          result = ' ' * vsize
+          # TODO switch to ffi
+          Win32API.new('version.dll', 'GetFileVersionInfoA', ['P', 'L', 'L', 'P'], 'L').call(filename, 0, vsize, result)
+          rstring = result.unpack('v*').map{ |s| s.chr if s < 256 } *'' 
+          version_match = /FileVersion..(.*?)\000/.match(rstring)
+          version = version_match[1].to_s
+          setcode { version }
+          # alternatives
+          # x = s.match(/F\0i\0l\0e\0V\0e\0r\0s\0i\0o\0n\0*(.*?)\0\0\0/)
+          #
+          #if x.class == MatchData
+          #  ver=x[1].gsub(/\0/,"")
+          #else
+          #  ver="No version"
+          #end
         end
       end
+  end
+end
+fact_name = 'registry_value'
+if Facter.value(:kernel) == 'windows'
+  require 'win32/registry'
+  def hklm_registry_read(key, value)
+    begin
+      reg = Win32::Registry::HKEY_LOCAL_MACHINE.open(key, Win32::Registry::KEY_READ | 0x0100) # read 64 subkey
+      rval = reg[value]
+      reg.close
+      rval
+    rescue
+      nil
+    end
+  end
+  Facter.add(fact_name) do
+    setcode { hklm_registry_read('SYSTEM\\CurrentControlSet\\services\\cdrom', 'ImagePath')}
   end
 else
   # TODO
 end
-
 # Facter validation
-%w/answer file_version/.each do |fact_name|
+%w/answer file_version registry_value/.each do |fact_name|
   puts fact_name + ': ' +  Facter.value(fact_name.to_sym)
 end  
   EOF
