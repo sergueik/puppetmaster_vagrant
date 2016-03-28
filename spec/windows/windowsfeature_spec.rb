d5bd430cb855d8f4c5fa0da604af3091e89eaeda
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-28 14:26:50 -0400

    intermediate revision - refactoring

--- spec/windows/windowsfeature_spec.rb
+++ spec/windows/windowsfeature_spec.rb
@@ -1,67 +1,50 @@
-require_relative '../windows_spec_helper'
+require_relative  '../windows_spec_helper'
 
-context 'Windows Roles and Features' do
-  describe command('Get-WindowsFeature | where-object {$_.Installed -eq $true} | format-list DisplayName') do
-    [ #  List of windows Server 2012 features from a typical IIS server node
-    #  'Application Development',
-    #  'Application Initialization',
-    #  'ASP',
-    #  'ASP.NET 3.5',
-    #  'ASP.NET 4.5',
-    #  'ASP.NET 4.5',
-    #  'Client Certificate Mapping Authentication',
-    #  'Common HTTP Features',
-    #  'Configuration APIs',
-    #  'Default Document',
-    #  'Directory Browsing',
-    #  'File and Storage Services',
-    #  'Graphical Management Tools and Infrastructure',
-    #  'Health and Diagnostics',
-    #  'HTTP Activation',
-    #  'IIS 6 Management Compatibility',
-    #  'IIS 6 Metabase Compatibility',
-    #  'IIS 6 Scripting Tools',
-    #  'IIS 6 WMI Compatibility',
-    #  'IIS Client Certificate Mapping Authentication',
-    #  'IIS Management Console',
-    #  'IIS Management Scripts and Tools',
-    #  'ISAPI Extensions',
-    #  'ISAPI Filters',
-    #  'Logging Tools',
-    #  'Management Service',
-    #  'Management Tools',
-    #  '.NET Extensibility 3.5',
-    #  '.NET Extensibility 4.5',
-    #  '.NET Framework 3.5 Features',
-      '.NET Framework 3.5 (includes .NET 2.0 and 3.0)',
-    #  '.NET Framework 4.5',
-    #  '.NET Framework 4.5 Features',
-    #  'Process Model',
-    #  'Request Filtering',
-    #  'Security',
-    #  'Server Graphical Shell',
-    #  'SMB 1.0/CIFS File Sharing Support',
-    #  'Static Content',
-    #  'Storage Services',
-    #  'TCP Port Sharing',
-    #  'Tracing',
-    #  'User Interfaces and Infrastructure',
-    #  'WCF Services',
-    #  'Web Server',
-    #  'Web Server (IIS)',
-    #  'Windows Authentication',
-      'Windows PowerShell',
-    #  'Windows PowerShell 2.0 Engine',
-    #  'Windows PowerShell 4.0',
-    #  'Windows PowerShell ISE',
-    #  'Windows Process Activation Service',
-    #  'WoW64 Support'
-    ].each  do |line|
-      its(:stdout) do
-        should contain Regexp.new(line.gsub(/[()]/,"\\#{$&}").gsub('[','\[').gsub(']','\]'))
-      end
+  # This server spec is intended to run in Windows Server 2012 environment
+  # to confirm that the 'Windows Search Service' was installed 
+  # it appear to be a challenge to dism and windowsfeature Puppet modules, 
+  # but doable e.g. via plain exec  
+  context 'Servermanager Test' do
+    feature_display_name = 'Windows Search Service'
+    feature_name = 'Search-Service'
+    describe windows_feature(feature_display_name) do
+      # will fail
+      xit{ should be_installed } 
     end
-  end 
 
+    describe windows_feature(feature_name) do
+      # will fail
+      xit{ should be_installed }
+    end
 
+  context 'Powershell test 1' do
+              
+  describe command( <<-EOF
 
+  $ProgressPreference = "SilentlyContinue"
+
+  Import-Module Servermanager
+  
+  get-windowsfeature |
+  where-object {$_.DisplayName -eq '#{feature_display_name}' } |
+  select-object -property 'Installed','DisplayName','Name'|
+  format-list
+  EOF
+  ) do
+
+    its(:stdout) { should  contain 'Installed   : True'}
+    its(:stdout) { should  contain 'DisplayName : Windows Search Service'}
+
+  end
+end
+
+context 'Powershell test 2' do
+              
+  describe command( <<-EOF
+    Import-Module Servermanager | out-null ; $count = @(get-windowsfeature | where-object {$_.DisplayName -eq '#{feature_display_name}' } | where-object {$_.Installed -eq $true } ).count; if ( $count  -eq 0  ) {write-output 'absent'}  else {write-output 'present'} 
+  EOF
+  ) do
+   its(:stdout) { should  contain 'present'}
+  end
+end
+end  
