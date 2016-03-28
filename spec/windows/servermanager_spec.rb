0ac4441c67c7f3897b541851aa9f57d554d936a8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-28 08:54:12 -0400

    intermediate revision - added ProgressPreference

--- spec/windows/servermanager_spec.rb
+++ spec/windows/servermanager_spec.rb
@@ -17,49 +17,34 @@ require_relative  '../windows_spec_helper'
       xit{ should be_installed }
     end
 
-  context 'Failing Powershell Test' do
-    # will fail with
-    #   #< CLIXML Version="1.1.0.1" xmlns="http://scS="progress" RefId="0"><TN RefId="0"...
-    # xml contains the output that one does not get in interactive run:
-    
-    # Preparing modules for first use.
-    # Collecting data...
-    # 5%
-    # Processing
-    # Collecting data...
-    # 100%
-    
-    # Pre-loading does not help. 
-    Specinfra::Runner::run_command(<<-EOF
-    Import-Module Servermanager | out-null
-    start-sleep -seconds 10
-    EOF
-    )
-                
-    describe command( <<-EOF
+  context 'Powershell test 1' do
+              
+  describe command( <<-EOF
 
-    Import-Module Servermanager | out-null
-    get-windowsfeature |
-    where-object {$_.DisplayName -match '#{feature_name}' } |
-    select-object -property 'Installed','DisplayName','Name'|
-    format-list
-    EOF
-    ) do
-    
-    # both will fail
+  $ProgressPreference = "SilentlyContinue"
+
+  Import-Module Servermanager
+  
+  get-windowsfeature |
+  where-object {$_.DisplayName -eq '#{feature_display_name}' } |
+  select-object -property 'Installed','DisplayName','Name'|
+  format-list
+  EOF
+  ) do
+
     its(:stdout) { should  contain 'Installed   : True'}
     its(:stdout) { should  contain 'DisplayName : Windows Search Service'}
 
-    end
   end
+end
 
-  context 'Successful Powershell Test' do
-    # minimize piping output between Powershell and Ruby              
-    describe command( <<-EOF
-      Import-Module Servermanager | out-null ; $status = @(get-windowsfeature | where-object {$_.DisplayName -match '#{feature_name}' } | where-object {$_.Installed -eq $true } ).count -eq 0 ; if (-not $status) {write-output 'absent'}  else {write-output 'present'} 
-    EOF
-    ) do
-     its(:stdout) { should  contain 'present'}
-    end
+context 'Powershell test 2' do
+              
+  describe command( <<-EOF
+    Import-Module Servermanager | out-null ; $count = @(get-windowsfeature | where-object {$_.DisplayName -eq '#{feature_display_name}' } | where-object {$_.Installed -eq $true } ).count; if ( $count  -eq 0  ) {write-output 'absent'}  else {write-output 'present'} 
+  EOF
+  ) do
+   its(:stdout) { should  contain 'present'}
   end
+end
 end  
