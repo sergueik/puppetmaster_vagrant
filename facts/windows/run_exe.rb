a8018e3e2942714c621d7f24c7ad7e5d6bdef773
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-04-16 11:45:41 -0400

    intermediate revision - restored exec snippet

---
+++ facts/windows/run_exe.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'facter'
+
+fact_name = 'application_version'
+
+if Facter.value(:kernel) == 'windows'
+  exe = 'C:/Program Files/Venafi/Platform/VAgent.exe'
+  if File.exists?(exe)
+    exe = "\"#{exe}\"" if Facter.value(:kernel) == 'windows'
+    Facter.add(fact_name) do
+      setcode do
+      	version = nil
+        # extract vendor application version from the help screen.
+        if output = Facter::Util::Resolution.exec("#{exe} -h")
+          version_line = output.split("\n").first
+          versions = version_line.scan /\bv\d+\.\d+\.[\d\-]+\b/
+          version = versions[0].gsub( /\-\d+/, '.0').gsub('v','')
+          version
+      	end
+      end
+    end
+  end
+end
