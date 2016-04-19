d11f9ca719da61296641ec829bb8e53c7815d48f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-04-19 15:24:35 -0400

    intermediate revision - fixing the syntax error

--- facts/windows/cert_check.rb
+++ facts/windows/cert_check.rb
@@ -17,10 +17,24 @@ if Facter.value(:kernel) == 'windows'
       setcode do
         cert_thumbprint = 'A88FD9BDAA06BC0F3C491BA51E231BE35F8D1AD5'       
       	status = nil
-      	script = "$cert_thumbprint = '#{cert_thumbprint}'; $certpath= 'LocalMachine\\TrustedPublisher'; pushd cert: ; cd '\\'; cd $certpath; $status = ((get-childitem -ErrorAction SilentlyContinue | where-object { $_.thumbprint -eq $cert_thumbprint }  ).count -ne 0 ); write-output $status.toString()" 
-        # extract vendor application version from the help screen.
+        # script = "write-output 'test'; $cert_thumbprint = '#{cert_thumbprint}'; $certpath= 'LocalMachine\\TrustedPublisher'; pushd cert: ; cd '\\'; cd $certpath; dir ;  $items = (get-childitem -ErrorAction SilentlyContinue | where-object { $_.thumbprint -eq $cert_thumbprint }  ); write-output ('items = {0}' -f $items.count) " 
+
+      	# script = "$cert_thumbprint = '#{cert_thumbprint}'; $certpath= 'LocalMachine\\TrustedPublisher'; pushd cert: ; cd '\\'; cd $certpath; $status = ((get-childitem -ErrorAction SilentlyContinue | where-object { $_.thumbprint -eq $cert_thumbprint }  ).count -ne 0 ); write-output $status.toString()" 
+        # NOTE: the | where-object  does not work
+       	script = <<-EOF
+
+$cert_thumbprint = '#{cert_thumbprint}'
+$certpath= 'LocalMachine\\TrustedPublisher' 
+pushd cert: 
+cd '\\'
+cd $certpath
+$items = get-childitem -path '.'
+foreach ($item in $items){ if ($item.thumbprint -eq $cert_thumbprint) {write-output $item.thumbprint} } 
+   EOF
+        script.gsub!(/\n/, ';')
         if output = Facter::Util::Resolution.exec("#{exe} #{script}")
-          status = output.split("\n").first
+          # Skip possible debugging output
+          status = output.split("\n").last
           status
       	end
       end
