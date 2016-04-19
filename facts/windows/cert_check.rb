b6cdd16aeba535e5fdbc6fbac14d800c9e7a545a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-04-19 19:59:07 -0400

    intermediate revision - documentation and cleanup

--- facts/windows/cert_check.rb
+++ facts/windows/cert_check.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/env ruby
 
-# checks for the file using Powershell 
-# This skeleton script allows generalization to many other providers that Powershell represents as directory-like interface:
-# certificates, registry, firewall.
+# checks for the ssl cert using Powershell 
+# this example shows a workaround to possibly a most intuitive from Powershell programmer's perspective code snippet
+# intended to print a true / false result dependent on the presence of a cert with spefic thumbprint under a specic path         
+# interesting, the snippets involving the '| where-object {}' do not seem to work correctly from Ruby -  the output of the following is blank:
+# script = "write-output 'test'; $cert_thumbprint = '#{cert_thumbprint}'; $certpath= 'LocalMachine\\TrustedPublisher'; pushd cert: ; cd '\\'; cd $certpath; dir ;  $items = (get-childitem -ErrorAction SilentlyContinue | where-object { $_.thumbprint -eq $cert_thumbprint }  ); write-output ('items = {0}' -f $items.count) " 
 
 require 'facter'
 
 fact_name = 'cert_check'
-filename = 'c:\Windows\Tasks\SampleJob.job'
+cert_path = 'LocalMachine\TrustedPublisher'
+cert_thumbprint = 'A88FD9BDAA06BC0F3C491BA51E231BE35F8D1AD5'       
 
 if Facter.value(:kernel) == 'windows'
   exe = 'C:/Windows/system32/WindowsPowershell/v1.0/powershell.exe'
@@ -15,25 +18,27 @@ if Facter.value(:kernel) == 'windows'
     exe = "\"#{exe}\"" if Facter.value(:kernel) == 'windows'
     Facter.add(fact_name) do
       setcode do
-        cert_thumbprint = 'A88FD9BDAA06BC0F3C491BA51E231BE35F8D1AD5'       
       	status = nil
-        # script = "write-output 'test'; $cert_thumbprint = '#{cert_thumbprint}'; $certpath= 'LocalMachine\\TrustedPublisher'; pushd cert: ; cd '\\'; cd $certpath; dir ;  $items = (get-childitem -ErrorAction SilentlyContinue | where-object { $_.thumbprint -eq $cert_thumbprint }  ); write-output ('items = {0}' -f $items.count) " 
-
-      	# script = "$cert_thumbprint = '#{cert_thumbprint}'; $certpath= 'LocalMachine\\TrustedPublisher'; pushd cert: ; cd '\\'; cd $certpath; $status = ((get-childitem -ErrorAction SilentlyContinue | where-object { $_.thumbprint -eq $cert_thumbprint }  ).count -ne 0 ); write-output $status.toString()" 
-        # NOTE: the | where-object  does not work
        	script = <<-EOF
 
 $cert_thumbprint = '#{cert_thumbprint}'
-$certpath= 'LocalMachine\\TrustedPublisher' 
+$cert_path= '#{cert_path}' 
 pushd cert: 
 cd '\\'
-cd $certpath
+cd $cert_path
 $items = get-childitem -path '.'
-foreach ($item in $items){ if ($item.thumbprint -eq $cert_thumbprint) {write-output $item.thumbprint} } 
+foreach ($item in $items){ 
+  if ( $item.thumbprint -eq $cert_thumbprint ) {
+     write-output $item.thumbprint  
+     write-output 'true'
+  } 
+} 
    EOF
+        # Convert to a single-line snippet
         script.gsub!(/\n/, ';')
         if output = Facter::Util::Resolution.exec("#{exe} #{script}")
           # Skip possible debugging output
+          puts "Debug: " + output 
           status = output.split("\n").last
           status
       	end
