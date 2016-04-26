94ae83776a16d7c3edb46ec852c8466c83fd4626
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-04-26 10:24:43 -0400

    intermediate revision -  cosmetic refactoring

--- spec/linux/named_pipe_spec.rb
+++ spec/linux/named_pipe_spec.rb
@@ -4,15 +4,19 @@
     process_mask = '[t]lmagent.bin'
     
     describe command("/bin/netstat -ano | grep '#{named_pipe}'") do
-      its (:stdout) { should contain 'STREAM' }
-      its (:stdout) { should contain 'LISTENING' }
-      its (:stdout) { should contain named_pipe }
+      its (:stdout) do
+        should contain 'STREAM' 
+        should contain 'LISTENING'
+        should contain named_pipe
+      end
     end
     
     describe command("/bin/netstat -anp | grep $(ps ax | grep '#{process_mask}' | awk '{print $1}')") do
-      its (:stdout) { should contain 'STREAM' }
-      its (:stdout) { should contain 'LISTENING' }
-      its (:stdout) { should contain named_pipe }
+      its (:stdout) do
+        should contain 'STREAM'
+        should contain 'LISTENING'
+        should contain named_pipe
+      end  
     end
     describe file(named_pipe)  do
       it { should be_socket } 
