326d0962e4378eeaa71de63779e07aa55d4e03e5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-12 16:30:58 -0400

    intermediate revision - added a yet another method

--- spec/windows/ports_spec.rb
+++ spec/windows/ports_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../windows_spec_helper'
 context 'ports' do
-      @executable =  'svchost' 
+    @executable =  'svchost' 
     describe command(<<-END_COMMAND
     \$process_id = (get-wmiobject -computername '.' -query "select name, processid from win32_process where name like '#{@executable}%'").processid
 $status  = -1
@@ -21,4 +21,30 @@ END_COMMAND
       its(:stdout) { should match /([\d],?)+/ }
       its(:exit_status) { should == 0 } 
     end
+    tcp_port = 3702
+    timeout  = 100
+    # origin http://poshcode.org/5310,  only TCP  part so far
+    describe command(<<-END_COMMAND
+
+$ErrorActionPreference = 'SilentlyContinue'
+$tcpobject = new-Object system.Net.Sockets.TcpClient
+$port = #{tcp_port}
+$timeout = #{timeout}
+$connect = $tcpobject.BeginConnect('127.0.0.1',$port,$null,$null)
+$status = $connect.AsyncWaitHandle.WaitOne($timeout,$false)
+if( -not $status ) {
+   $tcpobject.Close()
+   write-output "Connection to Port ${port} Timed Out"
+} else {
+ $tcpobject.EndConnect($connect) | out-Null
+ write-output "Connection to Port ${port} was successful"
+}
+$tcpobject.Close()
+    
+    END_COMMAND
+) do
+      its(:stdout) { should match /successful/ }
+      its(:exit_status) { should == 0 } 
+    end
+    
 end
