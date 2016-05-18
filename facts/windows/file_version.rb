9c421ebd2984e5b04aeb588899a0d09dee26f2f1
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-05-18 00:09:16 -0400

    intermediate revision - added registry_uninstall_version

--- facts/windows/file_version.rb
+++ facts/windows/file_version.rb
@@ -2,8 +2,6 @@
 
 require 'facter'
 
-require 'facter'
-
 fact_name = 'vboxcontrol_version'
 
 if Facter.value(:kernel) == 'windows'
@@ -20,21 +18,21 @@ if Facter.value(:kernel) == 'windows'
     attach_function :verqueryvalue, :VerQueryValueA,  [ :buffer_in, :buffer_in, :buffer_out, :pointer ], :int
   end
   def versioninfo_ffi_version(filepath)
-  
+
     # contains two alternative code snippets, one unfinished, equally cryptic
-    
+
     file_version = nil
     size_in_bytes = Test.version_resource_size_bytes(filepath, nil)
     if size_in_bytes != 0
       result = ' ' * size_in_bytes
       status = Test.version(filepath, 0, size_in_bytes, result)
-      rstring = result.unpack('v*').map{ |s| s.chr if s < 256 } * '' 
+      rstring = result.unpack('v*').map{ |s| s.chr if s < 256 } * ''
       rstring = rstring.gsub(/\000/, ' ')
       version_match = /FileVersion\s+\b([0-9.]+)\b/.match(rstring)
       version_value = version_match[1].to_s
       file_version = version_value
 
-      #  the following is  incomplete  and does not work 
+      #  the following is  incomplete  and does not work
       result_value_size = FFI::MemoryPointer.new(:long, size_in_bytes)
       version_information = '\VarFileInfo\Translation'.encode('UTF-16LE')
       result_value = ' ' * size_in_bytes
@@ -45,13 +43,33 @@ if Facter.value(:kernel) == 'windows'
       status = Test.verqueryvalue(result, version_information, result_value, result_value_size)
       # file_version = result_value
     end
-    file_version    
+    file_version
   end
-  
+
+  def registry_uninstall_version(product_guid)
+    # Puppet 'A26152DC-07BA-4ACE-A86C-F8385DC55097'
+    product_version = nil
+    # ideally one would simply return Puppet::Provider::Package::Windows::Package().
+    # https://github.com/puppetlabs/puppet/blob/master/lib/puppet/provider/package/windows/msi_package.rb
+    # https://github.com/puppetlabs/puppet/blob/master/lib/puppet/provider/package/windows.rb
+    # https://github.com/liamjbennett/puppet-win_facts/blob/master/lib/facter/windows_productkey.rb
+    access = Win32::Registry::KEY_READ | 0x100
+
+    Win32::Registry::HKEY_LOCAL_MACHINE.open("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{#{product_guid}}", access) do |reg|
+      reg.each do |name, type, data|
+        puts name, data
+        if name.eql?('DisplayVersion')
+          product_version = data
+        end
+       end
+    end
+    product_version
+  end
+
   def versioninfo_version(filepath)
     file_version = nil
     require 'Win32API'
-    
+
     if File.exists?(filepath)
 
       # https://msdn.microsoft.com/en-us/library/windows/desktop/ms647005%28v=vs.85%29.aspx
@@ -60,13 +78,13 @@ if Facter.value(:kernel) == 'windows'
         result = "\0" * size_in_bytes
         Win32API.new('version.dll', 'GetFileVersionInfo', ['P', 'L', 'L', 'P'], 'L').call(filepath, 0, size_in_bytes, result)
         rstring = result.unpack('v*').map{|s| s.chr if s<256 } * ''
-        r = /FileVersion..(.*?)\000/.match(rstring)      
+        r = /FileVersion..(.*?)\000/.match(rstring)
         file_version = "#{r ? r[1] : '??' }"
       end
     end
   end
   def powershell_get_version(filepath)
-    file_version = nil 
+    file_version = nil
     if File.exists?(filepath)
       exe = 'C:/Windows/system32/WindowsPowershell/v1.0/powershell.exe'
       script = <<-EOF
@@ -74,21 +92,23 @@ if Facter.value(:kernel) == 'windows'
         $info = get-item -path $filepath
         $raw_info  = $info.Versioninfo.ProductVersion
         $fact =  $raw_info  -replace '(\\d+\\.\\d+\\.\\d+)\\s+\\(Build\\s+(.+)\\)\\s*$' , '$1-$2'
-        write-output $fact 
+        write-output $fact
       EOF
       # Convert to a single-line snippet
       script.gsub!(/\n/, ';')
       if output = Facter::Util::Resolution.exec("#{exe} #{script}")
         file_version = output.split("\n").last
       end
-    end      
-  end      
+    end
+  end
   Facter.add(fact_name) do
     setcode do
       filepath = 'C:\Program Files\Oracle\VirtualBox Guest Additions\VBoxControl.exe'
       # versioninfo_version(filepath)
       # versioninfo_ffi_version(filepath) # vboxcontrol_version = '4.3.8.92456'
       powershell_get_version(filepath)  # vboxcontrol_version = '4.3.8.r92456'
-    end 
+      registry_uninstall_version('A26152DC-07BA-4ACE-A86C-F8385DC55097')
+     # Puppet 'A26152DC-07BA-4ACE-A86C-F8385DC55097'
+    end
   end
 end
