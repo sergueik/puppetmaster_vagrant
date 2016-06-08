554c42529431dfab8c422282ae9a488cb73fb3be
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-06-07 21:06:11 -0400

    intermediate revision - added all in one file uru spec runner bootstrap scripts

--- runner.ps1
+++ runner.ps1
@@ -1,4 +1,27 @@
-pushd C:\tools
+$ToolsPath='C:\tools'
+$RubyPath="${ToolsPath}\ruby"
+
+mkdir "${env:USERPROFILE}\.uru" -erroraction silentlycontinue
+
+@"
+
+{
+  "Version": "1.0.0",
+  "Rubies": {
+    "3516592278": {
+      "ID": "2.1.8-p440",
+      "TagLabel": "218p440",
+      "Exe": "ruby",
+      "Home": "$($RubyPath -replace '\\', '\\')\\bin",
+      "GemHome": "",
+      "Description": "ruby 2.1.8p440 (2015-12-16 revision 53160) [i386-mingw32]"
+
+    }
+  }
+}
+"@ |out-file -FilePath "${env:USERPROFILE}\.uru\rubies.json" -encoding ASCII
+pushd $ToolsPath
+
 $PWD =  pwd | select-object -expandproperty PATH
 $env:PATH="${env:PATH};${PWD}"
 # write-output $env:PATH
@@ -9,6 +32,6 @@ $tag = ($data -replace '^\s+\b(\w+)\b.*$', '$1')
 write-output ("tag = '{0}'" -f $tag )
 $env:URU_INVOKER = 'powershell'
 uru_rt.exe $tag
-uru_rt.exe ruby C:\tools\serverspec\ruby\lib\ruby\gems\2.1.0\gems\rake-11.1.2\bin\rake spec
+uru_rt.exe ruby "${RubyPath}\lib\ruby\gems\2.1.0\gems\rake-11.1.2\bin\rake" spec
 
 popd 
