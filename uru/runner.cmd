8bf7577d2d554e397d84dc5f26ed782ba8794a01
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-06-08 14:15:22 -0400

    intermediate revision - updated URU launchers

---
+++ uru/runner.cmd
@@ -0,0 +1,13 @@
+@echo OFF
+SETLOCAL
+SET TOOLSPATH=c:\tools
+SET RUBYPATH=%TOOLSPATH%\ruby
+set URU_INVOKER=batch
+pushd %TOOLSPATH%
+PATH=%PATH%;%CD%
+CALL uru.bat ls
+for /F "tokens=1" %%. in ('uru_rt.exe ls') do @set TAG=%%.
+echo TAG=%TAG%
+uru_rt.exe "%TAG%"
+uru_rt.exe ruby "%RUBYPATH%\lib\ruby\gems\2.1.0\gems\rake-11.1.2\bin\rake" spec
+popd
