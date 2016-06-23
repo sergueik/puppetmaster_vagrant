61e1b050ead43282942a9182868f3066790be4d8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-06-23 19:27:28 -0400

    initial revision

---
+++ facts/windows/specialfolder_value.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'facter'
+
+# Name of this fact.
+fact_name = 'specialfolder_value'
+
+if Facter.value(:kernel) == 'windows'
+
+  Facter.add(fact_name) do
+
+    setcode do
+      File.write('c:/windows/temp/test.ps1', <<-EOF
+
+      try {
+        # https://msdn.microsoft.com/en-us/library/system.environment.specialfolder(v=vs.110).asp
+        $Result = [environment]::GetFolderPath([System.Environment.SpecialFolder]::ProgramFiles)
+        # NOTE: naive conversion does not work -
+      } catch [Exception]{
+      }
+      $Result = [Environment]::GetFolderPath('LocalApplicationData') 
+      # [Environment+SpecialFolder]::GetNames([Environment+SpecialFolder])
+      # LocalApplicationData
+      # UserProfile
+      write-output ('Content: "{0}"' -f  $Result ) 
+      EOF
+      )
+      data_prefix = 'Content'
+      data = nil
+      if output = Facter::Util::Resolution.exec('C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy remotesigned -file "c:/windows/temp/test.ps1"')
+        data_line = output.split("\n").grep(/#{data_prefix}/).first
+        data = data_line.scan(/"[^"]+"/).first
+      end
+      data
+    end
+  end
+end
