72b79a92678a01b53f847d8ff723a69537640a11
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-07-09 14:02:31 -0400

    intermediate revision - format and convert to english

--- spec/windows/licensestatus_spec.rb
+++ spec/windows/licensestatus_spec.rb
@@ -1,15 +1,35 @@
 # http://stackoverflow.com/questions/29368414/need-script-to-find-server-activation-status
+# http://blog.tyang.org/2015/01/08/detecting-windows-license-activation-status-using-configmgr-dcm-opsmgr/
 
 require_relative '../windows_spec_helper'
 context 'LicenseStatus' do
   describe command (<<-EOF
+# License Status codes:
+$license_status_codes = @{
+  '0' = 'Unlicensed';
+  '1' = 'Licensed';
+  '2' = 'Out-of-Box Grace Period';
+  '3' = 'Out-of-Tolerance Grace Period';
+  '4' = 'Non-Genuine Grace Period';
+  '5' = 'Notification';
+  '6' = 'ExtendedGrace';
+}
+
 # Terse form
-Get-CimInstance -ClassName SoftwareLicensingProduct |where PartialProductKey |select PScomputername,LicenseStatus| format-list
-# Longer form presumably will work with Powershell 2.0
-Get-CimInstance -ClassName SoftwareLicensingProduct | where-object { $_.PartialProductKey -match '\\S' } |select-object -property PScomputername,LicenseStatus | format-list
-  
+# $license_status = Get-CimInstance -ClassName SoftwareLicensingProduct | 
+                    where PartialProductKey | 
+                    select LicenseStatus
+
+
+# Longer form - presumably will work with Powershell 2.0
+$license_status = Get-CimInstance -ClassName SoftwareLicensingProduct | 
+                  Where-Object { $_.PartialProductKey -match '\\S' } | 
+                  Select-Object -ExpandProperty LicenseStatus
+
+write-output $license_status_codes[$license_status.tostring()]
   EOF
   ) do
-    its(:stdout) { should contain 'LicenseStatus  : 1' }
+    its(:stdout) { should contain 'Licensed' }
+
   end
 end
