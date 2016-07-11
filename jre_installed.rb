a9facc6bc7b712b8e5bcc906e8604fbbb4cb94c1
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-07-11 13:38:54 -0400

    initial revision

---
+++ jre_installed.rb
@@ -0,0 +1,21 @@
+fact_name = 'jre_installed'
+
+if Facter.value(:kernel) == 'windows'
+  Facter.add(fact_name) do
+    versions = []
+    data_prefix = 'ProductName'
+    tool = 'C:/Windows/system32/reg.exe'
+    argument = 'query hklm\software\classes\installer\products /f "JRE " /s'
+    if output = Facter::Util::Resolution.exec("#{tool} #{argument}")
+      version = output.split("\n").grep(/#{data_prefix}/).last
+      version.gsub!(/^.*REG_SZ\s+/,'')
+      if version
+        versions.push( version)
+      end
+    end
+    if versions.length > 0 
+      setcode { versions.join(',') }
+    else 
+      setcode { '' }    
+    end
+  end
