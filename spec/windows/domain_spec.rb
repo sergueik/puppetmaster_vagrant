3eca25e86f3715e2755c695a14b3ec5472da416c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-08-12 15:22:29 -0400

    intermediate revision - actually providing a fixed file

--- spec/windows/domain_spec.rb
+++ spec/windows/domain_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../windows_spec_helper'
 
 context 'Domain Membership' do
-  domain = 'domain_name'
+  domain = '<DOMAIN NAME>'
   describe command(<<-END_COMMAND
-
-if (( get-wmiobject -class Win32_ComputerSystem).PartOfDomain -eq  $true){
-  write-output ('domain: {0}' -f ( [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()))
-  write-output ('domain: {0}' -f ( get-wmiobject -class Win32_ComputerSystem).Domain ))
-} else { 
-
-  write-output ( ( get-wmiobject -class Win32_ComputerSystem).workgroup )
-  # not in the domain 
-} 
-
+$o = get-wmiobject -class Win32_ComputerSystem
+if ($o.PartOfDomain -eq  $true ) {
+  # https://msdn.microsoft.com/en-us/library/system.directoryservices.activedirectory.domain(v=vs.110).aspx
+  # write-output ('domain: {0}' -f ( [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()))
+  # Exception calling "GetCurrentDomain" with "0" argument(s): "Current security context is not associated with an ActiveDirectory domain or forest."
+  write-output ('domain: {0}' -f $o.Domain )
+} else {
+  write-output ('workgroup: {0}' -f $o.workgroup )
+  # not in the domain
+}
 END_COMMAND
 ) do
     its(:stdout) { should match /domain: #{domain}/i }
