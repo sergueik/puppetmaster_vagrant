c03b2fafba2edb00fa928b83b96c8abf18c6f77a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-10-07 11:46:33 -0400

    intermediate revision - updated contexts

--- spec/linux/pgrep_spec.rb
+++ spec/linux/pgrep_spec.rb
@@ -6,9 +6,16 @@ context 'Specinfra Service Resource Alternative' do
 
   service_name = 'service_name'
   classpath = 'package.name.ClassName'
-
   let(:path) do
     '/sbin:/bin:/usr/bin'
+  end
+
+  describe service(service_name) do
+    # the canonical 'specinfra' appears to broken on RHEL 7
+    # specinfra probably falls into negative lookbehind trap
+    # https://github.com/mizzy
+    it { should be_enabled }
+    it { should be_running }
   end
 
   classpath = classpath.gsub(/^(.)/, '[\1]')
@@ -17,8 +24,9 @@ context 'Specinfra Service Resource Alternative' do
     describe command("service #{service_name} status") do
       its(:stdout) { should contain 'not running' }
     end
-
-    # specinfra uses sudo
+  end
+  context 'is not found by pgrep' do
+    # NOTE: specinfra uses sudo
     describe command("pgrep -f '#{classpath}' -l") do
       its(:exit_status) { should eq 1 }
       its(:stdout) do
@@ -27,13 +35,15 @@ context 'Specinfra Service Resource Alternative' do
       end
     end
   end
-
   context 'is running' do
     describe command("service #{service_name} status") do
       # NOTE: negative lookbehind
       its(:stdout) { should match(/(?<!not )running/) }
       its(:stdout) { should_not contain 'not running' }
     end
+  end
+  context 'is found by pgrep' do
+    # NOTE: specinfra uses sudo
     describe command("pgrep -f '#{classpath}' -l") do
       its(:exit_status) { should eq 0 }
       its(:stdout) do
