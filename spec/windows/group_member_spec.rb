52b0f3cb25b593302bf3e1365ca4e21d827b2628
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-10-18 20:13:09 -0400

    intermediate revision - added one win32_groupuser method

--- spec/windows/group_member_spec.rb
+++ spec/windows/group_member_spec.rb
@@ -1,9 +1,39 @@
 require_relative '../windows_spec_helper'
 
-# origin: http://poshcode.org/544
 
 context 'Administrators' do
 
+  # origin: http://poshcode.org/6581
+  context 'WMI' do
+    local_group = 'Administrators'
+    describe command(<<-EOF
+      function Get-LocalGroupMembers {
+        param(
+          [string]$LocalGroup = 'Administrators'
+        )
+        $pattern = ('*Name="{0}"' -f $LocalGroup)
+        foreach ($user in (Get-WmiObject -Class 'win32_groupuser' | Where-Object { $_.GroupComponent -like $pattern })) {
+          if ($user.PartComponent -match 'Name="([^"]+)"') {
+            Write-Output $matches[1]
+          }
+        }
+      }
+      Get-LocalGroupMembers #  '#{local_group}'
+      EOF
+    ) do
+      {
+        'Administrator' => true,
+        'Domain Admins' =>  false,
+        'Other Group'  => false,
+      }.each do |key,val|
+        if val
+          its(:stdout) { should match /#{key}/io }
+        end  
+      end
+    end 
+  end
+
+  # origin: http://poshcode.org/544
   context 'ADSI' do
 
     describe command(<<-EOF
@@ -30,8 +60,7 @@ context 'Administrators' do
           $output.InLocalAdmin = $MemberNames -contains $_
           Write-Output $output
         }
-      }
-    
+      }    
     EOF
     ) do
       {
