fd59792821e9a59c370aae060e85682b04f51959
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-11-07 10:39:54 -0500

    intermediate revision - added Utf8Checker wrapper spec

--- spec/windows/broken_encoding_spec.rb
+++ spec/windows/broken_encoding_spec.rb
@@ -28,7 +28,9 @@ this is a test this is a test this is a test this is a test this is a test
 "@
 			write-output $content | out-file '#{file_path}' -encoding Ascii
 			write-output $content | out-file '#{file_path}' -append
-			write-output $content | out-file '#{file_path}' -encoding Ascii -append
+			write-output $content | out-file '#{file_path}' -encoding Ascii -
+
+append
 		END_COMMAND
 		)
 	end
@@ -49,11 +51,17 @@ this is a test this is a test this is a test this is a test this is a test
         return
       }
 
-      [System.IO.FileStream]$content = New-Object System.IO.FileStream ($file_path,[System.IO.FileMode]::Open)
-      [byte[]]$byte_content = (New-Object System.IO.BinaryReader ($content)).ReadBytes([convert]::ToInt32($content.Length))
+      [System.IO.FileStream]$content = New-Object System.IO.FileStream ($file_path,
+
+[System.IO.FileMode]::Open)
+      [byte[]]$byte_content = (New-Object System.IO.BinaryReader ($content)).ReadBytes
+
+([convert]::ToInt32($content.Length))
       $content.Close()
       $offset = 0
-      for ([int]$cnt = 0; $cnt -ne $byte_content.Length; $cnt++) { if ($byte_content[$cnt] -eq 0) {
+      for ([int]$cnt = 0; $cnt -ne $byte_content.Length; $cnt++) { if ($byte_content[$cnt]
+
+-eq 0) {
           if ($offset -eq 0) {
             $offset = $cnt + 1
           } } }
@@ -102,4 +110,219 @@ this is a test this is a test this is a test this is a test this is a test
 			its(:stdout) { should contain encoding }
 		end
 	end
+  context 'UTF-8' do
+    # puts "\u{2B71F}"
+    # http://utf8checker.codeplex.com
+    describe command(<<-EOF
+      add-type @'
+      using System;
+      using System.IO;
+
+      namespace Unicode
+      {
+          public interface IUtf8Checker
+          {
+              // true if utf8 encoded, otherwise false.
+              bool Check(string fileName);
+              // true if utf8 encoded, otherwise false.
+              bool IsUtf8(Stream stream);
+          }
+          public class Utf8Checker : IUtf8Checker
+          {
+              public bool Check(string fileName)
+              {
+                  using (BufferedStream fstream = new BufferedStream(File.OpenRead
+
+(fileName)))
+                  {
+                      return this.IsUtf8(fstream);
+                  }
+              }
+
+              public bool IsUtf8(Stream stream)
+              {
+                  int count = 4 * 1024;
+                  byte[] buffer;
+                  int read;
+                  while (true)
+                  {
+                      buffer = new byte[count];
+                      stream.Seek(0, SeekOrigin.Begin);
+                      read = stream.Read(buffer, 0, count);
+                      if (read < count)
+                      {
+                          break;
+                      }
+                      buffer = null;
+                      count *= 2;
+                  }
+                  return IsUtf8(buffer, read);
+              }
+
+              public static bool IsUtf8(byte[] buffer, int length)
+              {
+                  int position = 0;
+                  int bytes = 0;
+                  while (position < length)
+                  {
+                      if (!IsValid(buffer, position, length, ref bytes))
+                      {
+                          return false;
+                      }
+                      position += bytes;
+                  }
+                  return true;
+              }
+
+              public static bool IsValid(byte[] buffer, int position, int length, ref int
+
+bytes)
+              {
+                  if (length > buffer.Length)
+                  {
+                      throw new ArgumentException("Invalid length");
+                  }
+
+                  if (position > length - 1)
+                  {
+                      bytes = 0;
+                      return true;
+                  }
+
+                  byte ch = buffer[position];
+
+                  if (ch <= 0x7F)
+                  {
+                      bytes = 1;
+                      return true;
+                  }
+
+                  if (ch >= 0xc2 && ch <= 0xdf)
+                  {
+                      if (position >= length - 2)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+                      if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0xbf)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+                      bytes = 2;
+                      return true;
+                  }
+
+                  if (ch == 0xe0)
+                  {
+                      if (position >= length - 3)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      if (buffer[position + 1] < 0xa0 || buffer[position + 1] > 0xbf ||
+                          buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+                      bytes = 3;
+                      return true;
+                  }
+
+
+                  if (ch >= 0xe1 && ch <= 0xef)
+                  {
+                      if (position >= length - 3)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0xbf ||
+                          buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      bytes = 3;
+                      return true;
+                  }
+
+                  if (ch == 0xf0)
+                  {
+                      if (position >= length - 4)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      if (buffer[position + 1] < 0x90 || buffer[position + 1] > 0xbf ||
+                          buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf ||
+                          buffer[position + 3] < 0x80 || buffer[position + 3] > 0xbf)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      bytes = 4;
+                      return true;
+                  }
+
+                  if (ch == 0xf4)
+                  {
+                      if (position >= length - 4)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0x8f ||
+                          buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf ||
+                          buffer[position + 3] < 0x80 || buffer[position + 3] > 0xbf)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      bytes = 4;
+                      return true;
+                  }
+
+                  if (ch >= 0xf1 && ch <= 0xf3)
+                  {
+                      if (position >= length - 4)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0xbf ||
+                          buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf ||
+                          buffer[position + 3] < 0x80 || buffer[position + 3] > 0xbf)
+                      {
+                          bytes = 0;
+                          return false;
+                      }
+
+                      bytes = 4;
+                      return true;
+                  }
+
+                  return false;
+              }
+          }
+      }
+'@
+      $o = new-object -typeName 'Unicode.Utf8Checker'
+      $filename = 'c:\\users\\vagrant\\unicode.txt'
+      $o.Check($filename)
+    EOF
+      ) do
+        its(:exit_status) { should eq 0 }
+        its(:stdout) { should contain 'False' }
+    end
+  end
 end
