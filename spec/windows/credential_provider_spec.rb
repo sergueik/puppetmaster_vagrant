8fb7ce43c0d4365f253c24872f1e6695522e374d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-11-15 14:34:55 -0500

    intermediate revision - fixed navigation in the registry

--- spec/windows/credential_provider_spec.rb
+++ spec/windows/credential_provider_spec.rb
@@ -5,18 +5,27 @@ require_relative '../windows_spec_helper'
 # http://winintro.com/?Category=Windows_10_2016&Policy=Microsoft.Policies.CredentialProviders%3A%3ADefaultCredentialProvider
 context 'Discover Credential Provider' do
   provider = 'PasswordProvider'
-  # varies with Windows OS version: on Windows 8.1 in the default configuration, is 'WLIDCredentialProvider'
-  # will be different for SSO and 2 Factor Authentication
+  # varies with Windows OS version:
+  # Windows 7  'PasswordProvider'
+  # Windows 8.1 'WLIDCredentialProvider'
+  # 2-Factor Auth 'SIDCredentialProvider'
+  # will be something different for SSO
   context 'Current Session' do
     describe command(<<-EOF
       pushd HKLM:
-      cd '/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/LogonUI/SessionData/1'
-      $guid = get-itemProperty -Path '/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/LogonUI/SessionData/1' -name 'LastLoggedOnProvider' | select-object -expandProperty 'LastLoggedOnProvider'
+      cd /
+      cd ( get-childitem -Path '/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/LogonUI/SessionData' |
+           select-object -first 1 |
+           select-object -expandproperty name |
+           foreach-object { $_ -replace 'HKEY_LOCAL_MACHINE', 'HKLM:' })
+      $guid = get-itemProperty -Path '.' -name 'LastLoggedOnProvider' |
+              select-object -expandProperty 'LastLoggedOnProvider'
       popd
       pushd HKLM:
       cd '/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/Credential Providers'
       cd $guid
-      $provider = get-itemProperty -Path ('/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/Credential Providers/{0}' -f $guid) -name '(Default)' | select-object -expandProperty '(Default)'
+      $provider = get-itemProperty -Path ('.' -f $guid) -name '(Default)' |
+                  select-object -expandProperty '(Default)'
       popd
       write-output $provider
       #
@@ -31,12 +40,14 @@ context 'Discover Credential Provider' do
     describe command(<<-EOF
       pushd HKLM:
       cd '/Software/Policies/Microsoft/Windows/System'
-      $guid = get-itemProperty -Path '/Software/Policies/Microsoft/Windows/System' -name 'DefaultCredentialProvider' | select-object -expandProperty 'DefaultCredentialProvider'
+      $guid = get-itemProperty -Path '/Software/Policies/Microsoft/Windows/System' -name 'DefaultCredentialProvider' |
+              select-object -expandProperty 'DefaultCredentialProvider'
       popd
       pushd HKLM:
       cd '/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/Credential Providers'
       cd $guid
-      $provider = get-itemProperty -Path ('/SOFTWARE/Microsoft/Windows/CurrentVersion/Authentication/Credential Providers/{0}' -f $guid) -name '(Default)' | select-object -expandProperty '(Default)'
+      $provider = get-itemProperty -Path '.' -name '(Default)' |
+                  select-object -expandProperty '(Default)'
       popd
       write-output $provider
       #
