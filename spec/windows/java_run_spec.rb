45de23c46d7badd536707b60155395c075ba0421
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-12-07 18:16:39 -0500

    intermediate revision  - first working revision

--- spec/windows/java_run_spec.rb
+++ spec/windows/java_run_spec.rb
@@ -3,34 +3,47 @@ require_relative '../windows_spec_helper'
 context 'Run Java class' do
 
   context 'Basic' do
-    # NOTE: frequent 'Illegal characters in path' errors
+    # NOTE: observed frequent 'Illegal characters in path' errors during development 
+    temp_path = 'C:\windows\temp'
+    java_path = 'C:\java\jdk1.8.0_101'    
+    # java_path = 'D:\apps\java\jre1.8.0_112-64'
     code_base64 = 'yv66vgAAADQAHQoABgAPCQAQABEIABIKABMAFAcAFQcAFgEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAARtYWluAQAWKFtMamF2YS9sYW5nL1N0cmluZzspVgEAClNvdXJjZUZpbGUBAAhBcHAuamF2YQwABwAIBwAXDAAYABkBAARUZXN0BwAaDAAbABwBAANBcHABABBqYXZhL2xhbmcvT2JqZWN0AQAQamF2YS9sYW5nL1N5c3RlbQEAA2VycgEAFUxqYXZhL2lvL1ByaW50U3RyZWFtOwEAE2phdmEvaW8vUHJpbnRTdHJlYW0BAAdwcmludGxuAQAVKExqYXZhL2xhbmcvU3RyaW5nOylWACEABQAGAAAAAAACAAEABwAIAAEACQAAAB0AAQABAAAABSq3AAGxAAAAAQAKAAAABgABAAAAAwAJAAsADAABAAkAAAAlAAIAAQAAAAmyAAISA7YABLEAAAABAAoAAAAKAAIAAAAGAAgABwABAA0AAAACAA4='
     class_name = 'App'
     describe command(<<-EOF
-
-    $code_base64 = '#{code_base64}'
-    $class_name = '#{class_name}'
-    $bytes = [System.Convert]::FromBase64String($code_base64)
-    $temp_path = 'C:\\windows\\temp'
-    [io.file]::WriteAllBytes("${temp_path}\\${class_name}.class",  $bytes )
-    write-output "java ${class_name}"
-    $env:PATH = "$env:PATH;C:\\java\\jdk1.8.0_101\\bin"
-    pushd $temp_path
-    dir
-    try {
-      $command = "java ${class_name}"
-      write-output 'Running command: {}' -f $command 
-      invoke-expression -command $command
-    } catch [Exception] {
-    # Error: Could not find or load main class App
-    # Exception calling "WriteAllBytes" with "2" argument(s): "Access tothe path 'C:\windows\temp\App.class' is denied."
-    }
-    popd
+      $java_path = '#{java_path}'
+      $code_base64 = '#{code_base64}'
+      $class_name = '#{class_name}'
+      $bytes = [System.Convert]::FromBase64String($code_base64)
+      $temp_path = '#{temp_path}'
+      try {      
+        [IO.File]::WriteAllBytes("${temp_path}\\${class_name}.class",  $bytes )
+        $env:PATH = "$env:PATH;${java_path}\\bin"
+        pushd $temp_path
+        dir "${class_name}.class"
+        try {
+          $command = "java ${class_name} 2>&1"
+          write-output ('Running command: {0} in "{1}"' -f $command, $temp_path )
+          $output = invoke-expression -command $command
+          write-output ('output: {0}' -f $output)
+        } catch [Exception] {
+          write-output (($_.Exception.Message) -split "`n")[0]
+        }
+      } catch [Exception] {
+          write-output (($_.Exception.Message) -split "`n")[0]
+      }
+      popd
     EOF
     ) do
-      let(:path) {'C:\\java\\jdk1.8.0_101\\bin'} 
-      # its(:stdout) { should match /Test/i }
+      let(:path) { "#{java_path}\\bin" } 
+      its(:stdout) { should match /Test/i }
       its(:stdout) { should match /#{class_name}.class/i }
     end
   end
+
+  # note there is also a `jrunscript` command in jdk 
+  # this is capable or interpreting a language-independent command-line script shells, 
+  # it is ranked experimental, and seldom used:
+  # https://docs.oracle.com/javase/8/docs/technotes/tools/windows/jrunscript.html
+  # http://www.herongyang.com/JavaScript/jrunscript-Run-JavaScript-Code-with-jrunscript.html
+  
 end
