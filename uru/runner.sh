5ad5b31397898832ea765db81d89af4dd44d1c31
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-12-24 11:52:54 -0500

    intermediate revision - refactoring

--- uru/runner.sh
+++ uru/runner.sh
@@ -1,51 +1,72 @@
 #!/bin/sh
 
-export URU_HOME='/uru'
+# set -x
+# URU_HOME='<%= @uru_home -%>'
+if [ -z $URU_HOME ]
+then 
+  URU_HOME='/uru'
+fi
+export URU_HOME
+
 export URU_INVOKER=bash
-export LD_LIBRARY_PATH=${URU_HOME}/ruby/lib
+export LD_LIBRARY_PATH=$URU_HOME/ruby/lib
 
-GEM_VERSION=2.1.0
-RAKE_VERSION=10.1.0
-RUBY_VERSION=2.1.0
-RUBY_RUNTIME_ID='219p490'
-URU_RUNNER="${URU_HOME}/uru_rt"
+GEM_VERSION='2.1.0'
+RAKE_VERSION='10.1.0'
+RUBY_VERSION='2.1.0'
+RUBY_VERSION_LONG='2.1.9p490'
+RUBY_TAG_LABEL='219p490'
 
-pushd ${URU_HOME}
-# TODO: $URU_RUNNER admin refresh
+URU_RUNNER=$URU_HOME/uru_rt
+
+pushd $URU_HOME
+
+# TODO: execute 
+# $URU_RUNNER admin refresh
 # when the ~/.uru/rubies.json, in particular the GemHome, is different
 
-export HOME='/root'
-if [[ ! -d "$HOME/.uru" ]]; then mkdir "$HOME/.uru"; fi
+if [ -z $HOME ] ; then HOME='/root'; fi
+if [[ ! -d $HOME/.uru ]]; then mkdir "$HOME/.uru"; fi
 rm "$HOME/.uru/rubies.json"
-cat <<EOF>"$HOME/.uru/rubies.json"
+cat <<EOF>$HOME/.uru/rubies.json
 {
   "Version": "1.0.0",
   "Rubies": {
   "2357568376": {
-    "ID": "2.1.9-p490",
-    "TagLabel": "219p490",
+    "ID": "$RUBY_VERSION_LONG",
+    "TagLabel": "$RUBY_TAG_LABEL",
     "Exe": "ruby",
-    "Home": "${URU_HOME}/ruby/bin",
-    "GemHome": "${URU_HOME}/.gem/ruby/${GEM_VERSION}",
-    "Description": "ruby 2.1.9p490 (2016-03-30 revision 54437) [x86_64-linux]"
+    "Home": "$URU_HOME/ruby/bin",
+    "GemHome": "$URU_HOME/.gem/ruby/$GEM_VERSION",
+    "Description": "ruby $RUBY_VERSION_LONG (2016-03-30 revision 54437) [x86_64-linux]"
     }
  }
 }
 EOF
-echo Y |$URU_RUNNER  admin rm $RUBY_RUNTIME_ID > /dev/null
+
+echo Y |$URU_RUNNER admin rm $RUBY_TAG_LABEL > /dev/null
 $URU_RUNNER admin add ruby/bin
 
-$URU_RUNNER ls --verbose
-export TAG=`$URU_RUNNER ls 2>& 1|awk '{print $1}'`
-$URU_RUNNER $TAG
+if [ ! -z $DEBUG ]
+then 
+  $URU_RUNNER ls --verbose
+fi
+if [ ! -z $DEBUG ]
+then 
+  TAG=`$URU_RUNNER ls 2>& 1|awk '{print $1}'`
+  $URU_RUNNER $TAG
+fi
 
-# TODO: fix it properly
 # Copy .gems to default location
 
+# TODO: fix it properly
 cp -R .gem $HOME
 
-# Verify the gems
-$URU_RUNNER gem list --local
+if [ ! -z $DEBUG ]
+then 
+  # Verify the gems
+  $URU_RUNNER gem list --local
+fi
 
 # Check that the required gems are present
 $URU_RUNNER gem list| grep -qi serverspec
