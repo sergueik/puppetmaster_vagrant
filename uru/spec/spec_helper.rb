1c88e2fde613fc3414ab0faa21b4a0d6991583bd
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-12-30 15:06:37 -0500

    moved uru serversppec development to separate repository

--- uru/spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'serverspec'
-
-set :backend, :exec
-
-RSpec.configure do |config|
-  config.filter_gems_from_backtrace 'vagrant', 'vagrant-serverspec'
-end
--- uru/spec/windows_spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'serverspec'
-
-set :backend, :cmd
-
-RSpec.configure do |config|
-  config.filter_gems_from_backtrace 'vagrant', 'vagrant-serverspec'
-end
