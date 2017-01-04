64ac2dee88f7955fcfd16bb7b803df97ab1b1d13
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-01-03 20:41:23 -0500

    intermediate revision - added a simple symbolic link target test

--- spec/windows/junction_pinvoke_spec.rb
+++ spec/windows/junction_pinvoke_spec.rb
@@ -1,154 +1,171 @@
 require_relative '../windows_spec_helper'
 
-context 'Use pinvoke to read Directory Junctions ans Reparse Points' do
-  # requires custom specinfra
-  context 'Junctions ans Reparse Points' do
-    describe file('c:/temp/xxx') do
+context 'Symbolic Links' do
+
+  before(:each) do
+    Specinfra::Runner::run_command( <<-EOF
+    pushd 'c:/temp'
+    mkdir 'directory_target' -erroraction 'silentlycontinue'
+    cmd %%- /c mklink /D 'directory_link' 'directory_target'
+  EOF
+  ) end
+
+  context 'Requires custom specinfra' do
+    describe file('c:/temp/directory_link') do
      it { should be_symlink }
     end
   end
-  describe command( <<-EOF
-
-# use pinvoke to read directory junction /  symlink target
-#  http://chrisbensen.blogspot.com/2010/06/getfinalpathnamebyhandle.html
-Add-Type -TypeDefinition @"
-// "
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel; // for Win32Exception
-using System.Data;
-using System.Text;
-using System.IO;
-using System.Runtime.InteropServices;
-using Microsoft.Win32.SafeHandles;
-
-public class Utility
-{
-    private const int FILE_SHARE_READ = 1;
-    private const int FILE_SHARE_WRITE = 2;
-
-    private const int CREATION_DISPOSITION_OPEN_EXISTING = 3;
-
-    private const int FILE_FLAG_BACKUP_SEMANTICS = 0x02000000;
-
-    // http://msdn.microsoft.com/en-us/library/aa364962%28VS.85%29.aspx
-    // http://pinvoke.net/default.aspx/kernel32/GetFileInformationByHandleEx.html
-
-    // http://www.pinvoke.net/default.aspx/shell32/GetFinalPathNameByHandle.html
-    [DllImport("kernel32.dll", EntryPoint = "GetFinalPathNameByHandleW", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern int GetFinalPathNameByHandle(IntPtr handle, [In, Out] StringBuilder path, int bufLen, int flags);
-
-    // https://msdn.microsoft.com/en-us/library/aa364953%28VS.85%29.aspx
-
-    // http://msdn.microsoft.com/en-us/library/aa363858(VS.85).aspx
-    // http://www.pinvoke.net/default.aspx/kernel32.createfile
-    [DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode,
-    IntPtr SecurityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
-
-    public static string GetSymbolicLinkTarget(DirectoryInfo symlink)
-    {
-        SafeFileHandle directoryHandle = CreateFile(symlink.FullName, 0, 2, System.IntPtr.Zero, CREATION_DISPOSITION_OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, System.IntPtr.Zero);
-        if (directoryHandle.IsInvalid)
-            throw new Win32Exception(Marshal.GetLastWin32Error());
-
-        StringBuilder path = new StringBuilder(512);
-        int size = GetFinalPathNameByHandle(directoryHandle.DangerousGetHandle(), path, path.Capacity, 0);
-        if (size < 0)
-            throw new Win32Exception(Marshal.GetLastWin32Error());
-        // http://msdn.microsoft.com/en-us/library/aa365247(v=VS.85).aspx
-        if (path[0] == '\\\\' && path[1] == '\\\\' && path[2] == '?' && path[3] == '\\\\')
-            return path.ToString().Substring(4);
-        else
-            return path.ToString();
-    }
-
-}
-"@ -ReferencedAssemblies 'System.Windows.Forms.dll','System.Runtime.InteropServices.dll','System.Net.dll','System.Data.dll','mscorlib.dll'
-
-$symlink_directory = 'c:\\temp\\test'
-$symlink_directory_directoryinfo_object = New-Object System.IO.DirectoryInfo ($symlink_directory)
-$junction_target = [utility]::GetSymbolicLinkTarget($symlink_directory_directoryinfo_object)
-write-output ('junction target: {0}' -f $junction_target )
-
-EOF
-) do
-    its(:exit_status) {should eq 0 }
-    its(:stdout) { should match /junction target: c:\\windows\\softwareDistribution/i }
+  context 'CMD' do
+    symlink_path = 'C:/temp/directory_link'
+    symlink_parent_path = 'c:/temp'
+    describe command(<<-EOF
+      $symlink_parent_path = '#{symlink_parent_path}' -replace '/' , '\\'
+      cmd %%- /c dir /A:L $symlink_parent_path
+    EOF
+    ) do
+      its(:stdout) { should match Regexp.new('directory_target') }
+    end
   end
 
+  context 'Directory Junctions and Reparse Points' do
+    # use pinvoke to read directory junction /  symlink target
+    # http://chrisbensen.blogspot.com/2010/06/getfinalpathnamebyhandle.html
+    describe command( <<-EOF
+      Add-Type -TypeDefinition @"
+      using System;
+      using System.Collections.Generic;
+      using System.ComponentModel; // for Win32Exception
+      using System.Data;
+      using System.Text;
+      using System.IO;
+      using System.Runtime.InteropServices;
+      using Microsoft.Win32.SafeHandles;
 
-  describe command( <<-EOF
+      public class Utility
+      {
+          private const int FILE_SHARE_READ = 1;
+          private const int FILE_SHARE_WRITE = 2;
 
-# use pinvoke to read directory junction / symlink target
-#  http://chrisbensen.blogspot.com/2010/06/getfinalpathnamebyhandle.html
-Add-Type -TypeDefinition @"
-// "
+          private const int CREATION_DISPOSITION_OPEN_EXISTING = 3;
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel; // for Win32Exception
-using System.Data;
-using System.Text;
-using System.IO;
-using System.Runtime.InteropServices;
-using Microsoft.Win32.SafeHandles;
+          private const int FILE_FLAG_BACKUP_SEMANTICS = 0x02000000;
 
-public class Utility
-{
+          // http://msdn.microsoft.com/en-us/library/aa364962%28VS.85%29.aspx
+          // http://pinvoke.net/default.aspx/kernel32/GetFileInformationByHandleEx.html
 
-    private const int FILE_SHARE_READ = 1;
-    private const int FILE_SHARE_WRITE = 2;
+          // http://www.pinvoke.net/default.aspx/shell32/GetFinalPathNameByHandle.html
+          [DllImport("kernel32.dll", EntryPoint = "GetFinalPathNameByHandleW", CharSet = CharSet.Unicode, SetLastError = true)]
+          public static extern int GetFinalPathNameByHandle(IntPtr handle, [In, Out] StringBuilder path, int bufLen, int flags);
 
-    private const int CREATION_DISPOSITION_OPEN_EXISTING = 3;
+          // https://msdn.microsoft.com/en-us/library/aa364953%28VS.85%29.aspx
 
-    private const int FILE_FLAG_BACKUP_SEMANTICS = 0x02000000;
+          // http://msdn.microsoft.com/en-us/library/aa363858(VS.85).aspx
+          // http://www.pinvoke.net/default.aspx/kernel32.createfile
+          [DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true)]
+          public static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode,
+          IntPtr SecurityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
 
-    // http://msdn.microsoft.com/en-us/library/aa364962%28VS.85%29.aspx
-    // http://pinvoke.net/default.aspx/kernel32/GetFileInformationByHandleEx.html
+          public static string GetSymbolicLinkTarget(DirectoryInfo symlink)
+          {
+              SafeFileHandle directoryHandle = CreateFile(symlink.FullName, 0, 2, System.IntPtr.Zero, CREATION_DISPOSITION_OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, System.IntPtr.Zero);
+              if (directoryHandle.IsInvalid)
+                  throw new Win32Exception(Marshal.GetLastWin32Error());
 
-    // http://www.pinvoke.net/default.aspx/shell32/GetFinalPathNameByHandle.html
-    [DllImport("kernel32.dll", EntryPoint = "GetFinalPathNameByHandleW", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern int GetFinalPathNameByHandle(IntPtr handle, [In, Out] StringBuilder path, int bufLen, int flags);
+              StringBuilder path = new StringBuilder(512);
+              int size = GetFinalPathNameByHandle(directoryHandle.DangerousGetHandle(), path, path.Capacity, 0);
+              if (size < 0)
+                  throw new Win32Exception(Marshal.GetLastWin32Error());
+              // http://msdn.microsoft.com/en-us/library/aa365247(v=VS.85).aspx
+              if (path[0] == '\\\\' && path[1] == '\\\\' && path[2] == '?' && path[3] == '\\\\')
+                  return path.ToString().Substring(4);
+              else
+                  return path.ToString();
+          }
 
-    // https://msdn.microsoft.com/en-us/library/aa364953%28VS.85%29.aspx
-
-    // http://msdn.microsoft.com/en-us/library/aa363858(VS.85).aspx
-    // http://www.pinvoke.net/default.aspx/kernel32.createfile
-    [DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true)]
-    public static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode,
-    IntPtr SecurityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
-
-    public static string GetSymbolicLinkTarget(FileInfo symlink)
-    {
-        SafeFileHandle fileHandle = CreateFile(symlink.FullName, 0, 2, System.IntPtr.Zero, CREATION_DISPOSITION_OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, System.IntPtr.Zero);
-        if (fileHandle.IsInvalid)
-            throw new Win32Exception(Marshal.GetLastWin32Error());
-
-        StringBuilder path = new StringBuilder(512);
-        int size = GetFinalPathNameByHandle(fileHandle.DangerousGetHandle(), path, path.Capacity, 0);
-        if (size < 0)
-            throw new Win32Exception(Marshal.GetLastWin32Error());
-        // http://msdn.microsoft.com/en-us/library/aa365247(v=VS.85).aspx
-        if (path[0] == '\\\\' && path[1] == '\\\\' && path[2] == '?' && path[3] == '\\\\')
-            return path.ToString().Substring(4);
-        else
-            return path.ToString();
-    }
-}
+      }
 "@ -ReferencedAssemblies 'System.Windows.Forms.dll','System.Runtime.InteropServices.dll','System.Net.dll','System.Data.dll','mscorlib.dll'
 
-$symlink_file = 'c:\\temp\\specinfra'
+      $symlink_directory = 'c:\\temp\\test'
+      $symlink_directory_directoryinfo_object = New-Object System.IO.DirectoryInfo ($symlink_directory)
+      $junction_target = [utility]::GetSymbolicLinkTarget($symlink_directory_directoryinfo_object)
+      write-output ('junction target: {0}' -f $junction_target )
 
-$symlink_file_fileinfo_object = New-Object System.IO.FileInfo ($symlink_file)
-$symlink_target = [utility]::GetSymbolicLinkTarget($symlink_file_fileinfo_object)
-write-output ('symlink target: {0}' -f $symlink_target )
+      EOF
+      ) do
+          its(:exit_status) {should eq 0 }
+          its(:stdout) { should match /junction target: c:\\windows\\softwareDistribution/i }
+        end
 
 
-EOF
-) do
-    its(:exit_status) {should eq 0 }
-    its(:stdout) { should match /symlink target: C:\\temp\\specinfra-2.43.5.gem/i }
+    describe command( <<-EOF
+
+  # use pinvoke to read directory junction / symlink target
+  #  http://chrisbensen.blogspot.com/2010/06/getfinalpathnamebyhandle.html
+  Add-Type -TypeDefinition @"
+  // "
+
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel; // for Win32Exception
+  using System.Data;
+  using System.Text;
+  using System.IO;
+  using System.Runtime.InteropServices;
+  using Microsoft.Win32.SafeHandles;
+
+  public class Utility
+  {
+
+      private const int FILE_SHARE_READ = 1;
+      private const int FILE_SHARE_WRITE = 2;
+
+      private const int CREATION_DISPOSITION_OPEN_EXISTING = 3;
+
+      private const int FILE_FLAG_BACKUP_SEMANTICS = 0x02000000;
+
+      // http://msdn.microsoft.com/en-us/library/aa364962%28VS.85%29.aspx
+      // http://pinvoke.net/default.aspx/kernel32/GetFileInformationByHandleEx.html
+
+      // http://www.pinvoke.net/default.aspx/shell32/GetFinalPathNameByHandle.html
+      [DllImport("kernel32.dll", EntryPoint = "GetFinalPathNameByHandleW", CharSet = CharSet.Unicode, SetLastError = true)]
+      public static extern int GetFinalPathNameByHandle(IntPtr handle, [In, Out] StringBuilder path, int bufLen, int flags);
+
+      // https://msdn.microsoft.com/en-us/library/aa364953%28VS.85%29.aspx
+
+      // http://msdn.microsoft.com/en-us/library/aa363858(VS.85).aspx
+      // http://www.pinvoke.net/default.aspx/kernel32.createfile
+      [DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true)]
+      public static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode,
+      IntPtr SecurityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+      public static string GetSymbolicLinkTarget(FileInfo symlink)
+      {
+          SafeFileHandle fileHandle = CreateFile(symlink.FullName, 0, 2, System.IntPtr.Zero, CREATION_DISPOSITION_OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, System.IntPtr.Zero);
+          if (fileHandle.IsInvalid)
+              throw new Win32Exception(Marshal.GetLastWin32Error());
+
+          StringBuilder path = new StringBuilder(512);
+          int size = GetFinalPathNameByHandle(fileHandle.DangerousGetHandle(), path, path.Capacity, 0);
+          if (size < 0)
+              throw new Win32Exception(Marshal.GetLastWin32Error());
+          // http://msdn.microsoft.com/en-us/library/aa365247(v=VS.85).aspx
+          if (path[0] == '\\\\' && path[1] == '\\\\' && path[2] == '?' && path[3] == '\\\\')
+              return path.ToString().Substring(4);
+          else
+              return path.ToString();
+      }
+  }
+"@ -ReferencedAssemblies 'System.Windows.Forms.dll','System.Runtime.InteropServices.dll','System.Net.dll','System.Data.dll','mscorlib.dll'
+
+  $symlink_file = 'c:\\temp\\specinfra'
+
+  $symlink_file_fileinfo_object = New-Object System.IO.FileInfo ($symlink_file)
+  $symlink_target = [utility]::GetSymbolicLinkTarget($symlink_file_fileinfo_object)
+  write-output ('symlink target: {0}' -f $symlink_target )
+  EOF
+  ) do
+      its(:exit_status) {should eq 0 }
+      its(:stdout) { should match /symlink target: C:\\temp\\specinfra-2.43.5.gem/i }
+    end
   end
 end
