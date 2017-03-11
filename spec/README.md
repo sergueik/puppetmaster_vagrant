081c2f8a53d2b1b2d23f94967906b9076f67f463
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-03-11 12:27:49 -0500

    intermediate revision - added YAML command stdout spec tests

--- spec/README.md
+++ spec/README.md
@@ -1,7 +1,8 @@
-Info
-----
-This directory contains collection of  serverspec snippets, mostly Windows-specific. 
+### Info
 
-Running ServerSpec directly on Target is covered in [uru README](https://github.com/sergueik/puppetmaster_vagrant/blob/master/uru/README.md)
--------------------------------------
+This directory contains collection of advanced server spec snippets, mostly Windows, also some Unix-specific.
 
+Running server spec directly on target instance is covered in [Uru Serverspec](https://github.com/sergueik/uru_serverspec)
+
+### Author
+[Serguei Kouzmine](kouzmine_serguei@yahoo.com)
--- spec/type/command.rb
+++ spec/type/command.rb
@@ -1,14 +1,17 @@
-require 'json'
 require 'pp'
+require 'yaml'
+require 'json'
+require 'csv'
+
 # origin: https://github.com/mizzy/serverspec/blob/master/lib/serverspec/type/command.rb
 # monkey-patching the Command class in the uru environment
 module Serverspec::Type
-	class Command < Base
-		def stdout
-			command_result.stdout
-		end
+  class Command < Base
+    def stdout
+      command_result.stdout
+    end
 
-		def stdout_as_json
+    def stdout_as_json
       begin
         @res = JSON.parse(command_result.stdout)
         # pp @res
@@ -16,19 +19,29 @@ module Serverspec::Type
       rescue => e
         nil
       end
-		end
+    end
 
-		def stderr
-			command_result.stderr
-		end
+    def stdout_as_yaml
+      begin
+        @res = YAML.load(command_result.stdout)
+        # pp @res
+        @res
+      rescue => e
+        nil
+      end
+    end
 
-		def exit_status
-			command_result.exit_status.to_i
-		end
+    def stderr
+      command_result.stderr
+    end
 
-		private
-		def command_result()
-			@command_result ||= @runner.run_command(@name)
-		end
-	end
+    def exit_status
+      command_result.exit_status.to_i
+    end
+
+    private
+    def command_result()
+      @command_result ||= @runner.run_command(@name)
+    end
+  end
 end
