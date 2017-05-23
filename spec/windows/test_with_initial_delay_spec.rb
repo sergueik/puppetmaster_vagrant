42f80ee12979b1958ed37cee967e3bf0dc7dca14
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-05-23 15:24:57 -0400

    intermediate revision - added realistic exit clause

--- spec/windows/test_with_initial_delay_spec.rb
+++ spec/windows/test_with_initial_delay_spec.rb
@@ -8,23 +8,43 @@ context 'Test with initial delay' do
     before(:each) do
       delay = 1000
       repeat = 10
-      log  = 'c:/windows/temp/sleeep.log'
+      logfile = 'c:/windows/temp/sleeep.log'
+
+      vendor = 'Hewlett Packard Enterprise'
+      version = '4.32'
+      product = 'HP Enterprise Security Fortify SCA and Applications'
+      reg_key = "HKLM:\\SOFTWARE\\Wow6432Node\\#{vendor}\\#{product} #{version}"
+      reg_value = 'version' 
+      
       Specinfra::Runner::run_command(<<-END_COMMAND
       
       $delay = #{delay}
       $repeat = #{repeat}
-      $log = '#{log}'
+      $logfile = '#{logfile}'
+      $reg_key = '#{reg_key}'
+      $reg_value = '#{reg_value}' 
+
       0..$repeat | foreach-object { 
         $round = $_ 
         write-output ('round {0}' -f $round) | out-file $log -append -encoding 'ascii'
         start-sleep -millisecond $delay
-        $test = $false
-        # do any reasonable check to abort the loop
-        if ($test) {
+
+        $status = $false
+        # placeholder for check to abort the loop
+        if ($status) {
           exit
         }
-        # can verify from the size of the $log, how many times the code was run
+        # more realistic case - wait for vendor  registry key is found in the Registry
+        $status = $false
+
+        $reg_data = get-itemproperty -path "${reg_key}" -erroraction silentlycontinue | 
+          select-object -expandproperty $reg_value
+        [bool]$status = ($reg_data -ne $null)
+        if ($status) {
+          exit
+        }
       }
+      # NORE: one can inspect the $logfile, how many times the code was run
       END_COMMAND
       ) 
     end
