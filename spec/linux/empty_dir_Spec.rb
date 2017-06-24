cc30a24f42278218577f145d58164f867125fd9c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-06-23 23:03:59 -0400

    intermediate revision - aded empty dir spec from s.o. example

---
+++ spec/linux/empty_dir_Spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+path_to_dir = '/var/run/sshd'
+describe file(path_to_dir) do
+  it { should be_a_directory }
+  it 'should be an empty directory' do
+    Dir.glob("#{path_to_dir}/*").should eq []
+  end
+end
