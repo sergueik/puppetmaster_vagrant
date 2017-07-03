1edd7630e0651523f52f324d7805b0cc7292cd1c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-07-03 13:51:56 -0400

    intermediate revision - added example of a failing partial match

--- spec/linux/cron_spec.rb
+++ spec/linux/cron_spec.rb
@@ -3,6 +3,9 @@
   # https://github.com/mizzy/specinfra/blob/master/lib/specinfra/command/base/cron.rb#L4
   describe cron do
     it { should have_entry('*/5 * * * * /usr/bin/rsync -avz --delete --port 1873 --password-file=/opt/baswso2/rsync.secret baswso2@apim-api-manager-gateway-manager-0.node.qualified_primary_dc.consul::deployment_repo/ deployment_repo').with_user(cronjob_user) }
+    # partial matches do not work because the underlyting command  enforces the full line match:    
+    # https://github.com/mizzy/specinfra/blob/master/lib/specinfra/command/base/cron.rb#L5
+    it { should_not have_entry('*/5 * * * * /usr/bin/rsync -avz --delete --port 1873 --password-file=/opt/baswso2/rsync.secret').with_user(user) }
   end
 
   # http://serverspec.org/resource_types.html#cron says
