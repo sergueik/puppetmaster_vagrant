0dc1bf43ce35c55e425be1c018df858cf5e4a09a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-07-04 10:38:10 -0400

    intermediate revision - updated extension type

--- spec/type/port_retry.rb
+++ spec/type/port_retry.rb
@@ -1,10 +1,10 @@
 module Serverspec::Type
-  class RetryPort < Port
+  class PortRetry < Port
     def retries(max_retries)
       options[:retry] = max_retries
     end
-    
-    def listening?(protocol, local_address, retries)
+
+    def listening?(protocol = 'TCP', local_address = nil, retries)
       protocol_matcher(protocol) if protocol
       local_address_matcher(local_address) if local_address
       retries(retries) if retries
@@ -12,8 +12,8 @@ module Serverspec::Type
     end
   end
 
-  def retry_port(port)
-    RetryPort.new(port)
+  def port_retry(port)
+    PortRetry.new(port)
   end
 end
 
@@ -23,29 +23,34 @@ RSpec::Matchers.define :be_listening do
   match do |port|
     port.listening? @with, @local_address, @retries
   end
-  
+
   chain :with do |protocol|
     @with = protocol
   end
-  
+
   chain :on do |local_address|
     @local_address = local_address
   end
-  
+
   chain :with_retry do |retries|
     @retries = retries
   end
+
+  chain :with_delay do |delay|
+    @delay = delay
+  end
 end
 
+# static override of https://github.com/mizzy/specinfra/blob/master/lib/specinfra/command/windows/base/port.rb#L3
 Specinfra::Command::Windows::Base::Port.class_eval do
-  def self.check_is_listening_with_retry(port, options)
+  def self.check_is_listening_with_retry(port, options = nil)
     script = <<-EOF
-      $ProgressPreference = "SilentlyContinue"
-      $retries = 0 + #{options[:retry]};
+      $ProgressPreference = 'SilentlyContinue'
+      $retries = 0 + #{options[:retry]}
       $port = #{port}
       0.. $retries | foreach-object {
         $x = & 'netstat' '-ano' '-p' 'TCP';
-        $count = @($x | where-object {$_ -match "LISTEN" } | where-object {$_ -match ":${port}" }).count;
+        $count = @($x | where-object {$_ -match 'LISTEN' } | where-object {$_ -match ":${port}" }).count;
         if  ($count -ne 0)  {
           write-output "Port ${port} is listening";
           exit 0;
@@ -53,15 +58,16 @@ Specinfra::Command::Windows::Base::Port.class_eval do
           start-sleep -seconds 30;
         }
       }
-      write-output "No access to port ${port}";
+      write-host "Port ${port} not listening";
       exit 1;
     EOF
     script
   end
 end
 
+# static override of https://github.com/mizzy/specinfra/blob/master/lib/specinfra/command/base/port.rb#L3
 Specinfra::Command::Base::Port.class_eval do
-  def self.check_is_listening_with_retry(port, options)
+  def self.check_is_listening_with_retry(port, options = {})
     script = <<-EOF
       isPortListening() {
         counter=0
@@ -81,4 +87,3 @@ Specinfra::Command::Base::Port.class_eval do
     script
   end
 end
-
