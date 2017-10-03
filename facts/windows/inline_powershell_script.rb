16141e4dc975163c16195caac4ca76c8388acb38
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-10-03 14:45:12 -0400

    misc changes, not likely to be worth further investment

--- facts/windows/inline_powershell_script.rb
+++ facts/windows/inline_powershell_script.rb
@@ -1,22 +1,23 @@
+# This skeleton script demonstrates 
+# perform Registry scan using Powershell inline
+$ This may be useful for Powershell directory-like providers other than registry:
+# certificates, registry, firewall
+# NOTE: this fact is quite fragile
+# the inline script may work correctly in Powershell and cmd console, but return no result to Ruby
+# NOTE: Powershell pipeline also seems to create a problem, also to a cmd console
+# $value = (Get-ItemProperty -LiteralPath 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters' -name 'ServiceDll') | select-object -expandproperty ServiceDll
+
 require 'yaml'
 require 'puppet'
 require 'pp'
 
 require 'facter'
 
-# name of the custom fact
 fact_name = 'registry_check'
 
-# code of the fact to follow
-# perform Registry scan using Powershell inline
-# This skeleton script demonstrates writing Powershell directory-like provider interface invocation inline:
-# certificates, registry, firewall
 property = 'ServiceDll'
 path = 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters'
 debug = false
-# inline script may work correctly in Powershell and cmd console, but return empty result to Ruby fact
-# NOTE: Powershell pipeline also seems to create a problem, also to a cmd console
-# $value = (Get-ItemProperty -LiteralPath 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters' -name 'ServiceDll') | select-object -expandproperty ServiceDll
 inline_script = <<-EOF
   $debugpreference = 'Continue'
   $value = (Get-ItemProperty -LiteralPath 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters' -name 'ServiceDll').ServiceDll
@@ -39,37 +40,31 @@ inline_script = <<-EOF
   return $result
 EOF
 if Facter.value(:kernel) == 'windows'
-  exe = 'C:/Windows/system32/WindowsPowershell/v1.0/powershell.exe'
-  if File.exists?(exe)
-    exe = "\"#{exe}\""
-  end
-  if Facter.value(:kernel) == 'windows'
-    Facter.add(fact_name) do
-      setcode do
-       status = nil
-        command = "#{exe} \"#{inline_script.gsub("\n",';')}\""
-        if debug
-          STDERR.puts "script=\n#{inline_script.gsub("\n",';')}"
-          STDERR.puts "command=\n#{command}"
-        end
-        output = Facter::Util::Resolution.exec(command)
-        if output.nil?
-          if debug
-            STDERR.puts 'no output'
-          end
-        else
-          if debug
-            STDERR.puts "output=\"#{output}\""
-          end
-          # TODO: debug
-          # status = output.split("\r?\n").grep(/^[^ ]+$/).first
-          status = output.split("\r?\n").last
-          if debug
-            STDERR.puts "status=#{status}"
-          end
-          status
-       end
+  Facter.add(fact_name) do
+    setcode do
+     result = nil
+      # NOTE: occasionally a simplified command line is better:
+      # command = "\"c:/windows/system32/windowspowershell/v1.0/powershell.exe\" \"#{inline_script.gsub("\n",';')}\""
+      command = "\"c:/windows/system32/windowspowershell/v1.0/powershell.exe\" -NoProfile -ExecutionPolicy unrestricted -command \"&{ #{inline_script.gsub("\n",';')} }\""
+      if debug
+        STDERR.puts "command=\n#{command}"
       end
+      output = Facter::Util::Resolution.exec(command)
+      if output.nil?
+        if debug
+          STDERR.puts 'no output'
+        end
+      else
+        if debug
+          STDERR.puts "output=\"#{output}\""
+        end
+        # NOTE: with Powershell debug messages printed to STDOUT become part of the "result"
+        result = output.split("\r?\n").reject {|line| line =~ /^\s*$/ }.last
+        if debug
+          STDERR.puts "result=#{result}"
+        end
+        result
+     end
     end
   end
 end
