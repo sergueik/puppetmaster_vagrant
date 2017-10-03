a56763570398136a1de354fd9e8896b149899bed
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-10-02 21:35:59 -0400

    intermediate revision - added registry data extraction script example

--- facts/windows/inline_powershell_script.rb
+++ facts/windows/inline_powershell_script.rb
@@ -14,12 +14,29 @@ fact_name = 'registry_check'
 property = 'ServiceDll'
 path = 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters'
 debug = false
-# inline script works when run in cmd window, but might fail to return the string value to Ruby
+# inline script may work correctly in Powershell and cmd console, but return empty result to Ruby fact
+# NOTE: Powershell pipeline also seems to create a problem, also to a cmd console
+# $value = (Get-ItemProperty -LiteralPath 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters' -name 'ServiceDll') | select-object -expandproperty ServiceDll
 inline_script = <<-EOF
   $debugpreference = 'Continue'
   $value = (Get-ItemProperty -LiteralPath 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters' -name 'ServiceDll').ServiceDll
   write-debug ('value = {0}' -f $value)
   return $value
+EOF
+inline_script = <<-EOF
+  $debugpreference = 'Continue'
+  $properties = @()
+  $base_obj = Get-Item "HKLM:\\SYSTEM\\CurrentControlSet\\services\\mcollective"
+  $count = $base_obj.Property.Count
+  $item = 0
+  while ($item -le $count) {
+    $value_name = $base_obj.GetValueNames()[$item]
+    $properties += $base_obj.GetValue($value_name)
+    $item = $item + 1;
+  }
+  $result = $properties -join ','
+  write-debug ('Result = {0}' -f $result)
+  return $result
 EOF
 if Facter.value(:kernel) == 'windows'
   exe = 'C:/Windows/system32/WindowsPowershell/v1.0/powershell.exe'
@@ -44,7 +61,9 @@ if Facter.value(:kernel) == 'windows'
           if debug
             STDERR.puts "output=\"#{output}\""
           end
-          status = output.split("\n").last
+          # TODO: debug
+          # status = output.split("\r?\n").grep(/^[^ ]+$/).first
+          status = output.split("\r?\n").last
           if debug
             STDERR.puts "status=#{status}"
           end
