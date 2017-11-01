2ebffbb1bf7884b1aaf0316bd7963af97d0fe9cc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-10-31 21:20:26 -0400

    intermediate revision - embedded shell provision in the Vagrantfile, added node and GD timings provision to test manifest

--- elk/README.md
+++ elk/README.md
@@ -1,2 +1,3 @@
 ### Info
-Replica of the repository  [comperiosearch/vagrant-elk-box](https://github.com/comperiosearch/vagrant-elk-box), modified to work with Puppet 4 / Vagrant 1.9.1 (W.I.P)
+
+Replica of the repository  [comperiosearch/vagrant-elk-box](https://github.com/comperiosearch/vagrant-elk-box), modified to work with Puppet 4 / Vagrant 1.9.1 (W.I.P) and to install GoDaddy [timings](https://github.com/godaddy/timings) API via Puppet `package` module using `npm` provier.
--- elk/manifests/default.pp
+++ elk/manifests/default.pp
@@ -1,26 +1,29 @@
-# Update APT Cache
-class { 'apt':
-# obsolete
-#  always_apt_update => true,
+include apt
+include java
+
+class { '::nodejs':
+  manage_package_repo       => false,
+  nodejs_dev_package_ensure => 'present',
+  npm_package_ensure        => 'present',
 }
 
+package { 'curl':
+  ensure  => 'present',
+  require => Class['apt'],
+}
 
-# Java is required
-class { 'java': }
-
-# Elasticsearch
 class { 'elasticsearch':
   manage_repo  => true,
   repo_version => '1.7',
 }
 
 elasticsearch::instance { 'es-01':
-  config => { 
+  config => {
   'cluster.name' => 'vagrant_elasticsearch',
   'index.number_of_replicas' => '0',
   'index.number_of_shards'   => '1',
   'network.host' => '0.0.0.0',
-  'marvel.agent.enabled' => false #DISABLE marvel data collection. 
+  'marvel.agent.enabled' => false #DISABLE marvel data collection.
   },        # Configuration hash
   init_defaults => { }, # Init defaults hash
   before => Exec['start kibana']
@@ -29,13 +32,12 @@ elasticsearch::instance { 'es-01':
 # elasticsearch::plugin{'royrusso/elasticsearch-HQ':
 #   instances  => 'es-01'
 # }
-# 
+#
 # elasticsearch::plugin{'elasticsearch/marvel/latest':
 #   instances  => 'es-01'
 # }
- 
 
-# Logstash
+
 class { 'logstash':
   # autoupgrade  => true,
   ensure       => 'present',
@@ -51,11 +53,6 @@ class { 'logstash':
 #}
 
 
-# Kibana
-package { 'curl':
-  ensure  => 'present',
-  require => [ Class['apt'] ],
-}
 
 file { '/opt/kibana':
   ensure => 'directory',
@@ -65,10 +62,18 @@ file { '/opt/kibana':
 
 exec { 'download_kibana':
   command => '/usr/bin/curl -L https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz | /bin/tar xvz -C /opt/kibana --strip-components 1',
-  require => [ Package['curl'], File['/opt/kibana'], Class['elasticsearch'] ],
+  require => [Package['curl'],File['/opt/kibana'],Class['elasticsearch']],
+  creates => '/opt/kibana/bin/kibana',
   timeout => 1800
 }
 
 exec {'start kibana':
-    command => '/etc/init.d/kibana start',
+  command => '/etc/init.d/kibana start',
+  require => Exec['download_kibana'],
+}
+
+package {['pm2','timings']:
+  ensure   => present,
+  provider => 'npm',
+  require  => Class['::nodejs'],
 }
