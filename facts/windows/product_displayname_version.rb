5fb0fe51907aed8c04fb4a4bfb9e456629d13ded
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-12-22 19:04:23 -0500

    inermediate revision  - fixed damaged indentation, logic and variable naming

--- facts/windows/product_displayname_version.rb
+++ facts/windows/product_displayname_version.rb
@@ -1,4 +1,44 @@
-fact_name = 'product_version'require 'win32/registry'        if Facter.value(:kernel) == 'windows'
-  def registry_uninstall_version(product_displayname)    product_version = nil    $access = Win32::Registry::KEY_READ | 0x100 # Win32::Registry::KEY_ALL_ACCESS    $key = ''
-    Win32::Registry::HKEY_LOCAL_MACHINE.open("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", $access ) do |reg_key| ;      reg_key.each_key do |subkey|        found_name = nil        Win32::Registry::HKEY_LOCAL_MACHINE.open("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\#{subkey}", $access ) do |reg_subkey|          reg_subkey.each do |name, type, data|            if name.eql?('DisplayVersion')               product_version = data            end            if name.eql?('DisplayName')                if data == product_displayname                  found_name = true                end            end          end          if found_name            break          end        end      end    end    product_version  end
-  Facter.add(fact_name) do    setcode do      filepath = 'C:\Program Files\Oracle\VirtualBox Guest Additions\VBoxControl.exe'      registry_uninstall_version('Puppet Agent (64-bit)')      # product_version '1.7.1'    end  endend  
+fact_name = 'product_version'
+require 'win32/registry'
+if Facter.value(:kernel) == 'windows'
+  def registry_uninstall_version(product_displayname)
+    product_version = nil
+    debug = false
+    $access = Win32::Registry::KEY_READ | 0x100
+    # Win32::Registry::KEY_ALL_ACCESS
+    # found_name = nil
+    Win32::Registry::HKEY_LOCAL_MACHINE.open("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", $access ) do |reg_key| ;
+      reg_key.each_key do |subkey|
+        found_name = nil
+        found_data = nil
+        Win32::Registry::HKEY_LOCAL_MACHINE.open("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\#{subkey}", $access ) do |reg_subkey|
+          reg_subkey.each do |value_name, value_type, value_data|
+            if value_name.eql?('DisplayVersion')
+              found_data = value_data
+            end
+            if value_name.eql?('DisplayName')
+              if value_data == product_displayname
+                found_name = true
+              end
+            end
+          end
+          if found_name
+            product_version = found_data
+            if debug
+              $stderr.puts subkey
+              $stderr.puts product_version
+            end
+            break
+          end
+        end
+      end
+    end
+    product_version
+  end
+  Facter.add(fact_name) do
+    setcode do
+      registry_uninstall_version('7-Zip 9.20')
+      # product_version = '9.20.00.0'
+    end
+  end
+end  
