16d0e4809acf1f7a76cc2e02a62eef0c89e826da
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-01-02 15:44:49 -0500

    initial revision -  use monkey patched command to examine and sanitize the XML, e.g. Jenkins build configurations

---
+++ spec/linux/jenkins_project2_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require_relative '../type/command'
+require 'yaml'
+require 'json'
+require 'csv'
+
+context 'Confirm able to load XML' do
+  config_dir = '/vagrant'
+  [
+    'good.xml',
+    'bad.xml'
+  ].each do |config|
+    xml = "#{config_dir}/#{config}"
+    describe command( "cat #{xml}") do
+      its(:stdout_as_xml) { should respond_to :version }
+    end
+  end
+end
