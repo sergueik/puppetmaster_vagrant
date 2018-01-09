76aee1f403363c26a170d0cb56bc20e79529876d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-01-08 19:38:56 -0500

    intermediate revision - added snapshot of the abandoned work

---
+++ mongodb/README.md
@@ -0,0 +1,3 @@
+### Info
+
+This directory contains Vagrantfile for setting up the replica set and authorization in Mongodb with the help of Puppet [wait_for](https://forge.puppet.com/basti1302/wait_for) resource (alternative is to use exported resources) and mongo scripting.
--- mongodb/manifests/default.pp
+++ mongodb/manifests/default.pp
@@ -6,6 +6,39 @@ class {'mongodb::globals':
   service_provider    => 'systemd',
   before              => Class['mongodb::server','mongodb::client'],
 }
+$strip_domain_suffix = false
+# Replica set
+$port = '27017'
+$cluser_members = ['mongo1.vagrant', 'mongo2:27017', 'mongo3:27017']
+$relicaset_members = flatten($cluster_members.map |Integer $index, String $value| {
+  $host = $value
+  if $strip_domain_suffix {
+    $res =  regsubst($host, '\..*$', '' , 'G')
+  } else {
+    $res = $host
+  }
+  $res
+})
+
+if $strip_domain_suffix {
+  $mongodb_configure_node = regsubst($::hostname, '\..*$','','G')
+} else { 
+  $mongodb_configure_node = $::hostname
+}
+$replicaset_name  = 'rs'
+$replicaset_members_expression = flatten($replicaset_members.map |Integer $index, String $value| {
+  $host = $value
+  if $index == 2 {
+    $res = "{ _id: $index,  host: \"${host}:${port}\", arbiterOnly: true}"
+  } else {
+    $res = "{ _id: $index,  host: \"${host}:${port}\"}"
+  }
+  $res
+}).join(',')
+
+$cluster_peers = flatten(delete(delete($cluster_members, $::hostname), "${::hostname}.local.vagrant").join(' ')
+# lists all members of the replica set, ignoring that arbiter never elevates to master
+$mongodb_replicaset_members = $replicaset_members.join('|')
 
 class {'mongodb::server':
   auth             => true,
@@ -13,7 +46,7 @@ class {'mongodb::server':
     replset_config => {
       'rs' => {
         ensure  => present,
-	members => ['mongo1:27017', 'mongo2:27017', 'mongo3:27017']
+	members => $members,
 	# NOTE:
 	# ==> mongo1: Warning: Can't connect to replicaset member mongo1:27017.
 	# ==> mongo1: Warning: Can't connect to replicaset member mongo2:27017.
