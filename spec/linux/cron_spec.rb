16101e66298a64644c4d73c90f01f8f3bcb05a7f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-01-10 19:44:17 +0000

    intermediate revision - added table check

--- spec/linux/cron_spec.rb
+++ spec/linux/cron_spec.rb
@@ -2,6 +2,8 @@
   # NOTE: there is no need to escape special characters
   # https://github.com/mizzy/specinfra/blob/master/lib/specinfra/command/base/cron.rb#L4
   describe cron do
+   
+    its(:table) { should match  /\* \* \* \* \* ls \/tmp/ } 
     it { should have_entry('*/5 * * * * /usr/bin/rsync -avz --delete --port 1873 --password-file=/opt/baswso2/rsync.secret baswso2@apim-api-manager-gateway-manager-0.node.qualified_primary_dc.consul::deployment_repo/ deployment_repo').with_user(cronjob_user) }
     # partial matches do not work because the underlyting command  enforces the full line match:    
     # https://github.com/mizzy/specinfra/blob/master/lib/specinfra/command/base/cron.rb#L5
