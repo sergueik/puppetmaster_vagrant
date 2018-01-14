066299f7d1b66a7a49abc066ac971643d01d97ef
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-01-13 23:59:00 -0500

    initial revision

---
+++ facts/linux/chromedriver_version.rb
@@ -0,0 +1,10 @@
+# inspired by https://github.com/jantman/puppet-facter-facts/blob/master/virtualenv_version.rb
+Facter.add("chromedriver_version") do
+  setcode do
+    begin
+      /^ChromeDriver\s+(\d+\.\d+\.\d+)\s+.*$/.match(Facter::Util::Resolution.exec('chromedriver --version 2>&1')).at(1)
+    rescue
+      false
+    end
+  end
+end
