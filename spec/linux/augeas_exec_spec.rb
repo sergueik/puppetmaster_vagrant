5a459393d320521e10f27a44446d4ad27b79530d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-02-23 04:39:07 +0000

    intermediate revision

--- spec/linux/augeas_exec_spec.rb
+++ spec/linux/augeas_exec_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
-context 'Tomcat' do
+context 'Tomcat Configuration' do
   context 'Use Puppet Augeas Commands to inspect the Tomcat server.xml' do
+    tcp_port = '8443'
+    catalina_home = '/apps/tomcat/current'
     describe command(<<-EOF
-      puppet apply -e 'augeas {"tomcat sever aug":  show_diff=> true, lens => "Xml.lns",  incl => "/apps/tomcat/current/conf/server.xml", changes => [  "set /files/apps/tomcat/current/conf/server.xml/Server/Service/Connector[#attribute/port=\\"8443\\"]/#text \\"    \\""],}'
+      puppet apply -e 'augeas {"tomcat sever aug": show_diff=> true, lens => "Xml.lns", incl => "#{catalina_home}/conf/server.xml", changes => [ "set /files#{catalina_home}/conf/server.xml/Server/Service/Connector[#attribute/port=\\"#{tcp_port}\\"]/#attribute/port \\"#{tcp_port}\\""],}'
     EOF
     ) do
+      # NOTE: using Puppet apply is a bad idea, since it leads to creation of new XML nodes in the target file when no matching nodes found
+      # e.g. <Connector></Connector>
       let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/bin'}
       its(:stdout) { should match /Notice: Applied catalog/ }
       its(:stderr) { should be_empty }
