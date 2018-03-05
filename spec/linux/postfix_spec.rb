1436716e7e95921efd7e9971bc2e189701c3459f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-03-04 23:59:07 -0500

    initial revision

---
+++ spec/linux/postfix_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+# origin: https://github.com/Pravkhande/serverspec/blob/master/Security/spec/CIS_audit/centos7_spec.rb
+context 'Postfix Local-Only Modtest' do  
+  postfix_state = command('systemctl status postfix').stdout
+  if postfix_state =~ Regexp.new(Regexp.escape('active (running)'))
+    describe port(25) do
+      it { should be_listening.with('127.0.0.1') }
+    end
+    # mongo insert tests
+  else
+    describe port(27017) do
+      it { should_not be_listening }
+    end
+    # mongoimport tests
+  end
+end
+
