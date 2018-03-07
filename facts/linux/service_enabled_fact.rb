24800c25dc81577be747b241512ff9afad2e6562
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-03-07 15:23:34 -0500

    intermediate revision - fixed the processing of the command output

--- facts/linux/service_enabled_fact.rb
+++ facts/linux/service_enabled_fact.rb
@@ -1,13 +1,13 @@
  #!/usr/bin/env ruby
 require 'facter'
 
-fact_name = 'mongod_service_is_running'
+fact_name = 'mongod_service_is_enabled'
 if Facter.value(:kernel) == 'Linux'
   service_name = 'mongod.service'
-  command = "/bin/systemctl status ${service_name}"
+  command = "/bin/systemctl list-unit-files| /bin/grep -E \"^#{service_name}\""
   command_output = Facter::Util::Resolution.exec(command)
   if ! command_output.nil?
-    result = command_output.split(/\r?\n/).grep(Regexp.escape('active (running)'))
+    result = command_output.split(/\r?\n/).grep(/^#{service_name}.*$/)
   end
 end
 if result != []
