de58ea4eed5b493fdf8d6a43388087fc7ad761d5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-03-08 21:07:01 +0000

    Update augeas_exec_spec.rb

--- spec/linux/augeas_exec_spec.rb
+++ spec/linux/augeas_exec_spec.rb
@@ -109,4 +109,72 @@ context 'Augeas' do
       its(:exit_status) {should eq 0 }
     end
   end
+
+ context 'SOLR Node' do
+      
+      
+            aug_script = '/tmp/test.aug'
+      
+      
+            xml_file = '/opt/app/wso2/apim/store/repository/conf/registry.xml'
+      
+      
+            
+      
+      
+            aug_path = "wso2registry/indexingConfiguration/lastAccessTimeLocation/#text"
+      
+      
+            aug_path_response = '/_system/local/repository/components/org.wso2.carbon.registry/indexing/lastaccesstime_\d{10}'
+      
+      
+            program=<<-EOF
+      
+      
+              set /augeas/load/xml/lens "Xml.lns"
+      
+      
+              set /augeas/load/xml/incl "#{xml_file}"
+      
+      
+              load
+      
+      
+              match /files#{xml_file}/#{aug_path} 
+      
+      
+            EOF
+      
+      
+            describe command(<<-EOF
+      
+      
+              echo '#{program}' > #{aug_script}
+      
+      
+              augtool -f #{aug_script}
+      
+      
+            EOF
+      
+      
+            ) do
+      
+      
+              let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/puppet/bin'}
+      
+      
+              its(:stdout) { should match /#{aug_path_response}/i }
+      
+      
+              its(:stderr) { should be_empty }
+      
+      
+              its(:exit_status) {should eq 0 }
+      
+      
+            end
+      
+      
+          end
 end
