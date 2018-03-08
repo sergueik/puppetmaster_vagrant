4af867d7500669ee44ed9aec7c728ba87ec7fdf3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-03-08 21:10:32 +0000

    intermediate revision added approxumate node #text  value test

--- spec/linux/augeas_exec_spec.rb
+++ spec/linux/augeas_exec_spec.rb
@@ -110,71 +110,27 @@ context 'Augeas' do
     end
   end
 
- context 'SOLR Node' do
-      
-      
-            aug_script = '/tmp/test.aug'
-      
-      
-            xml_file = '/opt/app/wso2/apim/store/repository/conf/registry.xml'
-      
-      
-            
-      
-      
-            aug_path = "wso2registry/indexingConfiguration/lastAccessTimeLocation/#text"
-      
-      
-            aug_path_response = '/_system/local/repository/components/org.wso2.carbon.registry/indexing/lastaccesstime_\d{10}'
-      
-      
-            program=<<-EOF
-      
-      
-              set /augeas/load/xml/lens "Xml.lns"
-      
-      
-              set /augeas/load/xml/incl "#{xml_file}"
-      
-      
-              load
-      
-      
-              match /files#{xml_file}/#{aug_path} 
-      
-      
-            EOF
-      
-      
-            describe command(<<-EOF
-      
-      
-              echo '#{program}' > #{aug_script}
-      
-      
-              augtool -f #{aug_script}
-      
-      
-            EOF
-      
-      
-            ) do
-      
-      
-              let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/puppet/bin'}
-      
-      
-              its(:stdout) { should match /#{aug_path_response}/i }
-      
-      
-              its(:stderr) { should be_empty }
-      
-      
-              its(:exit_status) {should eq 0 }
-      
-      
-            end
-      
-      
-          end
+ context 'Text of the node' do
+      # generated text attriute we do now know in advance the exact value
+      aug_script = '/tmp/test.aug'
+      xml_file = '/opt/app/wso2/apim/store/repository/conf/registry.xml'
+      aug_path = "wso2registry/indexingConfiguration/lastAccessTimeLocation/#text"
+      aug_path_response = '/_system/local/repository/components/org.wso2.carbon.registry/indexing/lastaccesstime_\d{10}'
+      program=<<-EOF
+        set /augeas/load/xml/lens "Xml.lns"
+        set /augeas/load/xml/incl "#{xml_file}"
+        load
+        match /files#{xml_file}/#{aug_path} 
+      EOF
+      describe command(<<-EOF
+        echo '#{program}' > #{aug_script}
+        augtool -f #{aug_script}
+      EOF
+      ) do
+        let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/puppet/bin'}
+        its(:stdout) { should match /#{aug_path_response}/i }
+        its(:stderr) { should be_empty }
+        its(:exit_status) {should eq 0 }
+      end
+   end
 end
