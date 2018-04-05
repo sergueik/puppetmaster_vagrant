efcce5b319e78bbc9eaa461b2e39c1e472eb1cea
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-04-05 16:39:34 -0400

    intermediate revision - trying to swith from YAML to JSON with parsing consul API response

--- spec/linux/consul_extended_spec.rb
+++ spec/linux/consul_extended_spec.rb
@@ -1,29 +1,50 @@
 require 'spec_helper'
+require 'json'
 require 'yaml'
 require 'ostruct'
 
-$DEBUG = false
+$DEBUG = true
 
-# this example may be considred a bulding block for 
-# specinfra  monitored_by? - style expectaton regarding consul
+# this example may be considred a bulding block for specinfra  monitored_by? -style expectaton with consul
 
 context 'Consul' do
   context 'Service checks' do
     {
       'web' => 80,
     }.each do |monitored_service_name, tcp_port|
+
       api_command = "curl http://localhost:8500/v1/catalog/service/#{monitored_service_name} | /usr/bin/jq '.'"
       # mock
       consul_service_config_file = "/etc/consul.d/service_#{monitored_service_name}.json"
       # mock
       consul_service_config_file = "/tmp/data.json"
-      api_command = "/usr/bin/jq '.' #{consul_service_config_file}"
-      $stderr.puts "command: " + api_command if $DEBUG
-      service_object_data = command(api_command).stdout
-      $stderr.puts "raw data: " + service_object_data if $DEBUG
+      api_command = "/usr/bin/jq '.' #{consul_service_config_file}" 
+      $stderr.puts "api_command: " + api_command if $DEBUG
+
+      service_object_data = nil
+      begin 
+        service_object_data = command(api_command).stdout 
+        $stderr.puts "raw service_object_data: " + service_object_data if $DEBUG
+      rescue Exception => e
+        $stderr.puts 'Exception: ' + e.to_s
+      end
       begin
+
+        $stderr.puts 'Deserializing data' if $DEBUG
         # https://stackoverflow.com/questions/6423484/how-do-i-convert-hash-keys-to-method-names
-        service_object = YAML.load(service_object_data)
+        begin 
+          $stderr.puts 'Tryin JSON parse' if $DEBUG
+          service_object = JSON.parse(service_object_data)
+        rescue Exception => e
+          $stderr.puts 'Exception: ' + e.to_s
+        end
+       
+        begin 
+          $stderr.puts 'Tryin YAML load' if $DEBUG
+          service_object = YAML.load(service_object_data)
+        rescue Exception => e
+          $stderr.puts 'Exception: ' + e.to_s
+        end
       rescue Exception => e
         $stderr.puts 'Exception: ' + e.to_s
       end
