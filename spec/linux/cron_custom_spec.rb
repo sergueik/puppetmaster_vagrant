af70deae6d65003ff63f5b5ad4a6dfe5342d46fc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-04-15 23:01:58 -0400

    updated the example based on the commit in mizzy/specinfra e01b780 to check all cron tables

--- spec/linux/cron_custom_spec.rb
+++ spec/linux/cron_custom_spec.rb
@@ -12,7 +12,7 @@ context 'Customized cron resource' do
     # crontab entry
     # 0 0 * * * find /usr/share/tomcat/logs/ -type f -mtime +30 -exec rm -rf {} \;
     # passing shell command
-    # crontab -l | grep '0 0 \* *\* *\* *find */usr/share/tomcat/logs/ *\- *type *f *\-mtime *+30 *\-exec *rm *\-rf *{} *\\;'
+    # cat /etc/cron.d/* /etc/crontab /var/spool/cron/* /var/spool/cron/crontabs/* 2> /dev/null | grep '0 0 \* *\* *\* *find */usr/share/tomcat/logs/ *\- *type *f *\-mtime *+30 *\-exec *rm *\-rf *{} *\\;'
     # serverspec
     it { should have_entry '0 0 * * * find /usr/share/tomcat/logs/ -type f -mtime +30 -exec rm -rf {} \;' }
   end
@@ -21,7 +21,8 @@ context 'Customized cron resource' do
     # crontab entry
     # 0 0 * * * /bin/find /tmp/repository/logs/ ! \\( -name "patches.*" -o -name "audit.*" \\) -type f -mtime +30 -exec rm -rf {} \\;
     # passing shell command
-    # crontab -l | grep '0 0 \* \* \* /bin/find /tmp/repository/logs/ ! \\\\( \-name \"patches.\*\" \-o \-name \"audit.\*" \\\\) \-type f \-mtime +30 \-exec rm \-rf {} \\\\;'
+    # instead of `crontab -l` one may explicitly list all cron directories
+    # cat /etc/cron.d/* /etc/crontab /var/spool/cron/* /var/spool/cron/crontabs/* 2> /dev/null | grep '0 0 \* \* \* /bin/find /tmp/repository/logs/ ! \\\\( \-name \"patches.\*\" \-o \-name \"audit.\*" \\\\) \-type f \-mtime +30 \-exec rm \-rf {} \\\\;'
     # serverspec expectations.
     # None: one still has to escape the backslashes
     xit { should have_entry '0 0 * * * /bin/find /tmp/repository/logs/ ! \\( -name "patches.*" -o -name "audit.*" \\) -type f -mtime +30 -exec rm -rf {} \\;' }
