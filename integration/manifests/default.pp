26110fc9b88910b62a15b3b93b2ac5aef8b8583f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-01 21:37:54 -0400

    intermediate revision - added xml_fragment test

--- integration/manifests/default.pp
+++ integration/manifests/default.pp
@@ -2,5 +2,37 @@
 # vi: set ft=puppet :
 node 'default' {
   include urugeas
+  # take 2
+  xml_fragment { '/web-app/filter':
+    path    => '/usr/share/tomcat/conf/web.xml',
+    xpath   => '/web-app/filter',
+    # '/usr/share/tomcat/conf/web.xml' is part of tomcat7 install
+    content => {
+      value => '',
+    }
+  }
+  xml_fragment { '/web-app/filter/filter-name':
+    path    => '/usr/share/tomcat/conf/web.xml',
+    xpath   => '/web-app/filter/filter-name',
+    require => Xml_fragment['/web-app/filter'],
+    content => {
+      value => 'httpHeaderSecurity',
+    }
+  }
+  xml_fragment { '/web-app/filter/filter-class':
+    path    => '/usr/share/tomcat/conf/web.xml',
+    xpath   => '/web-app/filter/filter-class',
+    require => Xml_fragment['/web-app/filter'],
+    content => {
+      value =>'org.apache.catalina.filters.HttpHeaderSecurityFilter',
+    }
+  }
+  xml_fragment { '/web-app/filter/async-supported':
+    path    => '/usr/share/tomcat/conf/web.xml',
+    xpath   => '/web-app/filter/async-supported',
+    require => Xml_fragment['/web-app/filter'],
+    content => {
+      value =>'true',
+    }
+  }
 }
-
