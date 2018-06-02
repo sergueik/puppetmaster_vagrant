d95e86decfc561ed93a9ef564ac65c968b49c38a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-02 17:24:51 -0400

    intermediate revision - refactord following puppet style guide

--- integration/manifests/default.pp
+++ integration/manifests/default.pp
@@ -1,38 +1,35 @@
 # -*- mode: puppet -*-
 # vi: set ft=puppet :
 node 'default' {
-  include urugeas
-  # take 2
-  xml_fragment { '/web-app/filter':
-    path    => '/usr/share/tomcat/conf/web.xml',
-    xpath   => '/web-app/filter',
-    # '/usr/share/tomcat/conf/web.xml' is part of tomcat7 install
-    content => {
-      value => '',
-    }
-  }
-  xml_fragment { '/web-app/filter/filter-name':
-    path    => '/usr/share/tomcat/conf/web.xml',
-    xpath   => '/web-app/filter/filter-name',
-    require => Xml_fragment['/web-app/filter'],
-    content => {
-      value => 'httpHeaderSecurity',
-    }
-  }
-  xml_fragment { '/web-app/filter/filter-class':
-    path    => '/usr/share/tomcat/conf/web.xml',
-    xpath   => '/web-app/filter/filter-class',
-    require => Xml_fragment['/web-app/filter'],
-    content => {
-      value =>'org.apache.catalina.filters.HttpHeaderSecurityFilter',
-    }
-  }
-  xml_fragment { '/web-app/filter/async-supported':
-    path    => '/usr/share/tomcat/conf/web.xml',
-    xpath   => '/web-app/filter/async-supported',
-    require => Xml_fragment['/web-app/filter'],
-    content => {
-      value =>'true',
-    }
+  # include urugeas
+  # https://puppet.com/docs/puppet/5.3/style_guide.html
+  xml_fragment {
+    default:
+      # NOTE: '/usr/share/tomcat/conf/web.xml' is part of tomcat7 install. not declared in the manifest
+      path    => '/usr/share/tomcat/conf/web.xml',;
+    '/web-app/filter':
+      xpath   => '/web-app/filter',
+      content => {
+        value => '',
+      },
+      before  => Xml_fragment['/web-app/filter/filter-name','/web-app/filter/filter-class','/web-app/filter/async-supported'];
+    '/web-app/filter/filter-name':
+      xpath   => '/web-app/filter/filter-name',
+      # require => Xml_fragment['/web-app/filter'],
+      content => {
+        value => 'httpHeaderSecurity',
+      },;
+    '/web-app/filter/filter-class':
+      xpath   => '/web-app/filter/filter-class',
+      # require => Xml_fragment['/web-app/filter'],
+      content => {
+        value =>'org.apache.catalina.filters.HttpHeaderSecurityFilter',
+      },;
+    '/web-app/filter/async-supported':
+      xpath   => '/web-app/filter/async-supported',
+      # require => Xml_fragment['/web-app/filter'],
+      content => {
+        value =>'true',
+      },;
   }
 }
