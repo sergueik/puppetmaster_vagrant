ee860c7501d3fd5a5888ee65bcdce6990248cbba
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-16 22:46:19 -0400

    intermediate revsion - added expectation for logging

--- spec/linux/tomcat_spec.rb
+++ spec/linux/tomcat_spec.rb
@@ -1,17 +1,36 @@
 require 'spec_helper'
+require 'rexml/document'
+include REXML
 
 context 'Tomcat in Java Proces List' do
-  context 'Bootstrp process check' do
+  catalina_home = '/usr/share/tomcat'
+
+  context 'Bootstrap process check' do
     catalina_bootstrap_class = 'org.apache.catalina.startup.Bootstrap'
     jps_class = 'sun.tools.js.Jps'
     describe command('jps -m') do
-      its(:stdout) { should contain 'Bootstrap start'}
-      its(:exit_status) { should_not be 0}
+      its(:stdout) { should contain 'Bootstrap start',
+      its(:exit_status) { should_not be 0,
     end
     describe command('jps -ml') do
-      its(:stdout) { should match /#{catalna_bootstrap_class} start/}
-      its(:stdout) { should contain jps_clas }
-      its(:exit_status) { should_not be 0}
+      its(:stdout) { should match /#{catalna_bootstrap_class, start/,
+      its(:stdout) { should contain jps_clas ,
+      its(:exit_status) { should_not be 0,
+    end
+  end
+  context 'Logging' do
+    # origin: https://github.com/T-Systems-MMS/tomcat-baseine/blob/master/controls/tomcat.rb
+    # conveted from 'inspec' syntax
+    describe file("#{catalina_home}/conf/logging.properties") do
+      [
+        '.handlers = 1catalina.org.apache.juli.FileHandler, java.util.logging.ConsoleHandler',
+        '1catalina.org.apache.juli.FileHandler.level = FINE',
+        '2localhost.org.apache.juli.FileHandler.level = FINE',
+        'java.util.logging.ConsoleHandler.level = FINE',
+        '.level = INFO'
+      ].each do |line|
+      its(:content) { is_expected.to match line }
+      end
     end
   end
 end
