8883e61bd7e063eb8b0996899ca7c29ca571dca1
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-17 12:29:58 -0400

    intermediate revision - misc. fixes and variations of valid conversions

--- spec/windows/environment_spec.rb
+++ spec/windows/environment_spec.rb
@@ -3,41 +3,68 @@ require_relative '../windows_spec_helper'
 # expectations that the package install location is added to system PATH in Windows
 context 'Environment' do
 
-  install_location = 'c:/opscode/chef/bin'
+  install_location = 'C:/Program Files/Puppet Labs/Puppet/bin'
+  registry_key = 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment'
 
-  describe command (<<-EOF
-    write-output ((Get-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment' ).Path -replace '\\\\', '/' )
-  EOF
-  ) do
-      its(:stdout) { should match Regexp.new(install_location, Regexp::IGNORECASE) }
-    end
-  describe command (<<-EOF
-
+  [
+    registry_key,
+    registry_key.gsub(/\\\\/,'/'),
+  ].each do |regkey|
+    describe command (<<-EOF
+      write-output ((Get-ItemProperty -Path '#{regkey}' ).Path -replace '\\\\', '/' )
+    EOF
+    ) do
+        its(:stdout) { should match Regexp.new(install_location, Regexp::IGNORECASE) }
+      end
+  end
   describe command ('([Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)) -replace "\\\\", "/" ') do
     its(:stdout) { should match Regexp.new(install_location, Regexp::IGNORECASE) }
   end
 
   # NOTE: backslashes translated
   install_location_converted = install_location.gsub('\\','/')
-  describe command ('([Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)) -replace "\\\\", "/" ') do
+  describe command ('([Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)) -replace "\\\\", "/"') do
      its(:stdout) { should match Regexp.new(install_location_converted, Regexp::IGNORECASE) }
   end
 
-  # note differences in registry hive / path formatting syntax between Ruby and Powershell
-
-  describe windows_registry_key("HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment") do
-    it do
-      should exist
-      should have_property('Path', :type_string)
-      should have_property_value( 'OS', :type_string_converted, 'Windows_NT' )
+  # NOTE: differences in registry hive / path formatting syntax between Ruby and Powershell
+  [
+    registry_key,
+    registry_key.gsub('HKLM','HKEY_LOCAL_MACHINE'),
+  ].each do |wrong_formatted_key|
+    describe windows_registry_key(wrong_formatted_key) do
+      it { should_not exist }
     end
   end
-  describe windows_registry_key('HKLM\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine') do
-    # does not work with :type_string_converted
-    it { should have_property_value('PowerShellVersion' , :type_string ,'4.0' ) }
+
+  [
+    registry_key,
+    registry_key.gsub(/\\\\/,'/'),
+  ].each do |regkey|
+    describe windows_registry_key(regkey.gsub('HKLM:','HKEY_LOCAL_MACHINE')) do
+      it do
+        should exist
+        should have_property('Path', :type_expandstring ) # ExpandString
+        should have_property_value( 'OS', :type_string_converted, 'Windows_NT' )
+      end
+    end
+    describe windows_registry_key(regkey.gsub('HKLM:','HKLM')) do
+      it do
+        should exist
+        should have_property('Path', :type_expandstring )
+        should have_property_value( 'OS', :type_string_converted, 'Windows_NT' )
+      end
+    end
   end
 
-  describe windows_registry_key("HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment") do
+  # NOTE: this does not work with '/' path separators
+  describe windows_registry_key('HKLM\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine') do
+    # NOTE: this does not work with :type_string_converted
+    it { should have_property_value('PowerShellVersion', :type_string ,'4.0' ) }
+  end
+
+  # NOTE: this test requires custom Specinfra with property_value_containing method
+  describe windows_registry_key(registry_key) do
 
     let(:value_check) do
       { :name  => 'OS',
@@ -46,7 +73,6 @@ context 'Environment' do
      }
     end
     # it { should have_property_value( value_check ) }
-    # requires custom specinfra.gem to support property_value_containing
     # xit { should have_property_valuecontaining( 'Path', :type_string_converted, 'c:\\\\windows' ) }
   end
 
