98d0cf7154b8cb9c08eec788334f8b25e5a240ff
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-18 16:31:01 -0400

    intermediate revision - added custom log apache configuration test with Ruby JSON, that is not working yet

--- spec/linux/apache_log_spec.rb
+++ spec/linux/apache_log_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
+require 'json'
+require 'ostruct'
+
+$DEBUG = true
 
 context 'Splunk Logging' do
+  log_format = 'json'
   context 'Virtual Host settings' do
     # Verifies presence of the line
     # CustomLog "logs/access-splunk-json.log" json
@@ -22,17 +27,51 @@ context 'Splunk Logging' do
   # in '/etc/httpd/conf/httpd.conf'
   # NOTE:  in real life scenario many more fields
   context 'Global Settings' do
-    log_format = 'json'
-    describe command(<<-EOF
-      sed -n 's/LogFormat \\(.*\\) #{log_format}$/echo \\1 | jq -M "."/p' '/etc/httpd/conf/httpd.conf' > /tmp/check.sh
-      /bin/sh /tmp/check.sh
-    EOF
-    ) do
-      let(:path) { '/bin:/usr/bin:/usr/local/bin:/opt/opedj/bin'}
-      its(:exit_status) { should eq 0 }
-      # include as many domain-specific fields from jq output as needed
-      its(:stdout) { should match Regexp.new('"protocol": "%H"', Regexp::IGNORECASE) }
-      its(:stderr) { should be_empty }
+    context 'Processing with Shell and jq' do
+      describe command(<<-EOF
+        sed -n 's/LogFormat \\(.*\\) #{log_format}$/echo \\1 | jq -M "."/p' '/etc/httpd/conf/httpd.conf' > /tmp/check.sh
+        /bin/sh /tmp/check.sh
+      EOF
+      ) do
+        let(:path) { '/bin:/usr/bin:/usr/local/bin:/opt/opedj/bin'}
+        its(:exit_status) { should eq 0 }
+        # include as many domain-specific fields from jq output as needed
+        its(:stdout) { should match Regexp.new('"protocol": "%H"', Regexp::IGNORECASE) }
+        its(:stderr) { should be_empty }
+      end
     end
+    # this will break with numerous 
+    # Exception `IO::EAGAINWaitReadable' at /root/.gem/ruby/2.1.0/gems/specinfra-2.73.0/lib/specinfra/backend/exec.rb:102 - 
+    # Resource temporarily unavailable - 
+    # read would block
+    # need to test under uru/Ruby 2.33
+    context 'Processing with Ruby JSON module' do
+      extract_command = "sed -n 's/LogFormat \\(.*\\) #{log_format}$/\\1/p' '/etc/httpd/conf/httpd.conf'"
+      begin
+        config_data = command(extract_command).stdout
+        $stderr.puts 'raw config_data: ' + config_data if $DEBUG
+      rescue => e
+        $stderr.puts 'Exception from running the command: ' + e.to_s
+      end
+      $stderr.puts 'Deserializing data' if $DEBUG
+      begin
+        $stderr.puts 'Trying JSON parse' if $DEBUG
+        service_object = JSON.parse(config_data)
+      rescue Exception => e
+        $stderr.puts 'Exception from JSON parse: ' + e.to_s
+        service_object = "{\"protocol\" : \"%H\"}"
+      end
+      if ! service_object.nil?
+        $stderr.puts 'Configuration: ' + service_object.class.name if $DEBUG
+        describe 'Test' do
+          # subject { OpenStruct.new service_object['protocol'] }
+          # undefined method `each_pair' for "protocol":String
+          # subject { OpenStruct.new service_object }
+          # undefined method `each_pair' for "{\"protocol\" : \"%H\"}":String
+          subject { service_object }
+          its(:protocol) { should be '%H' }
+        end
+      end
+    end  
   end
 end
