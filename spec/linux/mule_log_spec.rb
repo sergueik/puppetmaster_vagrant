53ae6d5673c16bcf70a9765f2e0100a2dc47ebe6
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-26 14:37:38 -0400

    intermetiate revision - added node regarding naive expectation

--- spec/linux/mule_log_spec.rb
+++ spec/linux/mule_log_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
-
 context 'Mule Enterprise Log files' do
+
   log_path = '/usr/share/mulee/logs'
   log_line = 'Valid license key --> Evaluation = false, Expiration Date ='
+
+  # NOTE: mule is usually configured with log rotation
+  # the following nive expectation would fail:
+  describe file("#{log_path}/mule_ee.log") do
+    its(:content) {  should match log_line }
+  end
   file_count = 0
   [
     'mule_ee.log',
@@ -25,8 +31,10 @@ context 'Mule Enterprise Log files' do
   describ(file_count) do
     it {should be > 0 }
   end
+
+  # alternatively one can use grep command to find license information.
+  # in one of the logs contain
   describe command("grep -il #{log_line} mule_ee.log mule_ee.log.*") do
     its(:exit_status) {should eq 0}
   end
 end
-
