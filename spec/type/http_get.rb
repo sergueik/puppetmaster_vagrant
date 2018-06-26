cc8e2095de83eee5319e35d567a0bdaac65669b3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-26 15:56:39 -0400

    intermediate revision - added  paremt project changes, added note to similar inspec class

--- spec/type/http_get.rb
+++ spec/type/http_get.rb
@@ -1,5 +1,6 @@
 # based on: https://github.com/jantman/serverspec-extended-types/blob/master/lib/serverspec_extended_types/http_get.rb
-
+# see also the `http` InSpec audit resource
+# https://github.com/inspec/inspec/blob/master/lib/resources/http.rb
 require 'faraday'
 require 'json'
 
@@ -7,6 +8,9 @@ module Serverspec
   module Type
 
     class Http_Get < Base
+
+      # Http status codes that are tested against for redirect test
+      @@redirect_codes = [301, 302, 307, 308]
 
       def initialize(port, host_header, path, protocol = 'http', bypass_ssl_verify = false, timeout_sec = 10)
         @ip = ENV['TARGET_HOST'] || 'localhost'
@@ -16,6 +20,8 @@ module Serverspec
         @port = port
         @protocol = protocol
         @host = host_header
+        @redirects = false
+        @redirect_path = nil
         @path = path
         @timed_out_status = false
         @content_str = nil
@@ -50,6 +56,8 @@ module Serverspec
         @response_code_int = response.status
         @content_str = response.body
         @headers_hash = Hash.new('')
+        @redirects = @@redirect_codes.include? @response_code_int
+        @redirect_path = @redirects ? @headers_hash['location'] : nil
         response.headers.each do |header, val|
           @headers_hash[header] = val
         end
@@ -84,6 +92,33 @@ module Serverspec
       def body
         @content_str
       end
+
+      # Whether or not it redirects to some other page
+      #
+      # @example
+      #   describe http_get(80, 'myhostname', '/') do
+      #     it { should be_redirected_to 'https://myhostname/' }
+      #   end
+      #
+      # @api public
+      # @return [Boolean]
+      def redirected_to? (redirect_path)
+        @redirects and @redirect_path == redirect_path
+      end
+
+      # Whether or not it redirects to any other page
+      #
+      # @example
+      #   describe http_get(80, 'myhostname', '/') do
+      #     it { should be_redirected }
+      #   end
+      #
+      # @api public
+      # @return [Boolean]
+      def redirected?
+        @redirects
+      end
+
 
       private :getpage
     end
