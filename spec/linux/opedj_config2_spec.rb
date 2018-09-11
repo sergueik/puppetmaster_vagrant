39c2c35237f39e412684405205789bc7e2d1f797
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-09-11 11:47:01 -0400

    intermediate revision - added backend index list opendj command mockup, some documentation

--- spec/linux/opedj_config2_spec.rb
+++ spec/linux/opedj_config2_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
-context 'Opendj config' do
+context 'Advanced opendj config' do
+  # This serverspec exercises one-liner commands used for Puppet to deal with opendj application configuration
   # https://backstage.forgerock.com/docs/ds/5/configref/index.html?global.html
-  state = 'true'
+  # where often a multi property objects (backends, indexes, etc.) are created or removed
+  # and properties or existing objects are changed
+  # based on intelligent presence checks
+  # A frequent alterative of creating a file on the target node which contents is
+  # a hash of the hieradata describing the target object is not very flexible
+  # Testing such snippet in real provision is inefficient, with some part of runs failing due to
+  # inevitable errors in the shell scripts serving as "unless" and "onlyif" conditons to Puppet exec resources
   entry_cache_name = 'Soft Reference'
   entry_cache_type = 'soft-reference'
   entry_cache_level = 2
@@ -10,26 +17,37 @@ context 'Opendj config' do
   entry_exclude_filter = '{objectClass=groupOfUniqueNames}'
   entry_include_filter = '-'
   debug = true
+  backend_index_name = 'uniqueMember'
+  backend_index_type = 'equality'
+  backend_index_entry_limit = 5000
+
   # mock dsconfig command output
   entry_list_datafile = '/tmp/entry_list.txt'
   entry_prop_datafile = '/tmp/entry_prop.txt'
+  backend_index_list_datafile = '/tmp/backend_index_list.txt'
   before(:each) do
     Specinfra::Runner::run_command( <<-EOF
     # mocking the dsconfig list-entry-caches command
       cat <<END>#{entry_list_datafile}
-Entry Cache    : Type           : cache-level : enabled
----------------:----------------:-------------:--------
-FIFO           : fifo           : 1           : false
-#{entry_cache_name} : #{entry_cache_type} : #{entry_cache_level}           : #{entry_cache_enabled}
+Entry Cache         : Type                : cache-level          : enabled
+--------------------:---------------------:----------------------:--------
+FIFO                : fifo                : 1                    : false
+#{entry_cache_name} : #{entry_cache_type} : #{entry_cache_level} : #{entry_cache_enabled}
 END
       # mocking the dsconfig get-entry-cache-prop command
       cat <<END>#{entry_prop_datafile}
-Property      : Value(s)
+Property       : Value(s)
 ---------------:----------------:-------------:--------
 cache-level    : #{entry_cache_level}
 enabled        : #{entry_cache_enabled}
 include-filter : -
 exclude-filter : #{entry_exclude_filter}
+END
+    # mocking the dsconfig list-backend-indexes command
+      cat <<END>#{backend_index_list_datafile}
+Backend Index         : index-type        : index-entry-limit            : index-extended-matching-rule : confidentiality-enabled
+----------------------:-------------------:------------------------------:------------------------------:-------------------------
+#{backend_index_name} : #{backend_index_type} : #{backend_index_entry_limit} : -                            : false
 END
   EOF
   )
@@ -63,4 +81,3 @@ END
     its(:exit_status) { should eq 0 }
   end
 end
-
