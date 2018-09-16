6c74cd9ef1c07317c94066becb9ce079946b950f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-09-15 21:08:29 -0400

    intermediate revision - exploring the alternative layout of inspec

--- inspec_test/test/integration/default/default_spec.rb
+++ inspec_test/test/integration/default/default_spec.rb
@@ -1,12 +1,21 @@
 describe file('/tmp/file') do
   it { should exist }
-  it { should be_file }
-  it { should_not be_directory }
-  it { should_not be_block_device }
-  it { should_not be_character_device }
-  it { should_not be_pipe }
-  it { should_not be_socket }
-  it { should_not be_symlink }
-  it { should_not be_mounted }
+  it 'test file attributes' do  
+    should be_file
+    should_not be_directory 
+    should_not be_pipe 
+    should_not be_socket 
+    should_not be_symlink 
+  end
+  # it { should_not be_block_device }
+  # it { should_not be_character_device }
+  # it { should_not be_mounted }
   its('content') { should eq 'hello world' }
+end
+# copied from test/integration/profile/controls/default.rb
+describe package('vim-minimal') do
+  it { should be_installed }
+  # it { should be_installed.with_version('123') }
+  # [FAIL]  undefined method `with_version' for #<RSpec::Matchers::DSL::Matcher be_installed>
+  its('version') { should eq '7.4.629-5.el6' }
 end
