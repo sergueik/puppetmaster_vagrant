4899f6540c46c3f1840cc31670d56f9257bc122a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-09-18 00:27:15 -0400

    intermediate revision - formatted systemctl output tokenizing the rhs

--- spec/linux/use_iconv_spec.rb
+++ spec/linux/use_iconv_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 context 'Sysctl tests' do
+  service_unit = 'httpd'
   # default output of systemctl needs unicode sanitation
   context 'Converting output unicode clean' do
-    describe command('systemctl status httpd | iconv --from-code UTF8 --to-code US-ASCII -c -') do
+    describe command("systemctl status '#{service_unit}' | iconv --from-code UTF8 --to-code US-ASCII -c -") do
       its(:stdout) { should contain Regexp.new(Regexp.escape('* httpd.service - The Apache HTTP Server')) }
     end
     begin
       # the next expectation may fail and abort the process when locale is wrong
-      describe command('systemctl status httpd') do
-        its(:stdout) { should contain Regexp.new(Regexp.escape('* httpd.service - The Apache HTTP Server')) }
+      describe command("systemctl status #{service_unit}") do
+        its(:stdout) { should contain Regexp.new(Regexp.escape("* httpd.service - The Apache HTTP Server")) }
       end
     rescue => e
       $stderr.puts 'Exception (ignored) ' + e.to_s
@@ -19,19 +20,23 @@ context 'Sysctl tests' do
   # alternatively command systemctl to format output in the list format
   context 'Structural output processing' do
     info = {
-      'Description' => 'The Apache HTTP Server',
-      'LoadState'   => 'loaded',
-      'ActiveState' => 'active', # 'inactive'  or 'failed' when stopped,
-      'After'       => 'system.slice remote-fs.target tmp.mount network.target basic.target -.mount nss-lookup.target systemd-journald.socket',
-      # 'Environment' =>   not in default systemctl configuration of Apache httpd
+      'Description'     => 'The Apache HTTP Server',
+      'LoadState'       => 'loaded',
+      'ActiveState'     => 'active', # 'inactive'  or 'failed' when stopped,
+      'After'           => 'system.slice remote-fs.target tmp.mount network.target basic.target -.mount nss-lookup.target systemd-journald.socket',
+      # 'Environment'     =>   not in default systemctl configuration of Apache httpd
+      # often is configured via KEY=VALUE array in /etc/systemd/system/httpd.service. These get concatenated in the systemctl show output
       'EnvironmentFile' => '/etc/sysconfig/httpd (ignore_errors=no)',
       # NOTE: the actual sysctl output will contain pid and start_time making the include? fail
-      # 'ExecStart'   => '{ path=/usr/sbin/httpd ; argv[]=/usr/sbin/httpd $OPTIONS -DFOREGROUND ; ignore_errors=no ; start_time=[n/a] ; stop_time=[n/a] ; pid=0 ; code=(null) ; status=0/0 }',
-      'ExecReload'  => '{ path=/usr/sbin/httpd ; argv[]=/usr/sbin/httpd $OPTIONS -k graceful ; ignore_errors=no ; start_time=[n/a] ; stop_time=[n/a] ; pid=0 ; code=(null) ; status=0/0 }',
-      'ExecStop'    => '{ path=/bin/kill ; argv[]=/bin/kill -WINCH ${MAINPID} ; ignore_errors=no ; start_time=[n/a] ; stop_time=[n/a] ; pid=0 ; code=(null) ; status=0/0 }',
+      # 'ExecStart'       => '{ path=/usr/sbin/httpd ; argv[]=/usr/sbin/httpd $OPTIONS -DFOREGROUND ; ignore_errors=no ; start_time=[n/a] ; stop_time=[n/a] ; pid=0 ; code=(null) ; status=0/0 }',
+      'ExecReload'      => '{ path=/usr/sbin/httpd ; argv[]=/usr/sbin/httpd $OPTIONS -k graceful ; ignore_errors=no ; start_time=[n/a] ; stop_time=[n/a] ; pid=0 ; code=(null) ; status=0/0 }',
+      'ExecStop'        => '{ path=/bin/kill ; argv[]=/bin/kill -WINCH ${MAINPID} ; ignore_errors=no ; start_time=[n/a] ; stop_time=[n/a] ; pid=0 ; code=(null) ; status=0/0 }',
     }
     info_regexp = '(:?' + info.keys.join('|') + ')'
-    command = "systemctl --no-page show httpd | grep -E '#{info_regexp}=' "
+    # some settings e.g. Environment for java launchers typically have multiple tokens -Dkey=value in the rhs separated by a space
+    # systemctl show uses \x20 for those settings to indicate whitespaces in the output
+    # for clean expectation expression it is probably better to tokenize the output keeping one token per line
+    command = "systemctl --no-page -o cat show '#{service_unit}' | sed 's|\\x20|\\n|g' | grep -E '#{info_regexp}=' "
     context 'Exact Match Evaluation' do
       service_info = command(command).stdout
       $stderr.puts "inspecting:\n------\n#{service_info}\n------\n"
