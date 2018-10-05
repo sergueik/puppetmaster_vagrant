b9a884536f5c86fa45bc3ec9820ff212c365fa17
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-10-04 21:52:29 -0400

    intermediate revision - added xmlstarlet spec

---
+++ spec/linux/xmlstarlet_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+require 'rexml/document'
+include REXML
+
+# when multiple nodes matching the xpath the xmllint collapsies the text into the single string,
+# which makes developing expectations somewhat more challenging
+# xmlstarlet which is available has a better output formatting
+context 'xmlstartlet' do
+  context 'attributes' do
+    datafile = '/tmp/attributes.xml'
+    before(:each) do
+      Specinfra::Runner::run_command( <<-EOF
+        # no space at beginning of the document is critical for xml
+        cat<<END>#{datafile}
+<?xml version="1.0"?>
+<catalog>
+  <driver url="jdbc:sqlite::memory:"/>
+  <driver url="jdbc:hdb::memory:"/>
+</catalog>
+END
+      EOF
+      )
+    end
+    describe command(<<-EOF
+      # xmllint --xpath '/catalog/driver/@url' '#{datafile}'
+      xmlstarlet sel -t -v "/catalog/driver/@url" '#{datafile}'
+    EOF
+    ) do
+        its(:exit_status) { should eq 0 }
+        [
+          'jdbc:sqlite::memory:',
+          'jdbc:hdb::memory:',
+        ].each do |line|
+          its(:stdout) { should match Regexp.new(line, Regexp::IGNORECASE) }
+        end
+        its(:stderr) { should_not match 'Failed to compile' }
+    end
+  end
+
+  context 'text' do
+    datafile = '/tmp/text.xml'
+    before(:each) do
+      Specinfra::Runner::run_command( <<-EOF
+        # no space at beginning of the document is critical for xml
+        cat<<END>#{datafile}
+<?xml version="1.0"?>
+<catalog>
+  <driver>
+    <url>jdbc:sqlite::memory:</url>
+  </driver>
+  <driver>
+    <url>jdbc:hdb::memory:</url>
+  </driver>
+</catalog>
+END
+    EOF
+    )
+    end
+    describe command(<<-EOF
+      # xmllint --xpath '/catalog/driver/url/text()' '#{datafile}'
+      xmlstarlet sel -t -c '/catalog/driver/url' '#{datafile}'
+    EOF
+    ) do
+        its(:exit_status) { should eq 0 }
+        [
+          'jdbc:sqlite::memory:',
+          'jdbc:hdb::memory:',
+        ].each do |line|
+          its(:stdout) { should match Regexp.new(line, Regexp::IGNORECASE) }
+        end
+        its(:stderr) { should_not match 'Failed to compile' }
+    end
+  end
+end
+
+# RPM package with the dependencies is in
+# http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/x/xmlstarlet-1.6.1-1.el7.x86_64.rpm
+# https://centos.pkgs.org/7/epel-x86_64/xmlstarlet-1.6.1-1.el7.x86_64.rpm.html
