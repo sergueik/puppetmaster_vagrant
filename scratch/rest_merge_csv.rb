d39f63866e5f2294b43ef394c4a5c4a3339f7fa5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-01-31 12:38:32 -0500

    intermediate revision - cleanup of debugging info and comments,fixed some logic issues

--- scratch/rest_merge_csv.rb
+++ scratch/rest_merge_csv.rb
@@ -20,7 +20,10 @@ options = {
 }
 
 opt = OptionParser.new
-opt.on('-bDIRECTORY', '--basedir=DIRECTORY', 'Path to the results') do |val|
+
+# loading cmdline parameters
+
+opt.on('-bDIRECTORY', '--basedir=DIRECTORY', 'Path to the csv files') do |val|
   options[:basedir] = val
 end
 
@@ -32,7 +35,7 @@ opt.on('-aAPI_KEY', '--api_key=API_KEY', ' api_key') do |val|
   options[:api_key] = val
 end
 
-opt.on('-mMAXCOUNT', '--maxcount=MAXCOUNT', Integer, 'Max number of rows to process') do |val|
+opt.on('-mMAXCOUNT', '--maxcount=MAXCOUNT', Integer, 'Max number of rows to process in one batch') do |val|
   options[:maxcount] = val
 end
 
@@ -40,15 +43,18 @@ opt.on('-d' , '--[no-]debug', 'Log detailed info') do |val|
   options[:debug] = val
 end
 
-opt.on('-f' , '--[no-]fulldump', 'Dump zips csv to SQLite (time consuming)') do |val|
+opt.on('-f' , '--[no-]fulldump', 'Initial dump of zips csv to SQLite (time consuming)') do |val|
   options[:fulldump] = val
 end
 
-opt.on('-s' , '--[no-]fullstore', 'store updated zips from SQLite  back to csv (time consuming)') do |val|
+opt.on('-s' , '--[no-]fullstore', 'Store updated zips from SQLite back into csv (time consuming)') do |val|
   options[:fullstore] = val
 end
 
 opt.parse!
+
+# use class wide constants for visibility
+
 $DEBUG = false
 if options[:debug]
   $DEBUG = options[:debug]	
@@ -68,42 +74,18 @@ $FULL_STORE = false
 if options[:fullstore]
   $FULL_STORE = options[:fullstore]	
 end
+
 # API: Multiple Zip Codes by Radius
 # activated via visiting the link https://www.zipcodeapi.com/Activate/...
-$BASE_URL_TEMPLATE = 'https://www.zipcodeapi.com/rest/<api_key>/info.<format>/<zip_code>/<distance>/<units>'
-
 $BASE_URL_TEMPLATE = 'https://www.zipcodeapi.com/rest/<api_key>/multi-radius.<format>/<distance>/<units>'
-# NOTE: POST, not GET
 
 $API_KEY = 'XXXXXXXXxxxxXXXXXXXXXXXXXXXXXXXxXXXXxXXxXXXXXXXXXXxXXXXXxxxXXXXX'
 unless options[:api_key].nil?
   $API_KEY = options[:api_key]
 end
 
-# https://rubyplus.com/articles/1141-SQL-Basics-SQLite3-Ruby-Driver-Basics
-# https://www.rubydoc.info/github/sparklemotion/sqlite3-ruby/SQLite3/Database
-
+# NOTE: database will be created in the current directory
 database_filename = 'zipcodes.db'
-if $DEBUG
-  $stderr.puts ('Creating database schema to store data ' + database_filename )
-end
-db = SQLite3::Database.new(database_filename)
-begin
-rows = db.execute <<-SQL
-  create table zipcodes(
-    primary_zipcode varchar (20),
-    neighbour_zipcodes varchar(120)
-  );
-SQL
-rescue => ex
-  # TODO: find if schema is already made
-  $stderr.puts ('Exception (ignored ) ' + ex.to_s)
-end
-
-if $DEBUG
-  puts rows
-  puts rows.inspect
-end
 
 basedir = ENV.fetch('HOME','') || ENV.fetch('USERPROFILE', '')
 
@@ -112,35 +94,98 @@ unless options[:basedir].nil?
 end
 basedir = basedir.gsub('\\', '/')
 
+# https://rubyplus.com/articles/1141-SQL-Basics-SQLite3-Ruby-Driver-Basics
+# https://www.rubydoc.info/github/sparklemotion/sqlite3-ruby/SQLite3/Database
+
 if $FULL_DUMP
-  # Full dump
+
+  if $DEBUG
+    puts ('Initialize SQL database schema in ' + database_filename )
+  end
+  db = SQLite3::Database.new(database_filename)
+  begin
+    rows = db.execute <<-EOF
+      create table zipcodes(
+        primary_zipcode varchar (20),
+        neighbour_zipcodes varchar(120)
+      );
+    EOF
+
+    if $DEBUG
+      unless rows.nil?
+        puts rows
+      end
+    end
+  rescue => ex
+    # TODO: find if schema is already made
+    puts ('Exception from initialize SQL database schema (ignored): ' + ex.message)
+  end
+  # Perform Full dump
   # NOTE: slow
   csv_file_path = "#{basedir}/Downloads/zip.csv"
   zips = CSV.read(csv_file_path )
   if $DEBUG
-    $stderr.puts ('Full dump to the database:' + zips.size.to_i.to_s + ' rows into ' + database_filename)
+    puts ('Full dump to the database:'+ database_filename +  ' Written'  + zips.size.to_i.to_s + ' rows ' )
   end
+  cnt = 0
   SQLite3::Database.new( database_filename  ) do |db|
     zips.each do |row|
       query = "insert into zipcodes (primary_zipcode,neighbour_zipcodes) values  (\"#{row[0]}\" ,\"\")"
       if $DEBUG
-        $stderr.puts ( 'Running query: ' + query)
+        if cnt < 3
+          puts 'Inserting row:'
+          pp row
+          puts ( 'Running query: ' + query)
+        end
+        cnt =  cnt + 1
       end
       db.execute(query)
-      if $DEBUG
-        pp row
-      end
     end
   end
 end
-# subset
-# NOTE: slow
-csv_file_path = "#{basedir}/Downloads/zip_small.csv"
-if $DEBUG
-  $stderr.puts "Reading zips csv from \"#{csv_file_path}\""
+$USE_SQLITE_DURING_RUN = true
+if $USE_SQLITE_DURING_RUN
+  puts ('Reading data from SQLite database ' + database_filename)
+  zips = []
+  SQLite3::Database.new( database_filename  ) do |db|
+    # SQL expectss single quotes as string delimeters
+    query = "select distinct(primary_zipcode) from zipcodes where neighbour_zipcodes is null  or neighbour_zipcodes =  ''"
+    rows = db.execute(query)
+    rows.each do |row|
+      # if $DEBUG
+      #   puts 'query result: '
+      #   pp row
+      # end
+      zips_row = []
+      zips_row[0] = row[0]
+      zips_row[1] = nil
+      zips.push zips_row
+    end
+  end
+else
+  # subset
+  # NOTE: slow
+  csv_file_path = "#{basedir}/Downloads/zip_small.csv"
+  if $DEBUG
+    puts "Reading zips csv from \"#{csv_file_path}\""
+  end
+  zips = CSV.read(csv_file_path )
 end
-zips = CSV.read(csv_file_path )
-# NOTE: slow
+puts ('Loaded ' + zips.size.to_s + ' rows')
+
+distance = 5
+inputs = {
+  'api_key'  => $API_KEY,
+  'units'    => 'miles', # 'km'
+  'format'   => 'json',
+  'distance' => distance,
+}
+base_url = $BASE_URL_TEMPLATE
+inputs.each do |key,val|
+  base_url.gsub!("<#{key}>", val.to_s)
+end
+
+puts ('Processing ' + $MAXCOUNT.to_s + ' rows')
 zips.select { |item| item.length < 2 || item[1].nil? }.slice(0,$MAXCOUNT - 1).each do |row|
   if row.length < 2	
     row.push('*')
@@ -148,49 +193,38 @@ zips.select { |item| item.length < 2 || item[1].nil? }.slice(0,$MAXCOUNT - 1).ea
     row[1] = '*'	
   end
   if $DEBUG
+    puts 'Filling missing data for row: '
     pp row
   end
   zip_code = row[0]
-  distance = 5
-  inputs = {
-    'api_key'  => $API_KEY,
-    'units'    => 'miles', # 'km'
-    'format'   => 'json',
-    'distance' => distance,
-    'zip_code' => zip_code
-  }
-  base_url = $BASE_URL_TEMPLATE
-  inputs.each do |key,val|
-    base_url.gsub!("<#{key}>", val.to_s)
-  end
   postdata = {:zip_codes => zip_code}
   if $DEBUG
-    $stderr.puts 'POST: ' + base_url
-    pp  postdata
+    puts 'POST: ' + base_url
+    # NOTE: POST, not GET
+    pp postdata
   end
   # https://github.com/rest-client/rest-client
   begin
     response = RestClient.post base_url, postdata
-
-    $stderr.puts 'Processing response code'
-    pp response.code
-    # zip_codes: 79905
-    # {base_zip_code: "79905", zip_codes: ["79905"]}
-    # https://www.zipcodeapi.com/rest/PYqmVHQg0020RNQsQIFWqJHnNokGDuw0Efhs3wO3Frvcbuylam6BbBAB449POFPa/info.json/79925/10/miles?minimal
-    # RestClient.get base_url
+    if $DEBUG
+      puts ('Processing response code ' + response.code.to_s )
+    end
     if response.code != 400  && response.code != 401 && response.code != 404  && response.code != 429
       o = JSON.load(response.body)
     end
   rescue => ex
-      o = nil
-      $stderr.puts 'Processing exception'
-      # pp response.code
-      # rest_merge_csv.rb:187:in `rescue in block in <main>': undefined method `code' for nil:NilClass (NoMethodError)
-      # C:/Ruby23-x64/lib/ruby/gems/2.3.0/gems/rest-client-2.0.2-x64-mingw32/lib/restclient/abstract_response.rb:223:in `exception_with_response': 400 Bad Request (RestClient::BadRequest)
+    o = nil
+    puts 'Ignoring RestClient exception: ' + ex.message
+    # Exception `RestClient::TooManyRequests'
+    # 400 Bad Request for e.g. zipcode: '33755-6314'
+    if ex.message =~ /429/
+      # mark ok to retry 
+      row[1] = ''
+    end
   end
   unless o.nil?
     if $DEBUG
-      $stderr.puts 'Raw response: '
+      puts 'Raw response: '
       pp o
     end
     data = o['responses'][0]
@@ -199,33 +233,44 @@ zips.select { |item| item.length < 2 || item[1].nil? }.slice(0,$MAXCOUNT - 1).ea
     zip_codes = data['zip_codes']
     puts zip_codes
     row[1] = zip_codes.join(',')
-    if $DEBUG
-      pp row
+   # if $DEBUG
+   #   pp row
+   # end
+  end
+end
+if !$USE_SQLITE_DURING_RUN
+  csv_filepath =  "#{basedir}/Downloads/zip_small.csv"
+  puts ('Updating the csv: '  + csv_filepath + ' with some of ' + zips.size.to_i.to_s + ' rows')
+  CSV.open(csv_filepath, 'w') do |o|
+    zips.each do |row|
+      o << row
     end
   end
 end
 
-CSV.open("#{basedir}/Downloads/zip_small.csv", 'w') do |o|
-  zips.each do |row|
-    o << row
-  end
-end
-$stderr.puts ('Updating the database:' + zips.size.to_i.to_s + ' rows into ' + database_filename)
+puts ('Updating the database:' + zips.size.to_i.to_s + ' rows into ' + database_filename)
+cnt = 0
 SQLite3::Database.new( database_filename )  do |db|
+  puts ('Actually Updating the database:' + database_filename)
   zips.each do |row|
-    query = "update zipcodes set neighbour_zipcodes = \"#{row[1]}\" where primary_zipcode = \"#{row[0]}\""
-    # query = "insert into zipcodes (primary_zipcode,neighbour_zipcodes) values  (\"#{row[0]}\" ,\"#{row[1]}\")"
-    if $DEBUG
-      $stderr.puts ( 'Running query: ' + query)
-    end
-    db.execute(query)
+  #  if row[1] =~ /\S/ 
+      query = "update zipcodes set neighbour_zipcodes = \"#{row[1]}\" where primary_zipcode = \"#{row[0]}\""
+      if $DEBUG
+        if cnt < 4
+          puts 'Saving row:'
+          pp row
+          puts ( 'Running query: ' + query)
+          cnt = cnt + 1 
+        end
+      end
+      db.execute(query)
+   # end
   end
 end
 if $FULL_STORE
   result_filename = "#{basedir}/Downloads/zip_full.csv"
   result = []
-
-  $stderr.puts ('Reading the data from ' + database_filename + ' backi into ' + result_filename)
+  puts ('Reading the data from ' + database_filename + ' backi into ' + result_filename)
   SQLite3::Database.new(database_filename ) do |db|
     db.execute( 'select primary_zipcode,neighbour_zipcodes from zipcodes' ) do |row|
       result_row = []
@@ -238,7 +283,7 @@ if $FULL_STORE
   CSV.open(result_filename, 'w') do |o|
     result.each do |row|
       if $DEBUG
-        $stderr.puts ( 'Updating row: ' + cnt.to_s)
+        puts ( 'Updating row: ' + cnt.to_s)
         pp row
       end
       cnt = cnt + 1
@@ -246,4 +291,16 @@ if $FULL_STORE
     end
   end
 end
+if $DEBUG
+  puts 'Counting finished rows: '
+  result = 0
+  SQLite3::Database.new( database_filename  ) do |db|
+    zips.each do |row|
+      query = "select count(primary_zipcode) from zipcodes where neighbour_zipcodes not null and neighbour_zipcodes <> ''"
+      result = db.execute(query)
+    end
+  end
+  puts ('Total rows ' + result.to_s)
+end
+
 exit
