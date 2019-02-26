a120909d95fc30a74cea703d6113f16ccd3e09c2
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-02-25 23:12:24 -0500

    intermediate revision - updated fact to support multiple "volums", and structured fact vs. to_json

--- facts/windows/diskmetrics.rb
+++ facts/windows/diskmetrics.rb
@@ -1,30 +1,57 @@
 fact_name = 'diskmetrics'
-
-# see also https://stackoverflow.com/questions/2903169/calculate-free-space-of-c-drive-using-vbscript
+@debug = false
+# NOTE: with older facter: undefined method `setcode' for #<Facter::Core::Aggregate:0x4529178> (NoMethodError)
+# Facter.add(fact_name, :type => :aggregate) do
 Facter.add(fact_name) do
   setcode do
     confine :operatingsystem => :windows
-    result = {}
+    result = []
     require 'win32ole'
 
     wmi_namespace = 'winmgmts://./root/CIMV2'
     # wmi_query = "select * from Win32_LogicalDisk"
     # wmi_query = "select * from Win32_LogicalDisk where DeviceID=\"C:\""
     wmi_query = "select * from Win32_LogicalDisk where DriveType=3"
-    $stderr.puts ('WMI query: ' + wmi_query)
-    wmi_fields = ['FreeSpace', 'DeviceID','DriveType','Availability','Size','Status']
+    if @debug
+      $stderr.puts ('WMI query: ' + wmi_query)
+    end
+    wmi_fields = %w|DeviceID Size FreeSpace|
     wmi = WIN32OLE.connect(wmi_namespace)
     query = wmi.ExecQuery(wmi_query)
-
     query.each do |o|
-      field_result = nil
+      result_row = {}
+      result_column = nil
       wmi_fields.each do |wmi_field|
-        field_result = o.send(wmi_field.to_sym)
-        result[wmi_field] = field_result
+        if @debug
+          $stderr.puts ('processing ' + wmi_field )
+        end
+        result_column = o.send(wmi_field.to_sym)
+        if @debug
+          $stderr.puts ('got ' + result_column )
+        end
+        result_row[wmi_field] = result_column
       end
-      break
+      if @debug
+        pp(result_row,out = $stderr,nil)
+      end
+      result.push result_row
+      # collect all disks
+      # NOTE: will possibly have redundant data in the result ?
+      # pp(result,out = $stderr,nil)
+      # break
     end
-    pp(result,out = $stderr,nil)
-    result.to_s
+    # see also https://stackoverflow.com/questions/2903169/calculate-free-space-of-c-drive-using-vbscript
+    # http://rubyonwindows.blogspot.com/2007/07/using-ruby-wmi-to-get-win32-process.html
+    # NOTE: need to build the structured fact as in
+    # https://puppet.com/docs/facter/3.9/fact_overview.html#writing-structured-facts
+    # diskmetrics = '[{"DeviceID" => "C:", "Size" => "25570570240", "FreeSpace" => "1190244352"}, {"DeviceID" => "E:", "Size" => "2238705664", "FreeSpace" => "2198364160"}]'
+    if @debug
+      pp(result, out = $stderr,nil)
+    end
+    if @debug
+      $stderr.puts result.to_json
+    end
+    # result.to_json
+    result
   end
 end
