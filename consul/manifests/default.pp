5a8ef65289cf18e60f97dc743a7353a700d4b627
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-04-18 23:27:55 -0400

    intermediate revision : misc. fixed in Vagrantfile, enabled debug,  engaged some debug messages to be logged

--- consul/manifests/default.pp
+++ consul/manifests/default.pp
@@ -1,9 +1,22 @@
 # -*- mode: puppet -*-
 # vi: set ft=puppet :
+
 node 'default' {
-  $home = env('HOME')
-  notify {"home=${home}":}
-  $path = env('PATH')
-  notify {"path=${path}":}
   include stdlib
+  class { 'consul': # remove the sugar
+    install_method => 'none',
+    init_style     => 'unmanaged',
+    manage_service => false,
+    config_hash => {
+      'data_dir'   => '/opt/consul',
+      'datacenter' => 'east-aws',
+      'log_level'  => 'INFO',
+      'node_name'  => 'agent',
+      'retry_join' => [$::ipaddress],
+    }
+  }
+  # examine the Puppet.debug() in Vagrant log
+  consul_key_value { 'test':
+    value => '42',
+  }
 }
