44bff1aa78c0fdd96c1f186ec1f3169ef8a842f0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-04-20 22:59:09 -0400

    intermediate revision - added  debug

--- facts/windows/inline_powershell_encoded_script.rb
+++ facts/windows/inline_powershell_encoded_script.rb
@@ -1,15 +1,14 @@
-# This example script demonstrates encoding the powershell script between Ruby and  Powershell
+# This example script demonstrates encoding the script between Ruby and  Powershell
 require 'yaml'
 require 'puppet'
 require 'pp'
-
 require 'facter'
 
 fact_name = 'registry_check'
 
 property = 'ServiceDll'
 path = 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters'
-debug = false
+debug = true
 inline_script = <<-EOF
   $debugpreference = 'Continue'
   $value = (Get-ItemProperty -LiteralPath 'HKLM:\\SYSTEM\\CurrentControlSet\\services\\LanmanWorkstation\\Parameters' -name 'ServiceDll').ServiceDll
@@ -31,15 +30,23 @@ inline_script = <<-EOF
   write-debug ('Result = {0}' -f $result)
   return $result
 EOF
+inline_script = <<-EOF
+  get-WmiObject -class Win32_Process  -filter 'name="explorer.exe"'|select-object -property ProcessName
+EOF
+
 if Facter.value(:kernel) == 'windows'
   Facter.add(fact_name) do
     setcode do
      result = nil
       # NOTE: occasionally a simplified command line is better:
       # command = "\"c:/windows/system32/windowspowershell/v1.0/powershell.exe\" \"#{inline_script.gsub("\n",';')}\""
-      
+      # note can not test in cmd shell.
+      # powershell.exe -NoProfile -ExecutionPolicy unrestricted -encodedCommand IAAgAGcAZQB0AC0AVwBtAGkATwBiAGoAZQBjAHQAIAAtAGMAbABhAHMAcwAgAFcAaQBuADMAMgBfAFAAcgBvAGMAZQBzAHMAIAAgAC0AZgBpAGwAdABlAHIAIAAnAG4AYQBtAGUAPQAiAGUAeABwAGwAbwByAGUAcgAuAGUAeABlACIAJwB8AHMAZQBsAGUAYwB0AC0AbwBiAGoAZQBjAHQAIAAtAHAAcgBvAHAAZQByAHQAeQAgAFAAcgBvAGMAZQBzAHMATgBhAG0AZQA7AA=="
+      # The input line is too long.
       encoded_inline_script  = Base64.strict_encode64(inline_script.gsub("\n",';').encode('utf-16le'))
-      command = "\"c:/windows/system32/windowspowershell/v1.0/powershell.exe\" -NoProfile -ExecutionPolicy unrestricted -command -encodedCommand #{encoded_inline_script}\""
+      # NOTE: no double quoted full path powershell.exe in the command below
+      # the command is somewhat fragile
+      command = "powershell.exe -NoProfile -ExecutionPolicy unrestricted -encodedCommand #{encoded_inline_script}\""
       if debug
         STDERR.puts "command=\n#{command}"
       end
