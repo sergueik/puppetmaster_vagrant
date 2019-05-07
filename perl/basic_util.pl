d3d35f5e8775ef450dd7f5ab913ad01dc18455c6
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-06 22:59:42 -0400

    intermeidate revision - added more playground script

--- perl/basic_util.pl
+++ perl/basic_util.pl
@@ -10,23 +10,23 @@ use List::Util qw(max);
 use vars qw($DEBUG $MAX $DATA);
 $DEBUG = 0;
 
-my $result = GetOptions (
+my $result = GetOptions(
 
-   'max=i'   => \$MAX, # integer
+    'max=i'  => \$MAX,     # integer
     'data=s' => \$DATA,
     'debug'  => \$DEBUG,
 
 );
 my $rawdata = undef;
 
-if ( $DATA && -f $DATA  ) {
-  print "Reading data: $DATA " if $DEBUG;
-  open (FH ,$DATA);
-  $rawdata = do { local $/; <FH> };
-  close(FH);
+if ( $DATA && -f $DATA ) {
+    print "Reading data: $DATA " if $DEBUG;
+    open( FH, $DATA );
+    $rawdata = do { local $/; <FH> };
+    close(FH);
 
 }
-our @provided_numbers= ();
+our @provided_numbers = ();
 $rawdata ||= <<DATA;
 1 3;4,5;6
 7:20
@@ -40,20 +40,23 @@ $rawdata ||= <<DATA;
 13
 DATA
 @provided_numbers = split /(?:\s+|(?:\r?\n)+|:|;|,)/, $rawdata;
+
 # cast
-map {$_ = 0 + $_ } @provided_numbers;
+map { $_ = 0 + $_ } @provided_numbers;
 our $b = {};
 
 print Dumper \@provided_numbers if $DEBUG;
-map {$b->{$_} = 1 } @provided_numbers;
-$MAX =  max( @provided_numbers ) unless $MAX;
+map { $b->{$_} = 1 } @provided_numbers;
+$MAX = max(@provided_numbers) unless $MAX;
 print "max (\@provided_numbers) = ${MAX}\n" if $DEBUG;
-my @missing_numbers = grep {  !defined($b->{$_}) } 1..$MAX + 1 ;
-print 'Number of missing numbers: ' . scalar(@missing_numbers) ."\n" if $DEBUG;
-if (scalar(@missing_numbers)){
-  print join( "\n", @missing_numbers ) . "\n";
-} else {
-  print 'Nothing is missing';
+my @missing_numbers = grep { !defined( $b->{$_} ) } 1 .. $MAX + 1;
+print 'Number of missing numbers: ' . scalar(@missing_numbers) . "\n" if $DEBUG;
+if ( scalar(@missing_numbers) ) {
+    print join( "\n", @missing_numbers ) . "\n";
 }
+else {
+    print 'Nothing is missing';
+}
+
 # print Dumper($b) if $DEBUG;
 
