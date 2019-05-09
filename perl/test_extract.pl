a54cd92f9a636d682152b71f10e0d4e3fd3e8fd0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-09 12:35:25 -0400

    intermediate revision - refactoring HTML::TagParser example

--- perl/test_extract.pl
+++ perl/test_extract.pl
@@ -7,27 +7,25 @@ use Getopt::Long;
 use Data::Dumper qw(Dumper);
 use HTML::TagParser; # https://metacpan.org/pod/HTML::TagParser
 
-use vars qw(@data @elements @elements2);
+use vars qw(@data $element @elements);
 
 my $filename = 'links.htm'; # http://www.louisianaoutdoorproperties.com
 my $html     = HTML::TagParser->new($filename);
-@elements = $html->getElementsByAttribute( 'id', 'acListWrap' );
+$element = ($html->getElementsByAttribute( 'id', 'acListWrap' ))[0]->subTree();
+
+@elements = $element->getElementsByAttribute( 'class', 'listSubtitle' );
 print "Number of nodes: " . scalar(@elements) . "\n";
-
-@elements2 =
-  $elements[0]->subTree()->getElementsByAttribute( 'class', 'listSubtitle' );
-print "Number of nodes: " . scalar(@elements2) . "\n";
-@data = map { $_->innerText } @elements2;
+@data = map { $_->innerText } @elements;
 print Dumper \@data;
 
-@elements2 =
-  $elements[0]->subTree()->getElementsByAttribute( 'class', 'acListPrice' );
-print "Number of nodes: " . scalar(@elements2) . "\n";
-@data = map { $_->innerText } @elements2;
+@elements =
+  $element->getElementsByAttribute( 'class', 'acListPrice' );
+print "Number of nodes: " . scalar(@elements) . "\n";
+@data = map { $_->innerText } @elements;
 print Dumper \@data;
 
-@elements2 =
-  $elements[0]->subTree()->getElementsByAttribute( 'class', 'lower tabsection' );
-print "Number of nodes: " . scalar(@elements2) . "\n";
-@data = map { my $text = $_->innerText; $text =~ s|\s+| |g;  $text } @elements2;
+@elements =
+  $element->getElementsByAttribute( 'class', 'lower tabsection' );
+print "Number of nodes: " . scalar(@elements) . "\n";
+@data = map { my $text = $_->innerText; $text =~ s|\s+| |g;  $text } @elements;
 print Dumper \@data;
