53fe01a57bc53653d831cb6d1ffb9e6cb4eb61d8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-10 01:04:32 -0400

    intermediate revision - continue processing in the pipeline launch

--- loader_skeleton.rb
+++ loader_skeleton.rb
@@ -8,48 +8,56 @@ require 'optparse'
 
 @options = {
   :debug => false,
-  :node  => 'discovery',
+  :role  => 'app-worker',
   :env   => 'test',
   :dc    => 'west',
 }
+options_defined = false
+# echo application-server bcp north |./loader_skeleton.rb
+# {:debug => false, :role => "application-server", :env => "bcp", :dc => "north"}
 
-# vagrant@localhost:/vagrant$ echo server bcp north |./loader_skeleton.rb
-# {:debug=>false, :node=>"server", :env=>"bcp", :dc=>"north"}
-
-# only works right in Linux environment 
+# $stdin.tty? does not appear to work right under Windows environment
+# https://stackoverflow.com/questions/170956/how-can-i-find-which-operating-system-my-ruby-program-is-running-on
 unless (/cygwin|mswin|mingw/ =~ RUBY_PLATFORM) != nil
-  # determine input is coming from the pipe
+  # determine input is coming from the pipeline
   unless $stdin.tty?
-    # $stderr.puts 'Processing STDIN' 
-    positional  = %w|node env dc|
+    # based on:
+    # https://www.ruby-forum.com/t/how-to-determine-if-pipe-is-given/70241/4
+    # $stderr.puts 'Processing STDIN'
+    positional  = %w|role env dc debug|
     ARGF.each do |line|
       line.split(/\s+/).each_with_index do |token, index|
+	token = 'exact-value' if token == 'alias' # for values that need alias
         @options[positional[index].to_sym] = token
       end
     end
-    pp @options
-    exit
+    options_defined = true
+    # exit
   end
 end
-o = OptionParser.new
+unless options_defined
+  o = OptionParser.new
 
-# ./loader_skeleton.rb --node server --dc east --env uat{:debug=>false, :node=>"server", :env=>"uat", :dc=>"east"}
+  # ./loader_skeleton.rb --role application-server --dc east --env uat
+  # {:debug=>false, :role=>"application-server", :env=>"uat", :dc=>"east"}
 
-o.on('--node [NODE]', 'role of the node') do |val|
-  @options[:node] = val
+  o.on('--role [NODE ROLE]', 'role of the node') do |val|
+    @options[:role] = val
+  end
+
+  o.on('--dc [DC]', 'cluster data center') do |val|
+    @options[:dc] = val
+  end
+
+  o.on('--env [ENV]', 'cluster environment') do |val|
+    val = 'exact-value' if val == 'alias' # for values that need alias
+    @options[:env] = val
+  end
+
+  o.on('--debug', 'Debug') do |val|
+    @options[:debug] = val
+  end
+
+  o.parse!
 end
-
-o.on('--dc [DC]', 'cluster data center') do |val|
-  @options[:dc] = val
-end
-
-o.on('--env [ENV]', 'cluster environment') do |val|
-  @options[:env] = val
-end
-
-o.on('--debug', 'Debug') do |val|
-  @options[:debug] = val
-end
-
-o.parse!
 pp @options
