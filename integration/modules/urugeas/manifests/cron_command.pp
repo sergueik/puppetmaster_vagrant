ba785e83f0f3945f46ee2c8f4d3716922f3bb138
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-08-16 00:25:12 -0400

    intermediate revision - added Pupppet syntaxsugar for substitutions

--- integration/modules/urugeas/manifests/cron_command.pp
+++ integration/modules/urugeas/manifests/cron_command.pp
@@ -1,11 +1,11 @@
 # -*- mode: puppet -*-
 # vi: set ft=puppet :
-class urugeas::cron_command ( 
+class urugeas::cron_command (
   Variant[String,Integer,Undef] $query_param1 = 42,
   Variant[String,Integer,Undef] $query_param2 = 'some query parameter',
-  String$command_template = hiera('urugeas::cron_command'),
+  String $command_template = hiera('urugeas::cron_command'),
   # query_filename is not currently used
-  String$query_filename = lookup('urugeas::query_filename', {'default_value' => 'dummy.sql'}),
+  String $query_filename = lookup('urugeas::query_filename', {'default_value' => 'dummy.sql'}),
   # fail if there is no hieradata for query_filepath
   # like
   # ==> urugeas: Error: Function lookup() did not find a value for the name 'urugeas::query_filepath'
@@ -17,4 +17,25 @@ class urugeas::cron_command (
   $command = regsubst(regsubst(regsubst(regsubst($command_template,'\${QUERY_FILEPATH_PARAMETER}',$query_filepath,'G'),'\${QUERY_FILENAME}',$query_filename,'G'),'\${QUERY_PARAM1_PARAMETER}',"${query_param1}",'G'),'\${QUERY_PARAM2_PARAMETER}',"${query_param2}",'G')
   notify { "command: ${command}":}
 
+  $sum = reduce([0, 2, 40]) |$result, $value|  { $result + $value }
+  notify { "sum: ${sum}":}
+  $command2_template = '${key1} ${key2} ${key3}'
+  $replaceents = {
+    'not a key' => $command2_template,
+    'key1' => 'first value',
+    'key2' => 'second value',
+    'key3' => 'third value'
+  }
+  $keys = [undef, 'not a key', 'key1', 'key2', 'key3']
+  $command_result = reduce($keys) |$result, $value|  {
+    if ($result) {
+      notify {"processing ${value} to update ${result}": }
+      # Evaluation Error: Cannot reassign variable '$result'
+      regsubst($result,"${value}",$replaceents[$value],'G')
+    } else {
+      notify {"processing ${value} to initialize ${result}": }
+      $replaceents[$value]
+    }
+  }
+  notify { "command_result: ${command_result}":}
 }
