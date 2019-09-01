be788929110abc7f16b1774afed6aa52b75777a1
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-09-01 18:11:06 -0400

    intermediate revision - fixed break in the logic added break out of the loop

--- integration/quarter.sh
+++ integration/quarter.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 TODAY_MONTH_DATE=$(date +%m%d)
+# testing
+DEBUG=true
+if [[ $DEBUG == 'true' ]] ; then
+  TODAY_MONTH_DATE=0215
+fi
+if [[ $DEBUG == 'true' ]] ; then
+  echo "Today is ${TODAY_MONTH_DATE}"
+fi
 TODAY_YEAR=$(date +%Y)
 # TODO: year adjustment for 4th quarter when evaluating the last completed quarter
 
@@ -14,19 +22,20 @@ QUARTER_END_DATES=( '1:03-31'
   '3:09-30'
   '4:12-31')
 
-
 FOLLOWING_QUARTER_START_DATE=( '0401:1'
   '0701:2'
   '1001:3')
-
 QUARTER=4
 # https://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
 for ENTRY in "${FOLLOWING_QUARTER_START_DATE[@]}" ; do
   KEY="${ENTRY%%:*}"
   VALUE="${ENTRY##*:}"
   if [[ "${TODAY_MONTH_DATE}" < "${KEY}" ]] ; then
-  QUARTER=$VALUE
-    printf "%s is for %s.\n" "${TODAY_MONTH_DATE}" "${QUARTER}"
+    QUARTER=$VALUE
+    if [[ $DEBUG == 'true' ]] ; then
+      printf "Date %s is quarter %s.\n" "${TODAY_MONTH_DATE}" "${QUARTER}"
+    fi
+    break
   fi
 done
 
@@ -35,7 +44,9 @@ for ENTRY in "${QUARTER_START_DATES[@]}" ; do
   VALUE="${ENTRY##*:}"
   if [[ "${QUARTER}" = "${KEY}" ]] ; then
     QUARTER_START_DATE=$VALUE
-    printf "%s is for %s.\n" "${QUARTER}" "${QUARTER_START_DATE}"
+    if [[ $DEBUG == 'true' ]] ; then
+      printf "Start date of quarter %s is: %s.\n" "${QUARTER}" "${QUARTER_START_DATE}"
+    fi
   fi
 done
 
@@ -44,10 +55,51 @@ for ENTRY in "${QUARTER_END_DATES[@]}" ; do
   VALUE="${ENTRY##*:}"
   if [[ "${QUARTER}" = "${KEY}" ]] ; then
     QUARTER_END_DATE=$VALUE
-    printf "%s is for %s.\n" "${QUARTER}" "${QUARTER_END_DATE}"
+    if [[ $DEBUG == 'true' ]] ; then
+      printf "End date of quarter %s is: %s.\n" "${QUARTER}" "${QUARTER_END_DATE}"
+    fi
   fi
 done
 # date format YYYY-mm-dd
-printf "Current quarter is %s-%s ... %s-%s" "${TODAY_YEAR}" "${QUARTER_START_DATE}" "${TODAY_YEAR}" "${QUARTER_END_DATE}"
+printf "Current quarter is %s-%s ... %s-%s\n" "${TODAY_YEAR}" "${QUARTER_START_DATE}" "${TODAY_YEAR}" "${QUARTER_END_DATE}"
  
 
+PREV_QUARTERS=('1:4' '2:1' '3:2' '4:3')
+for ENTRY in "${PREV_QUARTERS[@]}" ; do
+  KEY="${ENTRY%%:*}"
+  VALUE="${ENTRY##*:}"
+  if [[ "${QUARTER}" = "${KEY}" ]] ; then
+    PREV_QUARTER=$VALUE
+    if [[ "${PREV_QUARTER}" == '4' ]] ;  then
+      PREV_QUARTER_YEAR=$((TODAY_YEAR -1 ))
+    else
+      PREV_QUARTER_YEAR=$TODAY_YEAR
+    fi
+    if [[ $DEBUG == 'true' ]] ; then
+      printf "Previous quarter of %s is %s.\n" "${QUARTER}" "${PREV_QUARTER}"
+    fi  
+  fi
+done
+for ENTRY in "${QUARTER_START_DATES[@]}" ; do
+  KEY="${ENTRY%%:*}"
+  VALUE="${ENTRY##*:}"
+  if [[ "${PREV_QUARTER}" = "${KEY}" ]] ; then
+    PREV_QUARTER_START_DATE=$VALUE
+    printf "Start date of quarter %s is: %s.\n" "${PREV_QUARTER}" "${PREV_QUARTER_START_DATE}"
+    break
+  fi
+done
+
+for ENTRY in "${QUARTER_END_DATES[@]}" ; do
+  KEY="${ENTRY%%:*}"
+  VALUE="${ENTRY##*:}"
+  if [[ "${PREV_QUARTER}" = "${KEY}" ]] ; then
+    PREV_QUARTER_END_DATE=$VALUE
+    if [[ $DEBUG == 'true' ]] ; then
+      printf "End date of quarter %s is: %s.\n" "${PREV_QUARTER}" "${PREV_QUARTER_END_DATE}"
+    fi
+  fi
+done
+# date format YYYY-mm-dd
+printf "Previous quarter is %s-%s ... %s-%s\n" "${PREV_QUARTER_YEAR}" "${PREV_QUARTER_START_DATE}" "${PREV_QUARTER_YEAR}" "${PREV_QUARTER_END_DATE}"
+ 
