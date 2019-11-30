1f38770ac35c17084c86ad5ac72b3b2a8bd58565
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-11-30 01:10:21 -0500

    intermediate revision - added very elementary mysql proc/function exercises

--- fill_data.sql
+++ fill_data.sql
@@ -20,8 +20,8 @@ CREATE PROCEDURE `fill_table`()
 BEGIN
 DECLARE v_event_date0 DATETIME DEFAULT CURDATE();
 DECLARE v_event_date DATETIME DEFAULT DATE_SUB(CURDATE(), INTERVAL 10 DAY);
-DECLARE V_VERBOSE TINYINT DEFAULT 0;
-DECLARE V_APP VARCHAR(10);
+DECLARE v_verbose TINYINT DEFAULT 0;
+DECLARE v_app VARCHAR(10);
 DECLARE v_count INTEGER DEFAULT 0;
 DECLARE v_conseq_count INTEGER DEFAULT 0;
 DECLARE v_event_id INTEGER DEFAULT 0;
@@ -47,7 +47,7 @@ INS: LOOP
   -- ELSE
   --   SET v_status = MOD(v_count, 2);
   -- END IF;
-  SET V_APP = 'A1';
+  SET v_app = 'A1';
   INSERT INTO events (event_date, event_id, app, status)
   VALUES (v_event_date, v_event_id, v_app, v_status );
   -- SELECT v_app, v_event_date, v_status , v_count;
@@ -67,8 +67,8 @@ BEGIN
 
 DECLARE v_event_date DATETIME DEFAULT CURDATE();
 DECLARE v_count INTEGER DEFAULT 0;
-DECLARE V_APP VARCHAR(10);
-DECLARE V_FOLLOWING_STATUS TINYINT DEFAULT 0;
+DECLARE v_app VARCHAR(10);
+DECLARE v_following_status TINYINT DEFAULT 0;
 
 DECLARE data_cursor CURSOR FOR SELECT APP, EVENT_DATE FROM events
   WHERE STATUS <> 0 ORDER BY APP, EVENT_DATE;
@@ -84,11 +84,11 @@ END;
 -- based on: https://stackoverflow.com/questions/3463283/how-to-get-rid-of-error-1329-no-data-zero-rows-fetched-selected-or-process
 OPEN data_cursor;
 INS: LOOP
-  FETCH NEXT FROM data_cursor INTO V_APP,v_event_date;
+  FETCH NEXT FROM data_cursor INTO v_app,v_event_date;
   IF v_count > 100 THEN
     LEAVE INS;
   END IF;
-  SET v_count =  v_count + 1;
+  SET v_count = v_count + 1;
 END LOOP;
 END
 //
@@ -96,16 +96,15 @@ DELIMITER ;
 
 CALL `find_subsequent_errors`();
 
-
-
 DROP TABLE IF EXISTS results;
 CREATE TABLE IF NOT EXISTS results (
   status_string VARCHAR(1024) NOT NULL
 );
+
 -- https://wiki.ispirer.com/sqlways/mysql/techniques/return-value-from-procedure
 -- TODO: inout parameter not working
 
-DROP PROCEDURE `flat_status` ;
+DROP PROCEDURE `flat_status`;
 DELIMITER //
 CREATE PROCEDURE `flat_status`()
 
@@ -167,3 +166,118 @@ SELECT @status_string;
 -- +------------------------------------+
 -- |                                  0 |
 -- +------------------------------------+
+
+DELIMITER //
+CREATE FUNCTION `dummy_calc_int`()
+RETURNS INT
+BEGIN
+DECLARE v_count INTEGER DEFAULT 0;
+SELECT COUNT(1) FROM events WHERE status <> 0 into v_count;
+IF (v_count > 10 ) THEN
+  RETURN 0;
+ELSE  
+  RETURN 1;
+END IF;
+END;
+//
+DELIMITER ;
+
+set @result =  `dummy_calc_int`();
+select @result as result;
+DROP FUNCTION `dummy_calc_int`;
+
+
+
+DELIMITER //
+CREATE FUNCTION `make_dummy_string`()
+RETURNS VARCHAR(256)
+BEGIN
+DECLARE v_count INTEGER DEFAULT 0;
+SELECT COUNT(1) FROM events WHERE status <> 0 into v_count;
+IF (v_count > 10 ) THEN
+  RETURN '0000000000';
+END IF;
+RETURN '1111111111';
+END;
+//
+DELIMITER ;
+
+set @result =  `make_dummy_string`();
+select @result as result;
+DROP FUNCTION `make_dummy_string`;
+
+
+
+
+DELIMITER //
+CREATE FUNCTION `make_dummy_string`()
+RETURNS VARCHAR(256)
+BEGIN
+DECLARE v_count INTEGER DEFAULT 0;
+INS: LOOP
+  IF v_count > 100 THEN
+    LEAVE INS;
+  END IF;
+  SET v_count = v_count + 1;
+END LOOP;
+
+IF (v_count > 10 ) THEN
+  RETURN CONCAT('Result = ', v_count);
+END IF;
+RETURN '0';
+END;
+//
+DELIMITER ;
+
+set @result = `make_dummy_string`();
+select @result as result;
+
+-- +--------------+
+-- | result       |
+-- +--------------+
+-- | Result = 101 |
+-- +--------------+
+DROP FUNCTION `make_dummy_string`;
+
+
+--- following is broken
+
+DROP FUNCTION `construct_index_string`;
+
+DELIMITER //
+CREATE FUNCTION `construct_index_string`()
+RETURNS VARCHAR(1024)
+BEGIN
+DECLARE v_status_string VARCHAR(1024) default '';
+DECLARE v_count INTEGER DEFAULT 0;
+DECLARE v_status TINYINT DEFAULT 0;
+
+DECLARE data_cursor CURSOR FOR SELECT status FROM events
+  ORDER BY app, event_date limit 100;
+OPEN data_cursor;
+INS: LOOP
+  FETCH NEXT FROM data_cursor INTO v_status;
+  IF v_count > 100 THEN
+    LEAVE INS;
+  END IF;
+  SET v_count = v_count + 1;
+  IF v_status THEN
+    set v_status_string = CONCAT(v_status_string , 'o');
+  ELSE
+    set v_status_string = CONCAT(v_status_string , 'x');
+  END IF;
+END LOOP;
+RETURN (select v_status_string);
+END;
+//
+DELIMITER;
+
+
+set @result =  `construct_index_string`();
+select @result as result;
+
+
+-- ERROR 1415 (0A000): Not allowed to return a result set from a function
+-- RETURN v_status_string;
+
+
