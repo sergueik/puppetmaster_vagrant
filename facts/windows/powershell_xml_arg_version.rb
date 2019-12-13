808d0c2fa31c2061ef00609db0b42879dc7ff025
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-12-13 01:35:43 -0500

    intermediate revision - added fact executing powershell via -command with param exaample

---
+++ facts/windows/powershell_xml_arg_version.rb
@@ -0,0 +1,34 @@
+# name of the custom fact
+fact_name = 'powershell_version'
+
+# code of the fact
+$DEBUG = false
+if Facter.value(:kernel) == 'windows'
+  def powershell_xml_get_version
+
+    result = nil
+    # version.xml can be in Unicode
+    file_path = 'version.xml'
+    if File.exists?(file_path)
+      begin
+        script = "param($file_path) ([xml](get-content -path \"${file_path}\" )).Info.Product.version"
+        powershell_exec = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+        powershell_flags = '-executionpolicy remotesigned'
+        $stderr.puts "script: #{script}" if $DEBUG
+        command = "#{powershell_exec} #{powershell_flags} -command \" & { #{script} }\" \"#{file_path}\""
+        $stderr.puts "command: #{command}" if $DEBUG
+        result = Facter::Util::Resolution.exec(command)
+      rescue => ex
+        $stderr.puts ex.to_s
+        # throw ex
+      end
+    end
+  end
+
+  Facter.add(fact_name) do
+    setcode do
+      powershell_xml_get_version
+    end
+  end
+end
+# end of custom fact
