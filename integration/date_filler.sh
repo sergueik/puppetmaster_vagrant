6d87ca8c9fc4a9121eb4eff162e8db4c15bdbadc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-01-28 22:53:14 -0500

    intermediate revision - added conemporary bash syntax version

--- integration/date_filler.sh
+++ integration/date_filler.sh
@@ -7,7 +7,7 @@ DEBUG=true
 
 cd '/tmp'
 mkdir 'range'
-pushd '/tmp/range'
+1>/dev/null 2>/dev/null pushd '/tmp/range'
 
 # NOTE locale specific date formats
 NUM=0
@@ -17,10 +17,10 @@ DATE_START='2019-01-11'
 while [ "$NEXT_DATE" != "${DATE_END}" ]; do
   NEXT_DATE=$(date -d "${DATE_START} + ${NUM} day" +%d.%m.%Y)
   ((NUM++))
-  echo $NEXT_DATE
+  echo -n $NEXT_DATE " "
   # touch $NEXT_DATE
 done
-popd
+1>/dev/null 2>/dev/null popd
 
 DATE_START='09/05/2019'
 DATE_END='10/15/2019'
@@ -40,7 +40,7 @@ until [[ "${NEXT_DATE}" > "${DATE_END}" ]]; do
     echo "Running the report over START=${INTERVAL_START} END=${INTERVAL_END}"
   else  
     if [[ $DEBUG == 'true' ]] ; then
-      echo 'Skipping collapsed report interval'
+      echo 'Skipping collapsed date interval'
     fi  
   fi
   # echo "${NEXT_DATE}"
@@ -52,8 +52,6 @@ DATE_END='10/01/2019'
 
 # exotic version of the date interval generation
 seq 0 $DAY_INREMENT $(( ($(date -d $DATE_END +%s) - $(date -d $DATE_START +%s))/84600 )) | xargs -I {} date -d "$DATE_START {} days" +%m/%d/%Y | xargs printf "%s\n"
-
-DAY_INCREMENT=10
 
 DAY_INCREMENT=10
 
@@ -71,6 +69,7 @@ fi
 
 # safer loop against date comparison:
 # accidental date format change can lead to the endless loop
+
 NEXT_DATE="${DATE_START}"
 for DAY_COUNT in $( seq 0 $DAY_INCREMENT $(( ($(date -d $DATE_END +%s) - $(date -d $DATE_START +%s))/84600 ))) ; do
   DAY_COUNT=$(expr $DAY_COUNT + $DAY_INCREMENT)
@@ -87,9 +86,41 @@ for DAY_COUNT in $( seq 0 $DAY_INCREMENT $(( ($(date -d $DATE_END +%s) - $(date 
   # avoid running SQL if report date interval collapses to the same start, end
   if [[ ${INTERVAL_START} < ${INTERVAL_END} ]]; then
     echo "Running the report over INTERVAL_START=${INTERVAL_START} INTERVAL_END=${INTERVAL_END}"
+    # run the data collection over specicic date range
+    # count the aggregated data e.g. 
+    # jq '.results | length' $CUMULATIVE_REPORT
   else 
     if [[ $DEBUG == 'true' ]] ; then
-      echo 'Skipping collapsed report interval'
+      echo 'Skipping collapsed date interval'
+    fi  
+  fi
+done
+
+# for modern bash preferred loop syntax see https://www.cyberciti.biz/faq/bash-for-loop/
+NEXT_DATE="${DATE_START}"
+MAX_DAY_COUNT=$(( ($(date -d $DATE_END +%s) - $(date -d $DATE_START +%s))/84600 ))
+MAX_DAY_COUNT=$(( ($(date -d $DATE_END +%s) - $(date -d $DATE_START +%s))/84600 ))
+for (( DAY_COUNT=0; DAY_COUNT<=$MAX_DAY_COUNT; DAY_COUNT+=$DAY_INCREMENT )) 
+do
+  if [[ $DEBUG == 'true' ]] ; then
+    echo "DAY_COUNT=${DAY_COUNT}"
+  fi
+  INTERVAL_START=$NEXT_DATE
+  NEXT_DATE=$(date -d "${DATE_START} + ${DAY_COUNT} day" +%m/%d/%Y)
+  if  [[ "${NEXT_DATE}" < "${DATE_END}" ]]; then
+    INTERVAL_END=$NEXT_DATE
+  else
+    INTERVAL_END=$DATE_END
+  fi
+  # avoid running SQL if report date interval collapses to the same start, end
+  if [[ ${INTERVAL_START} < ${INTERVAL_END} ]]; then
+    echo "Running the report over INTERVAL_START=${INTERVAL_START} INTERVAL_END=${INTERVAL_END}"
+    # run the data collection over specicic date range
+    # count the aggregated data e.g. 
+    # jq '.results | length' $CUMULATIVE_REPORT
+  else 
+    if [[ $DEBUG == 'true' ]] ; then
+      echo 'Skipping collapsed date interval'
     fi  
   fi
 done
