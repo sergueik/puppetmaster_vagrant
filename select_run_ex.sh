8af0d447411be73765c52536ecc17b2994cb9813
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-01-09 20:59:43 -0500

    intermediate revision - aded select  menu template example

---
+++ select_run_ex.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# based on: https://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script
+# Bash Menu Script Example
+DEBUG=true
+
+runprocess(){
+  for ENTRY in "${ARGUMENTS[@]}" ; do
+    KEY="${ENTRY%%=*}"
+    VALUE="${ENTRY##*=}"
+    1>&2     printf "KEY %s VALUE: %s.\n" "${KEY}" "${VALUE}"
+    1>&2 echo $ARGUMENT
+  done		
+  if [[ "$DEBUG" = 'true' ]]; then
+    1>&2 echo "PASSWORD: ${PASSWORD}"
+  fi
+}
+ARGUMENTS=()
+PS3='Please enter your choice: '
+OPTIONS=('Enter arguments' 'Password' 'Run' 'Quit')
+select OPT in "${OPTIONS[@]}"
+do
+  case $OPT in
+    "Enter arguments")
+      echo $OPT
+      read -ea ARGUMENTS
+      ;;
+    "Password")
+      echo 'Enter password: '
+      read -s PASSWORD
+      ;;
+    "Run")
+      echo "Running process"
+      DUMMY=''
+      RESULT=$(runprocess $DUMMY)
+      ;;
+    "Quit")
+      break
+      ;;
+    *) echo "invalid option $REPLY";;
+  esac
+done
