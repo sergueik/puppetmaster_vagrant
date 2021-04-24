a5ae9db9655aea2e915df30a574c8f2425021e68
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-04-24 00:24:48 -0400

    intermediate revision - refactored revealer script

--- reveal_ip.sh
+++ reveal_ip.sh
@@ -11,15 +11,21 @@ EOF
 for NODE in $CHECK_NODES ; do
   # echo "Pinging $NODE"
   # NOTE: the options of ping command is different on mingw/github shell on Windows
-  ping -c 1 $NODE 2>&1 | tee /tmp/a.txt > /dev/null
-  # ping -n 1 $NODE 2>&1 | tee /tmp/a.txt > /dev/null
-  cat /tmp/a.txt
-  # NOTE: the format of ping output is different on mingw/github shell on Windows
-  TARGET_IP=$(sed -n 's|Pinging \([^ ]*\) \[\([0-9.][0-9.]*\)\] .*$|\2|p' /tmp/a.txt  )
-  # echo "TARGET_IP=${TARGET_IP}"
-  if [ "$TARGET_IP" == "" ] ; then
+  unset TARGET_IP
+  if [ ! -z "$OS" ] ; then
+    ping -n 1 $NODE 2>&1 | tee /tmp/a.txt > /dev/null 2> /dev/null
+    # cat /tmp/a.txt
+    # NOTE: the format of ping output is different on mingw/github shell on Windows
+    TARGET_IP=$(sed -n 's|Pinging \([^ ]*\) \[\([0-9.][0-9.]*\)\] .*$|\2|p' /tmp/a.txt  )
+    # echo "TARGET_IP=${TARGET_IP}"
+  fi
+  if [ -z "$OS" ] ; then
+    ping -c 1 $NODE 2>&1 | tee /tmp/a.txt > /dev/null
     TARGET_IP=$(sed -n 's|PING \([^ ]*\) (\([0-9.][0-9.]*\)) .*$|\2|p' /tmp/a.txt  )
+  fi
+  if [ "$TARGET_IP" == '' ] ; then
+    echo 'Failed to determine ip of' $NODE
   fi
   TARGET_HOSTNAME=$(echo $NODE|sed 's|\..*$||g')
   echo -e "$TARGET_HOSTNAME\t$TARGET_IP"
-done  
+done
