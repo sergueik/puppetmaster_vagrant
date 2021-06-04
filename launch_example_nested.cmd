274dc08d7fcb8199c839e9049d7cafd9c5f6011a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-06-04 03:43:22 -0400

    intermediate revision - added argumentlist example

--- launch_example_nested.cmd
+++ launch_example_nested.cmd
@@ -8,3 +8,13 @@ REM NOTE  command easily becomes too long and silently fails
 powershell.exe -noprofile -executionpolicy remotesigned "&{ param( $d = 10 ) $env:PATH=($env:PATH +';' + 'c:\java\selenium'); [array]$r = iex 'chromedriver.exe -v' ;write-host $r[0]; start-sleep $d; }" 10
 
 powershell.exe -noprofile -executionpolicy remotesigned "&{ $env:PATH=('{0};{1}' -f $env:PATH, 'c:\java\selenium'); $result = invoke-expression 'chromedriver.exe -v' ;write-host $result; start-sleep 10; }"
+
+REM deal with arguments.
+REM Note: no need in "argumentlist" when invoked from cmd
+powershell.exe -noprofile -executionpolicy remotesigned "& { write-output $args ; } " -argumentlist "a,b,c"
+REM will print the following
+REM -argumentlist
+REM a
+REM b
+REM c
+
